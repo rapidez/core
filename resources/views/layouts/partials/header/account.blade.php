c4d2855a1c4b4992b2cb50cdb31c5342160e837b
Roy Duineveld <royduineveld@gmail.com>
2020-11-13 10:22:38 +0100

    Header account fixes

--- resources/views/layouts/partials/header/account.blade.php
+++ resources/views/layouts/partials/header/account.blade.php
@@ -14,7 +14,7 @@
             </a>
         </div>
     </div>
-    <div v-else>
+    <div class="my-1" v-else>
         <a href="/login">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
         </a>
