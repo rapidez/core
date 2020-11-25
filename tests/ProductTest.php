25d42f07086c4c057b014bfe363d2faade91390f
Roy Duineveld <royduineveld@gmail.com>
2020-11-25 16:23:30 +0100

    Test scaffolding

---
+++ tests/ProductTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rapidez\Core\Tests;
+
+use Rapidez\Core\Models\Product;
+use Rapidez\Core\Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+    public function test_supported_product_types_can_be_received()
+    {
+        foreach (['simple', 'configurable', 'downloadable'] as $unsupportedType) {
+            $this->assertInstanceOf(
+                Product::class,
+                Product::selectAttributes(['sku'])->where('type_id', $unsupportedType)->first()
+            );
+        }
+    }
+
+    public function test_unsupported_product_types_can_not_be_received()
+    {
+        foreach (['grouped', 'bundle'] as $unsupportedType) {
+            $this->assertNull(Product::selectAttributes(['sku'])->where('type_id', $unsupportedType)->first());
+        }
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Rapidez\Core\Tests;
+
+use Illuminate\Support\Facades\Schema;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use Rapidez\Core\RapidezServiceProvider;
+use TorMorten\Eventy\EventServiceProvider;
+
+class TestCase extends BaseTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpDatabase($this->app);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            EventServiceProvider::class,
+            RapidezServiceProvider::class,
+        ];
+    }
+
+    protected function setUpDatabase($app)
+    {
+        if (Schema::hasTable('yotpo_sync')) {
+            return;
+        }
+
+        fwrite(STDOUT, 'A Magento 2 database dump is being imported.' . PHP_EOL);
+
+        exec(strtr('mysql -h HOST -P PORT -u USERNAME -pPASSWORD DATABASE < DUMP', [
+            'HOST' => config('database.connections.mysql.host'),
+            'PORT' => config('database.connections.mysql.port'),
+            'USERNAME' => config('database.connections.mysql.username'),
+            'PASSWORD' => config('database.connections.mysql.password'),
+            'DATABASE' => config('database.connections.mysql.database'),
+            'DUMP' => __DIR__ . '/database/dump.sql',
+        ]));
+
+        fwrite(STDOUT, 'Done' . PHP_EOL);
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        //
+    }
+}
