b3789bc9d74b04ed649e27206d66ba6afdd81b84
Roy Duineveld <royduineveld@gmail.com>
2020-12-04 10:24:44 +0100

    Product option select placeholder fix

--- resources/views/product/partials/addtocart.blade.php
+++ resources/views/product/partials/addtocart.blade.php
@@ -9,7 +9,7 @@
                     v-model="options[superAttributeId]"
                     class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
                 >
-                    <option value="">@lang('Select') @{{ superAttribute.label }}</option>
+                    <option disabled selected hidden :value="undefined">@lang('Select') @{{ superAttribute.label.toLowerCase() }}</option>
                     <option
                         v-for="(label, value) in config.product[superAttribute.code]"
                         v-text="label"
