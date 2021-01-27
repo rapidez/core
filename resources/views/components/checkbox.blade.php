c6745bceef4750a6fcb99b70dbb3c6224c959d92
Roy Duineveld <royduineveld@gmail.com>
2021-01-27 11:46:28 +0100

    Checkbox label color

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,6 +1,6 @@
 <label class="flex items-center">
     <input type="checkbox" {{ $attributes->merge(['class' => 'focus:ring-green-500 h-4 w-4 text-green-600 border-gray-300 rounded']) }}>
-    <div class="ml-2 text-gray-800">
+    <div class="ml-2 text-gray-700">
         {{ $slot }}
     </div>
 </label>
