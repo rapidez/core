98a439bf303c21d400f0763f7b79d2d031443c46
Bob Wezelman <bob.wezelman@hotmail.com>
2021-02-04 12:18:44 +0100

    Moved some logic

--- resources/views/category/overview.blade.php
+++ resources/views/category/overview.blade.php
@@ -13,7 +13,7 @@
                 :style="baseStyles"
             >
                 <reactive-base
-                    :app="config.es_prefix + '_products'"
+                    :app="config.es_prefix + '_products_' + config.store"
                     :url="config.es_url"
                     v-if="loaded"
                 >
