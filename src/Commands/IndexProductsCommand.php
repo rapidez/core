165c610ff3dc933580ee68ffdfa852a389dc9546
Bob Wezelman <bob.wezelman@hotmail.com>
2021-02-05 14:13:25 +0100

    Revert "Add alias to indices to prevent having no products in shop"
    This reverts commit 4494ea43c4e33e659db72bcda2c1bbdbd6d4e154.

--- src/Commands/IndexProductsCommand.php
+++ src/Commands/IndexProductsCommand.php
@@ -21,9 +21,6 @@ class IndexProductsCommand extends Command
 
     protected int $chunkSize = 500;
 
-    protected string $index;
-    protected string $indexToDelete;
-
     protected Elasticsearch $elasticsearch;
 
     public function __construct(Elasticsearch $elasticsearch)
@@ -36,15 +33,13 @@ class IndexProductsCommand extends Command
     public function handle()
     {
         foreach (Store::all() as $store) {
-            $this->line('Store: ' . $store->name);
+            $this->line('Store: '.$store->name);
             config()->set('rapidez.store', $store->store_id);
-            $this->index = config('rapidez.es_prefix') . '_products_' . $store->store_id;
 
-            $this->createIndexIfNeeded($this->index,
-                $this->option('fresh'));
+            $this->createIndexIfNeeded(config('rapidez.es_prefix') . '_products_' . $store->store_id, $this->option('fresh'));
 
             $flat = (new Product)->getTable();
-            $productQuery = Product::where($flat . '.visibility', 4)->selectOnlyIndexable();
+            $productQuery = Product::where($flat.'.visibility', 4)->selectOnlyIndexable();
 
             $scopes = Eventy::filter('index.product.scopes') ?: [];
             foreach ($scopes as $scope) {
@@ -61,25 +56,11 @@ class IndexProductsCommand extends Command
                         $data[$superAttribute->code] = array_keys((array)$product->{$superAttribute->code});
                     }
                     $data = Eventy::filter('index.product.data', $data, $product);
-                    IndexProductJob::dispatch($data, $this->index);
+                    IndexProductJob::dispatch($data);
                 }
 
                 $bar->advance($this->chunkSize);
             });
-
-            if ($this->elasticsearch->indices()->exists(['index' => $this->indexToDelete])) {
-                try {
-                    $this->deleteIndex($this->indexToDelete);
-                } catch (\Exception $e) {
-                    //
-                }
-            }
-
-            try {
-                $this->attachAlias($this->index);
-            } catch (\Exception $e) {
-
-            }
 
             $bar->finish();
             $this->line('');
@@ -92,65 +73,29 @@ class IndexProductsCommand extends Command
         if ($recreate) {
             try {
                 $this->elasticsearch->indices()->delete(['index' => $index]);
-
             } catch (Missing404Exception $e) {
                 //
             }
         }
 
-        if (!$this->elasticsearch->indices()->exists(['index' => $index])) {
-            $this->createIndex($index);
-            $this->indexToDelete = str_replace(1, 2, $index);
-            $this->index = $index;
-        } else {
-            $index++;
-            $this->createIndex($index);
-            $this->indexToDelete = str_replace(2, 1, $index);
-            $this->index = $index;
-        }
-    }
-
-    public function createIndex(string $index): void
-    {
-        $this->elasticsearch->indices()->create([
-            'index' => $index,
-            'body' => [
-                'mappings' => [
-                    'properties' => [
-                        'price' => [
-                            'type' => 'double',
-                        ],
-                        'children' => [
-                            'type' => 'flattened',
+        try {
+            $this->elasticsearch->cat()->indices(['index' => $index]);
+        } catch (Missing404Exception $e) {
+            $this->elasticsearch->indices()->create([
+                'index' => $index,
+                'body'  => [
+                    'mappings' => [
+                        'properties' => [
+                            'price' => [
+                                'type' => 'double',
+                            ],
+                            'children' => [
+                                'type' => 'flattened',
+                            ]
                         ]
                     ]
                 ]
-            ]
-        ]);
-    }
-
-    public function deleteIndex(string $oldIndex): void
-    {
-        try {
-            $this->elasticsearch->indices()->delete(['index' => $oldIndex]);
-        } catch (Missing404Exception $e) {
-
+            ]);
         }
-    }
-
-    public function attachAlias(string $index): void
-    {
-        $params['body'] = [
-            'actions' => [
-                [
-                    'add' => [
-                        'index' => $index,
-                        'alias' => 'rapidez_products'
-                    ],
-                ],
-            ]
-        ];
-
-        $this->elasticsearch->indices()->updateAliases($params);
     }
 }
