78e6bd5278b14ec3c2ec8bdbc460c9a7b0d512fb
Bob Wezelman <bob.wezelman@hotmail.com>
2021-02-15 11:53:55 +0100

    Bugfix where query is run before mutation

--- resources/views/cart/overview.blade.php
+++ resources/views/cart/overview.blade.php
@@ -38,7 +38,7 @@
                             v-bind:dusk="'qty-'+index"
                         />
                         <button
-                            v-on:click="mutate(); runQuery();"
+                            v-on:click="mutate().then(runQuery());"
                             class="btn btn-primary ml-1"
                             :disabled="$root.loading"
                             title="@lang('Update')"
@@ -53,9 +53,9 @@
             <div class="w-2/6 sm:w-1/6 lg:w-1/12 flex justify-end text-right">
                 @{{ item.prices.price.value * item.quantity | price }}
                 <graphql-mutation :updatecart="true" :changes="{ cart_id: data.cart.id, cart_item_id: Number(item.id) }" query="@include('rapidez::cart.queries.removeItemFromCart')">
-                    <button slot-scope="{changes, mutate}" href="#" v-on:click="mutate(); runQuery();" class="ml-2" title="@lang('Remove')" :dusk="'item-delete-'+index">
+                    <button :disabled="$root.loading" slot-scope="{changes, mutate}" href="#" v-on:click="mutate().then(runQuery)" class="ml-2" title="@lang('Remove')" :dusk="'item-delete-'+index">
                         <svg class="fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path class="heroicon-ui" d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v14h14V5H5zm8.41 7l1.42 1.41a1 1 0 1 1-1.42 1.42L12 13.4l-1.41 1.42a1 1 0 1 1-1.42-1.42L10.6 12l-1.42-1.41a1 1 0 1 1 1.42-1.42L12 10.6l1.41-1.42a1 1 0 1 1 1.42 1.42L13.4 12z"/></svg>
-                    </a>
+                    </button>
                 </graphql-mutation>
 
             </div>
