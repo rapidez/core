8dcbdd5fc5b5283bbf9c9c4564a8e213c94407c9
Roy Duineveld <royduineveld@gmail.com>
2021-02-16 11:10:22 +0100

    Image size change

--- resources/views/product/overview.blade.php
+++ resources/views/product/overview.blade.php
@@ -12,7 +12,7 @@
                 @forelse($product->images as $image)
                     <div class="flex items-center justify-center border rounded p-5 {{ $product->images->count() == 1 ? 'h-96' : 'h-48 sm:h-64 md:h-80 lg:h-96' }}">
                         <img
-                            src="/storage/resizes/467/catalog/product{{ $image->value }}"
+                            src="/storage/resizes/450/catalog/product{{ $image->value }}"
                             alt="{{ $product->name }}"
                             class="max-h-full max-w-full"
                             loading="lazy"
