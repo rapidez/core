f8875db9e96c79eef139b840422696bf74c6f63a
Roy Duineveld <royduineveld@gmail.com>
2021-02-16 14:48:09 +0100

    Includes for pages

--- resources/views/page/overview.blade.php
+++ resources/views/page/overview.blade.php
@@ -7,8 +7,11 @@
     <div class="container mx-auto">
         <h1 class="font-bold text-4xl mb-5">{{ $page->content_heading }}</h1>
         @widget('content', 'pages', ($page->identifier == 'home' ? 'cms' : $page->identifier).'_index_index')
-        <div class="mb-5 prose prose-green">
-            {!! $page->content !!}
-        </div>
+        @includeIf('pages.'.$page->identifier)
+        @if($page->content)
+            <div class="mb-5 prose prose-green">
+                {!! $page->content !!}
+            </div>
+        @endif
     </div>
 @endsection
