7fda8e5f928b3d5882bf04aa5dd424e36ed4411e
Bob Wezelman <bob.wezelman@hotmail.com>
2021-03-01 13:59:43 +0100

    Stopped image from changing if size get's selected first

--- resources/js/components/Product/Product.vue
+++ resources/js/components/Product/Product.vue
@@ -1,10 +1,12 @@
 <script>
     export default {
+        props: ['images'],
         render() {
             return this.$scopedSlots.default({
                 changeAttribute: this.changeAttribute,
                 productImages: this.productImages,
-                attributeOptions: this.attributeOptions
+                attributeOptions: this.attributeOptions,
+                getCurrentChild: this.getCurrentChild
             })
         },
         data() {
@@ -19,14 +21,32 @@
             changeAttribute(superAttributeCode, option) {
                 let children = window.config.product.children
                 this.attributeOptions[superAttributeCode] = Number(option)
-
                 Object.keys(this.attributeOptions).forEach(key => {
                     children = Object.values(children).filter(c => c[key] === this.attributeOptions[key])
                 })
 
-                this.currentChild = children.shift()
+                this.currentChild = this.getCurrentChild(children)
                 this.productImages = this.currentChild.images
-            }
+            },
+            getCurrentChild(children) {
+                let images = JSON.parse(this.images)
+                let child
+                if(children.length > 1) {
+                    Object.keys(children).forEach(key => {
+                        console.log(children[key].images.every((val, index) => val === images[index].value))
+                        if(children[key].images.every((val, index) => val === images[index].value) === true) {
+                            this.currentChild = children[key]
+                            child = children[key]
+                       }
+                   })
+                } else {
+                    child = children.shift()
+                }
+
+                if(child === undefined) child = children.shift()
+
+                return child
         }
     }
+}
 </script>
