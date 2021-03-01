63f16f4a76fc20bfe2a05ab69f47451eb0ff0e29
Bob Wezelman <bob.wezelman@hotmail.com>
2021-03-01 11:00:00 +0100

    Refactored change image

--- resources/js/components/Product/Product.vue
+++ resources/js/components/Product/Product.vue
@@ -18,25 +18,12 @@
         methods: {
             changeAttribute(superAttributeCode, option) {
                 let children = window.config.product.children
-                Object.keys(children).forEach(key => {
-                    Object.keys(children[key]).forEach(keyAttribute => {
-                        if(keyAttribute === superAttributeCode && children[key][keyAttribute] == option) {
-                            this.attributeOptions[keyAttribute] = option
-                        }
-                    })
-                })
-
-                let child;
+                this.attributeOptions[superAttributeCode] = Number(option)
                 Object.keys(this.attributeOptions).forEach(key => {
-                    let childArray = Object.values(children)
-                    if(child === undefined) {
-                        child = Object.values(children).filter(c => c.hasOwnProperty(key) && String(c[key]) === String(this.attributeOptions[key]));
-                    } else {
-                        child = child.filter(c => c.hasOwnProperty(key) && String(c[key]) === String(this.attributeOptions[key]))
-                    }
+                    children = Object.values(children).filter(c => c[key] === this.attributeOptions[key])
                 })
 
-                this.currentChild = child.shift();
+                this.currentChild = children.shift();
                 this.productImages = this.currentChild.images
             }
         }
