3064a591819b6822cfc7678674fc3b7adfdf365b
Roy Duineveld <royduineveld@gmail.com>
2021-03-06 15:25:04 +0100

    Price filters reacts at the category

--- resources/views/category/partials/filter/price.blade.php
+++ resources/views/category/partials/filter/price.blade.php
@@ -3,6 +3,7 @@
     :component-id="filter.code"
     :data-field="filter.code"
     :title="filter.name.replace('_', ' ')"
+    :react="{and: ['category']}"
     :show-filter="false"
     :inner-class="{
         title: 'capitalize font-semibold',
