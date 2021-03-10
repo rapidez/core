0d6cf0a0cebb22fc5f28d47a79019b897c4e1568
Bob Wezelman <bob.wezelman@hotmail.com>
2021-03-10 17:33:13 +0100

    Added a filter on the attributes variable

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -2,8 +2,5 @@
     @if(!isset($label) || (isset($label) && $label))
         <x-rapidez::label for="{{ $name }}">@lang($label ?? ucfirst($name))</x-rapidez::label>
     @endif
-    @php
-        unset($attributes['wrapperClass']);
-    @endphp
-    <input {{ $attributes->merge(['class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500', 'type' => 'text', 'placeholder' => __($placeholder ?? ucfirst($name)), 'dusk' => $attributes->get('v-bind:dusk') ? null : $name, 'id' => $name]) }}>
+    <input {{ $attributes->filter(fn ($value, $key) => !in_array($key, ['wrapperClass']))->merge(['class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500', 'type' => 'text', 'placeholder' => __($placeholder ?? ucfirst($name)), 'dusk' => $attributes->get('v-bind:dusk') ? null : $name, 'id' => $name]) }}>
 </div>
