11614bfef740d672450d5028145a5847f6a41954
Bob Wezelman <bob.wezelman@hotmail.com>
2021-03-10 17:26:27 +0100

    unset wrapper class for input itself

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -2,5 +2,8 @@
     @if(!isset($label) || (isset($label) && $label))
         <x-rapidez::label for="{{ $name }}">@lang($label ?? ucfirst($name))</x-rapidez::label>
     @endif
+    @php
+        unset($attributes['wrapperClass']);
+    @endphp
     <input {{ $attributes->merge(['class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500', 'type' => 'text', 'placeholder' => __($placeholder ?? ucfirst($name)), 'dusk' => $attributes->get('v-bind:dusk') ? null : $name, 'id' => $name]) }}>
 </div>
