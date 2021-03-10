0adc0c2529ad1363c6072eb1f23e5875312ea501
Bob Wezelman <bob.wezelman@hotmail.com>
2021-03-10 20:27:11 +0100

    Added props directive

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,6 +1,7 @@
+@props(['label', 'wrapperClass', 'name'])
 <div class="{{ $wrapperClass ?? '' }}">
     @if(!isset($label) || (isset($label) && $label))
         <x-rapidez::label for="{{ $name }}">@lang($label ?? ucfirst($name))</x-rapidez::label>
     @endif
-    <input {{ $attributes->filter(fn ($value, $key) => !in_array($key, ['wrapperClass']))->merge(['class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500', 'type' => 'text', 'placeholder' => __($placeholder ?? ucfirst($name)), 'dusk' => $attributes->get('v-bind:dusk') ? null : $name, 'id' => $name]) }}>
+    <input {{ $attributes->merge(['class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500', 'type' => 'text', 'placeholder' => __($placeholder ?? ucfirst($name)), 'dusk' => $attributes->get('v-bind:dusk') ? null : $name, 'id' => $name, 'name' => $name]) }}>
 </div>
