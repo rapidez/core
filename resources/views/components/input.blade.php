6fc5c0a6ccb9d89bd1206657f223327dd900af30
Roy Duineveld <royduineveld@gmail.com>
2021-03-11 11:31:04 +0100

    Newsletter subscription improvements

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,7 +1,17 @@
-@props(['label', 'wrapperClass', 'name'])
+@props(['name', 'label', 'wrapperClass'])
+
 <div class="{{ $wrapperClass ?? '' }}">
     @if(!isset($label) || (isset($label) && $label))
-        <x-rapidez::label for="{{ $name }}">@lang($label ?? ucfirst($name))</x-rapidez::label>
+        <x-rapidez::label for="{{ $name }}">
+            @lang($label ?? ucfirst($name))
+        </x-rapidez::label>
     @endif
-    <input {{ $attributes->merge(['class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500', 'type' => 'text', 'placeholder' => __($placeholder ?? ucfirst($name)), 'dusk' => $attributes->get('v-bind:dusk') ? null : $name, 'id' => $name, 'name' => $name]) }}>
+    <input {{ $attributes->merge([
+        'id' => $name,
+        'name' => $name,
+        'type' => 'text',
+        'placeholder' => __($placeholder ?? ucfirst($name)),
+        'dusk' => $attributes->get('v-bind:dusk') ? null : $name,
+        'class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500',
+    ]) }}>
 </div>
--- resources/views/layouts/partials/footer/newsletter.blade.php
+++ resources/views/layouts/partials/footer/newsletter.blade.php
@@ -1,34 +1,46 @@
-@if(Route::currentRouteName() !== 'checkout')
-    <graphql-mutation v-cloak query="mutation {subscribeEmailToNewsletter(changes){status}}" >
-        <div class="max-w-full mx-auto px-4 pt-12 sm:px-6 lg:pt-16 lg:px-8" slot-scope="{mutate, changes, mutated}">
-            <div class="px-6 py-6 bg-gray-200 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center">
-                <div class="xl:w-0 xl:flex-1">
-                    <h2 class="text-2xl font-extrabold tracking-tight text-gray-600 sm:text-3xl">
-                      @lang('Want product news and updates?')
-                    </h2>
-                    <p class="mt-3 max-w-3xl text-lg leading-6 text-gray-600">
-                        @lang('Sign up for our newsletter to stay up to date.')
-                    </p>
-                </div>
-                <div class="sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
+<graphql-mutation v-cloak query="mutation { subscribeEmailToNewsletter(changes) { status } }" :alert="false" :clear="true">
+    <div class="max-w-full mx-auto px-5 pt-12 lg:pt-16" slot-scope="{ mutate, changes, mutated, error }">
+        <div class="px-6 py-6 bg-gray-200 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16 xl:flex xl:items-center">
+            <div class="xl:w-0 xl:flex-1">
+                <strong class="text-2xl font-extrabold tracking-tight text-gray-600 sm:text-3xl">
+                  @lang('Want product news and updates?')
+                </strong>
+                <p class="mt-3 max-w-3xl text-lg leading-6 text-gray-600">
+                    @lang('Sign up for our newsletter to stay up to date.')
+                </p>
+            </div>
+            <div class="sm:w-full sm:max-w-md xl:mt-0 xl:ml-8">
+                <p v-if="mutated" class="text-primary text-xl font-bold">
+                    @lang('Thank you for subscribing!')
+                </p>
+                <div v-else>
                     <form class="sm:flex mt-4" v-on:submit.prevent="mutate">
-                        <x-rapidez::input wrapperClass="flex-grow" label="" id="email" name="emailAddress" type="email" v-model="changes.email" autocomplete="email" required class="h-full px-5 py-3" placeholder="Enter your email" />
-                        <button type="submit" class="btn btn-primary px-5 py-3 ml-5" :disabled="$root.loading">
+                        <x-rapidez::input
+                            label=""
+                            name="email"
+                            type="email"
+                            v-model="changes.email"
+                            class="px-5 py-3"
+                            wrapperClass="flex-grow"
+                            autocomplete="email"
+                            placeholder="Enter your email"
+                            required
+                        />
+                        <button type="submit" class="btn btn-primary w-full mt-3 px-5 py-3 sm:ml-5 sm:w-auto sm:mt-0" :disabled="$root.loading">
                             @lang('Subscribe')
                         </button>
                     </form>
-                    <div v-if="mutated" class="ml-3 text-green-500">
-                        @lang('Subscribed successfully!')
-                    </div>
+                    <p v-if="error" class="mt-3 text-sm text-red-700">
+                        @{{ error }}
+                    </p>
                     <p class="mt-3 text-sm text-gray-600">
                         @lang('We care about the protection of your data. Read our')
                         <a href="/privacy-policy-cookie-restriction-mode" class="text-gray-400 font-medium underline">
-                            @lang('Privacy Policy.')
+                            @lang('Privacy Policy')
                         </a>
                     </p>
                 </div>
             </div>
         </div>
-    </graphql-mutation>
-@endif
-
+    </div>
+</graphql-mutation>
