cef38d89368cc5b91d30a5a5707c024c83e31e25
Roy Duineveld <royduineveld@gmail.com>
2021-03-12 14:40:42 +0100

    Out of stock message

--- resources/views/category/partials/listing/item/addtocart.blade.php
+++ resources/views/category/partials/listing/item/addtocart.blade.php
@@ -1,25 +1,26 @@
 <add-to-cart v-bind:product="item" v-cloak>
-    <div class="px-2" slot-scope="{ options, error, add, disabledOptions, price, getValuesByCode }">
-        <div class="font-semibold">@{{ price | price}}</div>
-        <div v-for="(superAttribute, superAttributeId) in item.super_attributes">
-            <x-rapidez::label v-bind:for="'super_attribute_'+superAttributeId">@{{ superAttribute.label }}</x-rapidez::label>
-            <x-rapidez::select
-                v-bind:id="'super_attribute_'+superAttributeId"
-                v-bind:name="superAttributeId"
-                v-model="options[superAttributeId]"
-                class="block w-full mb-3"
-            >
-                <option disabled selected hidden :value="undefined">@lang('Select') @{{ superAttribute.label.toLowerCase() }}</option>
-                <option
-                    v-for="(label, value) in getValuesByCode(superAttribute.code)"
-                    v-text="label"
-                    :value="value"
-                    :disabled="disabledOptions[superAttribute.code].includes(value)"
-                />
-            </x-rapidez::select>
-        </div>
+    <div class="px-2 pb-2" slot-scope="{ options, error, add, disabledOptions, price, getValuesByCode }">
+        <div class="font-semibold mb-2">@{{ price | price}}</div>
+        <p v-if="!item.in_stock" class="text-red-600 text-xs">@lang('Sorry! This product is currently out of stock.')</p>
+        <div v-else>
+            <div v-for="(superAttribute, superAttributeId) in item.super_attributes">
+                <x-rapidez::label v-bind:for="'super_attribute_'+superAttributeId">@{{ superAttribute.label }}</x-rapidez::label>
+                <x-rapidez::select
+                    v-bind:id="'super_attribute_'+superAttributeId"
+                    v-bind:name="superAttributeId"
+                    v-model="options[superAttributeId]"
+                    class="block w-full mb-3"
+                >
+                    <option disabled selected hidden :value="undefined">@lang('Select') @{{ superAttribute.label.toLowerCase() }}</option>
+                    <option
+                        v-for="(label, value) in getValuesByCode(superAttribute.code)"
+                        v-text="label"
+                        :value="value"
+                        :disabled="disabledOptions[superAttribute.code].includes(value)"
+                    />
+                </x-rapidez::select>
+            </div>
 
-        <div class="py-3">
             <button
                 class="btn btn-primary"
                 :disabled="$root.loading"
@@ -28,8 +29,8 @@
             >
                 @lang('Add to cart')
             </button>
-        </div>
 
-        <div v-if="error" v-text="error" class="text-red-600"></div>
+            <div v-if="error" v-text="error" class="text-red-600"></div>
+        </div>
     </div>
 </add-to-cart>
