6784dc960f8a44c266df570d3b485f3a98cbbbd7
Bob Wezelman <bob.wezelman@hotmail.com>
2021-03-16 13:25:34 +0100

    Added function for the attributes needed in category and specified the tablename in isactive scope

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -2,8 +2,9 @@
 
 namespace Rapidez\Core\Models;
 
-use Rapidez\Core\Models\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Rapidez\Core\Models\Scopes\IsActiveScope;
+use TorMorten\Eventy\Facades\Eventy;
 
 class Category extends Model
 {
@@ -15,7 +16,17 @@ class Category extends Model
     {
         parent::boot();
 
-        static::addGlobalScope(new IsActiveScope);
+        static::addGlobalScope(new IsActiveScope());
+        static::addGlobalScope('defaults', function (Builder $builder) {
+            $builder
+                ->select(parent::getQueryAttributes())
+                ->groupBy($builder->getQuery()->from . '.entity_id');
+        });
+
+        $scopes = Eventy::filter('category.scopes') ?: [];
+        foreach ($scopes as $scope) {
+            static::addGlobalScope(new $scope());
+        }
     }
 
     public function getTable()
@@ -26,5 +37,23 @@ class Category extends Model
     public function getUrlAttribute(): string
     {
         return '/' . $this->url_path . Config::getCachedByPath('catalog/seo/category_url_suffix', '.html');
+    }
+
+    public function getQueryAttributes() : array
+    {
+        return [
+                $this->getTable() . '.entity_id',
+                $this->getTable() . '.meta_title',
+                $this->getTable() . '.name',
+                $this->getTable() . '.is_anchor',
+                $this->getTable() . '.description',
+                $this->getTable() . '.is_active',
+                $this->getTable() . '.include_in_menu',
+                $this->getTable() . '.path',
+                $this->getTable() . '.parent_id',
+                $this->getTable() . '.children',
+                $this->getTable() . '.position',
+                $this->getTable() . '.url_path'
+            ];
     }
 }
