24ef7e1f980c5fa3d70c5be6b0fab0e5e3f7ccfc
Bob Wezelman <bob.wezelman@hotmail.com>
2021-03-18 16:26:50 +0100

    Changed check in template

--- resources/views/product/partials/product-list.blade.php
+++ resources/views/product/partials/product-list.blade.php
@@ -1,11 +1,10 @@
-@if(isset($productIds))
-    <reactive-base v-cloak :app="config.es_prefix + '_products_' + config.store" :url="config.es_url">
+    <reactive-base v-if="crossSellProducts !== null || '{{ isset($productIds) && count($productIds) > 0 }}'" v-cloak :app="config.es_prefix + '_products_' + config.store" :url="config.es_url">
         <reactive-list
             component-id="{{ $code }}"
             data-field="id"
             list-class="flex flex-wrap mt-5 -mx-1 overflow-hidden"
             :size="6"
-            :default-query="function () { return { query: { terms: { 'id': ['{{ implode("','", $productIds) }}'] } } } }"
+            :default-query="function () { return { query: { terms: { 'id': crossSellProducts !== null ? crossSellProducts : ['{{ isset($productIds) ? implode("','", $productIds) : '' }}'] } } } }"
         >
             <strong class="font-bold text-2xl mt-5" slot="renderResultStats">
                 @lang($title)
@@ -16,22 +15,4 @@
             @include('rapidez::category.partials.listing.item', ['perLine' => 6])
         </reactive-list>
     </reactive-base>
-@else
-    <reactive-base v-if="crossSellProducts" v-cloak :app="config.es_prefix + '_products_' + config.store" :url="config.es_url">
-        <reactive-list
-            component-id="{{ $code }}"
-            data-field="id"
-            list-class="flex flex-wrap mt-5 -mx-1 overflow-hidden"
-            :size="6"
-            :default-query="function () { return { query: { terms: { 'id': crossSellProducts } } } }"
-        >
-            <strong class="font-bold text-2xl mt-5" slot="renderResultStats">
-                @lang($title)
-            </strong>
 
-            <div slot="renderNoResults"></div>
-
-            @include('rapidez::category.partials.listing.item', ['perLine' => 6])
-        </reactive-list>
-    </reactive-base>
-@endif
