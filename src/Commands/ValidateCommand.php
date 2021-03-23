37a785946b05d5faa790218f882bbd5b7ec3dbac
Roy Duineveld <royduineveld@gmail.com>
2021-03-23 14:12:26 +0100

    Moved validate command info to the docs

--- src/Commands/ValidateCommand.php
+++ src/Commands/ValidateCommand.php
@@ -17,12 +17,8 @@ class ValidateCommand extends Command
     {
         $this->call('cache:clear');
 
-        if (!Config::getCachedByPath('catalog/frontend/flat_catalog_category', 0)) {
-            $this->error('catalog/frontend/flat_catalog_category is disabled, you have to enable it in Magento and reindex: "bin/magento indexer:reindex"');
-        }
-
-        if (!Config::getCachedByPath('catalog/frontend/flat_catalog_product', 0)) {
-            $this->error('catalog/frontend/flat_catalog_product is disabled, you have to enable it in Magento and reindex: "bin/magento indexer:reindex"');
+        if (!Config::getCachedByPath('catalog/frontend/flat_catalog_category', 0) || !Config::getCachedByPath('catalog/frontend/flat_catalog_product', 0)) {
+            $this->error('The flat tables are disabled!');
         }
 
         $nonFlatAttributes = Arr::pluck(Attribute::getCachedWhere(function ($attribute) {
@@ -31,16 +27,15 @@ class ValidateCommand extends Command
 
         if (!empty($nonFlatAttributes)) {
             $this->warn('Not all attributes are in the flat table: '.PHP_EOL.'- '.implode(PHP_EOL.'- ', $nonFlatAttributes));
-            $this->warn('It is recommended to have as much attributes in the flat table as possible. The recommended way to do this is by enabling "used_in_product_listing" (but be careful with sensitive attributes as these will end up in Elasticsearch) or "is_visible_on_front" and run the indexes afterwards: "bin/magento indexer:reindex". If you run into issues like "Row size too large" MySQL errors then you disable that option for the attributes which contains the most data until the indexes run fine.');
         }
 
         $superAttributesCount = count(Attribute::getCachedWhere(fn ($attribute) => $attribute['super']));
         $joinCount = ($superAttributesCount * 2) + (count($nonFlatAttributes) * 3) + 4;
 
         if ($joinCount > 61) {
-            $this->error('Most likely the queries needed for Rapidez will exceed 61 joins when indexing or viewing products so you have to reduce them by adding more attributes to the flat tables as described!');
+            $this->error('Most likely the queries needed for Rapidez will exceed 61 joins when indexing or viewing products so you have to reduce them by adding more attributes to the flat tables');
         }
 
-        $this->info('The validation finished, if there where any errors; fix them before you continue.');
+        $this->info('The validation finished, if there where any errors; fix them before you continue. See: https://rapidez.io/docs/0.x/installation#flat-tables');
     }
 }
