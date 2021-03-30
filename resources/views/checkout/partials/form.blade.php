046578f875eb59214bf4a81ec8b086e181190ca5
Quinten <quinten@justbetter.nl>
2021-03-30 10:12:42 +0200

    Change country field to select

--- resources/views/checkout/partials/form.blade.php
+++ resources/views/checkout/partials/form.blade.php
@@ -53,13 +53,21 @@
     />
 </div>
 <div class="col-span-12 sm:col-span-6">
-    <x-rapidez::input
-        name="{{ $type }}_country"
-        label="Country"
-        :placeholder="__('Country')"
-        v-model="checkout.{{ $type }}_address.country_id"
-        required
-    />
+    <graphql query="{ countries { two_letter_abbreviation full_name_locale } }">
+        <div v-if="data" slot-scope="{ data }">
+            <x-rapidez::select
+                name="{{ $type }}_country"
+                label="Country"
+                v-model="checkout.{{ $type }}_address.country_id"
+                class="w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500"
+                required
+            >
+                <option v-for="country in data.countries" :value="country.two_letter_abbreviation.toUpperCase()">
+                        @{{country.full_name_locale}}
+                </option>
+            </x-rapidez::select>
+        </div>
+    </graphql>
 </div>
 <div class="col-span-12 sm:col-span-6 sm:col-start-1">
     <x-rapidez::input
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,3 +1,12 @@
-<select {{ $attributes->merge(['class' => 'py-2 pl-3 pr-8 border-gray-300 rounded focus:ring-green-500 focus:border-green-500 sm:text-sm']) }}>
+@if(!isset($label) || (isset($label) && $label))
+    <x-rapidez::label for="{{ $name ?? '' }}">
+        @lang($label ?? ucfirst($name ?? ''))
+    </x-rapidez::label>
+@endif
+<select {{ $attributes->merge([
+    'id' => $name ?? '',
+    'name' => $name ?? '',
+    'class' => $class ?? 'py-2 pl-3 pr-8 border-gray-300 rounded focus:ring-green-500 focus:border-green-500 sm:text-sm'
+]) }}>
     {{ $slot }}
 </select>
