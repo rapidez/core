e59aaafd51099f85f1d991cf0df447866181ede7
Roy Duineveld <royduineveld@gmail.com>
2021-03-30 13:37:51 +0200

    Do not queue the index product job

--- src/Jobs/IndexProductJob.php
+++ src/Jobs/IndexProductJob.php
@@ -3,15 +3,11 @@
 namespace Rapidez\Core\Jobs;
 
 use Cviebrock\LaravelElasticsearch\Manager as Elasticsearch;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
-class IndexProductJob implements ShouldQueue
+class IndexProductJob
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
 
     protected string $index;
 
