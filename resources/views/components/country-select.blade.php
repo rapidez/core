2f9aa7f3f29b85215a2e52627ae156e131c532ae
Quinten <quinten@justbetter.nl>
2021-03-31 09:04:34 +0200

    Fix styling

--- resources/views/components/country-select.blade.php
+++ resources/views/components/country-select.blade.php
@@ -1,6 +1,6 @@
 <graphql query="{ countries { two_letter_abbreviation full_name_locale } }" cache="countries">
     <div v-if="data" slot-scope="{ data }">
-        <x-rapidez::select {{ $attributes }}>
+        <x-rapidez::select {{ $attributes }} class="w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500 sm:text-sm">
             <option v-for="country in data.countries" :value="country.two_letter_abbreviation.toUpperCase()">
                 @{{ country.full_name_locale }}
             </option>
