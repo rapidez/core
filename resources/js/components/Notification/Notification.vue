2a27fc561a8ebf7b96642e286aaa8b29debf1768
Bob Wezelman <bob.wezelman@hotmail.com>
2021-04-11 17:40:39 +0200

    refactor

--- resources/js/components/Notification/Notification.vue
+++ resources/js/components/Notification/Notification.vue
@@ -29,7 +29,19 @@
     export default {
         data: () => ({
             message: null,
-            timer: null
+            timer: null,
+            classTypes: {
+                'success': 'text-green-600 bg-green-100 border border-green-300',
+                'error': 'text-red-600 bg-red-100 border border-red-300',
+                'info': 'text-blue-600 bg-blue-100 border border-blue-300',
+                'warning': 'text-yellow-600 bg-yellow-100 border border-yellow-300'
+            },
+            iconTypes: {
+                'success': '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />',
+                'error': '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />',
+                'info': '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />',
+                'warning': '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />',
+            }
         }),
         mounted() {
             Bus.$on('notification-message', (message) => {
@@ -42,25 +54,11 @@
         },
         computed: {
             classType() {
-                let types = {
-                    'success': 'text-green-600 bg-green-100 border border-green-300',
-                    'error': 'text-red-600 bg-red-100 border border-red-300',
-                    'info': 'text-blue-600 bg-blue-100 border border-blue-300',
-                    'warning': 'text-yellow-600 bg-yellow-100 border border-yellow-300'
-                }
-
-                return types[this.message.type]
+                return this.classTypes[this.message.type]
             },
 
             icon() {
-                let types = {
-                    'success': '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />',
-                    'error': '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />',
-                    'info': '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />',
-                    'warning': '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />',
-                }
-
-                return types[this.message.type]
+                return this.iconTypes[this.message.type]
             }
         }
     }
