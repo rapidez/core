517c4f721104331b9c7e467f48df50e3f9c04ddb
Roy Duineveld <royduineveld@gmail.com>
2021-05-22 14:36:40 +0200

    Cleanup

--- src/Http/Controllers/UrlRewriteController.php
+++ src/Http/Controllers/UrlRewriteController.php
@@ -3,11 +3,6 @@
 namespace Rapidez\Core\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Rapidez\Core\Http\Controllers\CategoryController;
-use Rapidez\Core\Http\Controllers\PageController;
-use Rapidez\Core\Http\Controllers\ProductController;
-use Rapidez\Core\Models\Page;
-use Rapidez\Core\Models\Product;
 use Rapidez\Core\Models\Rewrite;
 
 class UrlRewriteController
