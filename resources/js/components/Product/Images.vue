f197da51c384a2fa02e76701488f9bb5dec98266
Roy Duineveld <royduineveld@gmail.com>
2021-06-17 11:31:42 +0200

    Image zoom

--- resources/js/components/Product/Images.vue
+++ resources/js/components/Product/Images.vue
@@ -2,23 +2,33 @@
     export default {
         data: () => ({
             images: false,
+            opened: false,
         }),
 
         render() {
             return this.$scopedSlots.default({
                 images: this.images,
+                opened: this.opened,
+                toggle: this.toggle,
             })
         },
 
         created() {
             let self = this
             this.$root.$on('productSuperAttributeChange', function (simpleProduct) {
-            Object.values(window.config.product.children).forEach((child) => {
+                Object.values(window.config.product.children).forEach((child) => {
                     if (child === simpleProduct && Object.values(window.config.product.super_attributes).filter((attribute) => attribute.update_image).length) {
                         self.images = simpleProduct.images
                     }
                 })
             })
         },
+
+        methods: {
+            toggle(index, event) {
+                this.opened = this.opened === false ? index : false
+                event.currentTarget.firstChild.src = event.currentTarget.href
+            },
+        }
     }
 </script>
--- resources/views/product/partials/images.blade.php
+++ resources/views/product/partials/images.blade.php
@@ -1,15 +1,20 @@
 <images>
-    <div slot-scope="{ images }">
+    <div slot-scope="{ images, opened, toggle }">
         <div v-if="!images" class="{{ in_array(count($product->images), [0, 1]) ? '' : 'grid gap-3 grid-cols-2' }}">
             @forelse($product->images as $image)
-                <div class="flex items-center justify-center border rounded p-5 {{ count($product->images) == 1 ? 'h-96' : 'h-48 sm:h-64 md:h-80 lg:h-96' }}">
+                <a
+                    href="{{ config('rapidez.media_url').'/catalog/product'.$image }}"
+                    class="flex items-center justify-center border rounded p-5 {{ count($product->images) == 1 ? 'h-96' : 'h-48 sm:h-64 md:h-80 lg:h-96' }}"
+                    :class="{ 'fixed inset-0 bg-white !h-full z-10 cursor-[zoom-out]': opened === {{ $loop->index }} }"
+                    v-on:click.prevent="toggle({{ $loop->index }}, $event)"
+                >
                     <img
                         src="/storage/resizes/450/catalog/product{{ $image }}"
                         alt="{{ $product->name }}"
                         class="max-h-full max-w-full"
                         loading="lazy"
                     />
-                </div>
+                </a>
             @empty
                 <x-rapidez::no-image class="rounded h-96"/>
             @endforelse
@@ -21,10 +26,16 @@
         So the first load we use Blade and when options change we use Vue.
         --}}
         <div v-cloak v-else :class="[0, 1].includes(images.length) ? '' : 'grid gap-3 grid-cols-2'">
-            <div
-                v-for="image in images"
+            <a
+                :href="config.media_url + '/catalog/product' + image"
+                v-for="(image, index) in images"
                 class="flex items-center justify-center border rounded p-5"
-                :class="images.length == 1 ? 'h-96' : 'h-48 sm:h-64 md:h-80 lg:h-96'"
+                :class="{
+                    'h-96': images.length == 1,
+                    'h-48 sm:h-64 md:h-80 lg:h-96': images.length != 1,
+                    'fixed inset-0 bg-white !h-full z-10 cursor-[zoom-out]': opened === index
+                }"
+                v-on:click.prevent="toggle(index, $event)"
             >
                 <img
                     :src="'/storage/resizes/450/catalog/product' + image"
@@ -32,8 +43,14 @@
                     class="max-h-full max-w-full"
                     loading="lazy"
                 />
-            </div>
+            </a>
             <x-rapidez::no-image v-if="images.length == 0" class="rounded h-96"/>
+        </div>
+
+        <div v-if="opened !== false" class="fixed top-3 right-3 z-10 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
         </div>
     </div>
 </images>
