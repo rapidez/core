c82bdfb9293192ff47bda14a6e29251d98aeaaaf
Roy Duineveld <royduineveld@gmail.com>
2021-06-30 14:35:08 +0200

    Added an example kibana.yml

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -18,6 +18,7 @@ class InstallCommand extends Command
         copy(__DIR__.'/../../tailwind.config.js', base_path('tailwind.config.js'));
         copy(__DIR__.'/../../docker-compose.yml', base_path('docker-compose.yml'));
         copy(__DIR__.'/../../elasticsearch.yml', base_path('elasticsearch.yml'));
+        copy(__DIR__.'/../../kibana.yml', base_path('kibana.yml'));
 
         $this->info('Done 🚀');
     }
