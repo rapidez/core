8a27c9fad79b874a446fc25e390754bb20dc6b1e
Bob <bob@justbetter.nl>
2021-08-10 09:12:17 +0200

    Small change

--- src/Widgets/Block.php
+++ src/Widgets/Block.php
@@ -8,12 +8,13 @@ class Block
 
     public function __construct($vars)
     {
-        $this->blockId = is_array($vars) ? $vars['block_id'] : $vars;
+        $this->blockId = is_array($vars) ? $vars['block_id'] : json_decode($vars)->block_id;
     }
 
     public function render()
     {
         $blockModel = config('rapidez.models.block');
+        
         return $blockModel::find($this->blockId)->content;
     }
 }
