96f3523c6197f9893c30f91ca6cd7c769cc60c3f
Roy Duineveld <royduineveld@gmail.com>
2021-08-27 15:36:39 +0200

    Productlist slider

--- resources/views/components/productlist.blade.php
+++ resources/views/components/productlist.blade.php
@@ -1,12 +1,11 @@
-@props(['value', 'title', 'field' => 'sku.keyword', 'size' => 6])
+@props(['value', 'title', 'field' => 'sku.keyword'])
 
 @if($value)
     <reactive-base v-cloak :app="config.es_prefix + '_products_' + config.store" :url="config.es_url">
         <reactive-list
             component-id="{{ md5(serialize($value)) }}"
             data-field="{{ $field }}"
-            list-class="flex flex-wrap mt-5 -mx-1 overflow-hidden"
-            :size="{{ $size }}"
+            :size="999"
             :default-query="function () { return { query: { terms: { '{{ $field }}': {!!
                 is_array($value)
                     ? "['".implode("','", $value)."']"
@@ -23,7 +22,23 @@
 
             <div slot="renderNoResults"></div>
 
-            @include('rapidez::listing.partials.item', ['perLine' => $size])
+            <div class="relative" slot="render" slot-scope="{ data, loading }" v-if="!loading">
+                <slider>
+                    <div slot-scope="{ navigate, showLeft, showRight }">
+                        <div class="flex mt-5 overflow-x-auto snap" ref="slider">
+                            <template v-for="item in data">
+                                @include('rapidez::listing.partials.item', ['slider' => true])
+                            </template>
+                        </div>
+                        <button class="absolute left-0 top-1/2 -translate-y-1/2 flex items-center justify-center rounded-full w-12 h-12 bg-white border hover:bg-primary hover:text-white" v-if="showLeft" v-on:click="navigate(-1)">
+                            <x-heroicon-o-chevron-left class="w-6 h-6"/>
+                        </button>
+                        <button class="absolute right-0 top-1/2 -translate-y-1/2 flex items-center justify-center rounded-full w-12 h-12 bg-white border hover:bg-primary hover:text-white" v-if="showRight"  v-on:click="navigate(1)">
+                            <x-heroicon-o-chevron-right class="w-6 h-6"/>
+                        </button>
+                    </div>
+                </slider>
+            </div>
         </reactive-list>
     </reactive-base>
 @endif
--- resources/views/widget/productlist.blade.php
+++ resources/views/widget/productlist.blade.php
@@ -1,1 +1,1 @@
-<x-rapidez::productlist field="{{ $condition->attribute }}.keyword" :value="array_map('trim', explode(',', $condition->value))" :size="$options->products_count"/>
+<x-rapidez::productlist field="{{ $condition->attribute }}.keyword" :value="array_map('trim', explode(',', $condition->value))"/>
