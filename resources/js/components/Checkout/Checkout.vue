06b09b2c1dd3f1e2ca3c9156f45c94a29bdba9fc
indy koning <indy@justbetter.nl>
2021-09-22 16:03:58 +0200

    Move responsibility for clearing cart to module canceling next step

--- resources/js/components/Checkout/Checkout.vue
+++ resources/js/components/Checkout/Checkout.vue
@@ -215,22 +215,21 @@
                         }
                     })
                     // response.data = orderId
-                    localStorage.removeItem('mask')
-                    localStorage.removeItem('cart')
 
-                    localStorage.removeItem('email')
-
-                    this.getShippingKeys().forEach((key) => {
-                        localStorage.removeItem('shipping_' + key)
-                    })
-
-                    this.$root.cart = null
                     this.$root.$emit('CheckoutPaymentSaved', {
                         order: {
                             id: response.data,
                             payment_method_code: this.checkout.payment_method
                         }
                     })
+
+                    if (!window.app.checkout.doNotGoToTheNextStep) {
+                        // If a payment method prevents us from going to the next step
+                        // it will be responsible for checking the payment status and
+                        // finishing the order if payment was successful.
+                        this.clearCart()
+                    }
+
                     return true
                 } catch (error) {
                     Notify(error.response.data.message, 'error')
