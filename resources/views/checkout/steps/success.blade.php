d3d8aef08765a3488718a177f568bb3ee229d187
indy koning <indy@justbetter.nl>
2021-09-24 10:49:23 +0200

    Only return one root element on success page

--- resources/views/checkout/steps/success.blade.php
+++ resources/views/checkout/steps/success.blade.php
@@ -1,5 +1,7 @@
 <checkout-success>
-    <h1 class="font-bold text-4xl mb-5">@lang('Order placed succesfully')</h1>
-    <p>@lang('Partytime!')</p>
-    <p class="animate-spin inline-block text-6xl">🥳</p>
+    <div>
+        <h1 class="font-bold text-4xl mb-5">@lang('Order placed succesfully')</h1>
+        <p>@lang('Partytime!')</p>
+        <p class="animate-spin inline-block text-6xl">🥳</p>
+    </div>
 </checkout-success>
