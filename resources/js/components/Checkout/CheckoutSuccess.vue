8d49ddcf081fe6acbb58d1509883fd31bd9f2c24
indy koning <indy@justbetter.nl>
2021-09-24 10:32:58 +0200

    Use relative path for import

--- resources/js/components/Checkout/CheckoutSuccess.vue
+++ resources/js/components/Checkout/CheckoutSuccess.vue
@@ -1,5 +1,5 @@
 <script>
-    import GetCart from 'Vendor/rapidez/core/resources/js/components/Cart/mixins/GetCart'
+    import GetCart from './../Cart/mixins/GetCart'
 
     export default {
         mixins: [GetCart],
