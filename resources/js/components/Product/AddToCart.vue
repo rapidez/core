d277aba23e164c781f2149484f0c9e3a734796fe
Roy Duineveld <royduineveld@gmail.com>
2021-11-03 13:54:03 +0100

    Removed unneeded code

--- resources/js/components/Product/AddToCart.vue
+++ resources/js/components/Product/AddToCart.vue
@@ -95,7 +95,7 @@
                         Notify(window.config.translations.errors.session_expired, 'error')
                         this.logout('/login')
                     }
-                    if (this.notifyError && window.location.pathname !== this.product.url) {
+                    if (this.notifyError) {
                         Notify(error.response.data.message, 'error')
                     }
                     this.error = error.response.data.message
