88fb23af4f09f265f7d0fc6efda1cf9a9a636878
Claudio Ferraro <claudio@justbetter.nl>
2021-11-12 14:24:10 +0100

    Added eventy filter to add attributes to ProductController

--- src/Http/Controllers/ProductController.php
+++ src/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace Rapidez\Core\Http\Controllers;
 
+use TorMorten\Eventy\Facades\Eventy;
+
 class ProductController
 {
     public function show(int $productId)
@@ -9,6 +11,8 @@ class ProductController
         $productModel = config('rapidez.models.product');
         $product = $productModel::selectForProductPage()->findOrFail($productId);
         $attributes = ['id', 'name', 'sku', 'super_attributes', 'children', 'price', 'images', 'url'];
+
+        $attributes = array_merge($attributes, Eventy::filter('frontend.product.attributes', $attributes));
 
         foreach ($product->super_attributes ?: [] as $superAttribute) {
             $attributes[] = $superAttribute->code;
