6a0968063ab9e1f2bdb8810a2fedea878f7e7320
Bob <bob@justbetter.nl>
2021-12-09 15:22:16 +0100

    Ability to use togglers in a toggler and close all

--- resources/js/components/Elements/Toggler.vue
+++ resources/js/components/Elements/Toggler.vue
@@ -4,9 +4,11 @@
             return this.$scopedSlots.default({
                 isOpen: this.isOpen,
                 toggle: this.toggle,
-                close: this.close
+                close: this.close,
+                closeAll: this.closeAll
             })
         },
+
         props: {
             open: {
                 type: Boolean,
@@ -15,17 +17,33 @@
             callback: {
                 type: Function,
             },
+            refs: {
+                type: String,
+                default: 'toggler-'
+            }
         },
+
         data: () => ({
             isOpen: false,
         }),
+
         mounted() {
             this.isOpen = this.open
         },
+
         methods: {
+            closeAll() {
+                let refs = Object.keys(this.$scopedSlots.default()[0].context.$refs).filter((ref) => {
+                    return ref.includes(this.refs)
+                })
+
+                refs.forEach((ref) => {
+                    this.$scopedSlots.default()[0].context.$refs[ref].close()
+                })
+            },
+
             toggle() {
                 this.isOpen = !this.isOpen
-
                 if (this.callback) {
                     this.callback(this.isOpen)
                 }
