6868b630762b80318c621b2a9c4248bd0975472f
Roy Duineveld <royduineveld@gmail.com>
2022-01-11 11:48:43 +0100

    Recaptcha component

---
+++ resources/views/components/recaptcha.blade.php
@@ -0,0 +1,5 @@
+@if(Rapidez::config('recaptcha_frontend/type_for/customer_create') == 'recaptcha_v3' && $key = Rapidez::config('recaptcha_frontend/type_recaptcha_v3/public_key', null, true))
+@push('head')
+<script src="https://www.google.com/recaptcha/api.js?render={{ $key }}" async defer></script>
+@endpush
+@endif
