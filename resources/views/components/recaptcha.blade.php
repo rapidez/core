93382842590afd1843bcd2f75ec4d6fe4dda515e
Roy Duineveld <royduineveld@gmail.com>
2022-01-31 14:11:21 +0100

    Recaptcha component location prop

--- resources/views/components/recaptcha.blade.php
+++ resources/views/components/recaptcha.blade.php
@@ -1,4 +1,6 @@
-@if(Rapidez::config('recaptcha_frontend/type_for/customer_create') == 'recaptcha_v3' && $key = Rapidez::config('recaptcha_frontend/type_recaptcha_v3/public_key', null, true))
+@props(['location'])
+
+@if(Rapidez::config('recaptcha_frontend/type_for/'.$location) == 'recaptcha_v3' && $key = Rapidez::config('recaptcha_frontend/type_recaptcha_v3/public_key', null, true))
 @push('head')
 <script src="https://www.google.com/recaptcha/api.js?render={{ $key }}" async defer></script>
 @endpush
