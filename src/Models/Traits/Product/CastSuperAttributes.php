3306d6cee76f3767c3f6d5dc8816a33b72bb9aeb
Robin Mulder <robin@justbetter.nl>
2022-02-14 09:28:01 +0100

    Only cast the Super Attributes that are really needed by the product. Prevent multiple code execution of script with the array cache driver

--- src/Models/Traits/Product/CastSuperAttributes.php
+++ src/Models/Traits/Product/CastSuperAttributes.php
@@ -3,22 +3,23 @@
 namespace Rapidez\Core\Models\Traits\Product;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cache;
 
 trait CastSuperAttributes
 {
     protected function getSuperAttributeCasts(): array
     {
-        $attributeModel = config('rapidez.models.attribute');
-        $superAttributes = Arr::pluck($attributeModel::getCachedWhere(function ($attribute) {
-            return $attribute['super'];
-        }), 'code');
+        return Cache::driver('array')->rememberForever('super_attributes_'.$this->attributes['id'], function () {
+            if($superAttributes = json_decode($this->attributes['super_attributes'], true)){
+                $superAttributes = Arr::pluck($superAttributes, 'code');
 
-        foreach ($superAttributes as $superAttribute) {
-            $casts[$superAttribute] = 'object';
-        }
+                foreach ($superAttributes as $superAttribute) {
+                    $casts[$superAttribute] = 'object';
+                }
 
-        $casts['super_attributes'] = 'object';
-
-        return $casts;
+                $casts['super_attributes'] = 'object';
+            }
+            return $casts ?? [];
+        });
     }
 }
