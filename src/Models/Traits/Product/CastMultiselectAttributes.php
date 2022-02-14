76368782aedd6b270591d776bf5fe0bccae7688a
Robin Mulder <robin@justbetter.nl>
2022-02-14 09:28:41 +0100

    Prevent multiple code execution of script with the array cache driver

--- src/Models/Traits/Product/CastMultiselectAttributes.php
+++ src/Models/Traits/Product/CastMultiselectAttributes.php
@@ -3,21 +3,25 @@
 namespace Rapidez\Core\Models\Traits\Product;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cache;
 use Rapidez\Core\Casts\Multiselect;
 
 trait CastMultiselectAttributes
 {
     protected function getMultiselectAttributeCasts(): array
     {
-        $attributeModel = config('rapidez.models.attribute');
-        $multiselectAttributes = Arr::pluck($attributeModel::getCachedWhere(function ($attribute) {
-            return $attribute['input'] == 'multiselect';
-        }), 'code');
+        return Cache::driver('array')->rememberForever('multiselect_attributes', function () {
+            $attributeModel = config('rapidez.models.attribute');
 
-        foreach ($multiselectAttributes as $multiselectAttribute) {
-            $casts[$multiselectAttribute] = Multiselect::class;
-        }
+            $multiselectAttributes = Arr::pluck($attributeModel::getCachedWhere(function ($attribute) {
+                return $attribute['input'] == 'multiselect';
+            }), 'code');
 
-        return $casts ?? [];
+            foreach ($multiselectAttributes as $multiselectAttribute) {
+                $casts[$multiselectAttribute] = Multiselect::class;
+            }
+
+            return $casts ?? [];
+        });
     }
 }
