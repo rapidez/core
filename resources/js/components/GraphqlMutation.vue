3fbafedbfefc992005b6b46e82a5856a4d94b34e
Roy Duineveld <royduineveld@gmail.com>
2022-03-16 13:52:01 +0100

    Removed the changes GraphQL mutation option in favor of variables

--- resources/js/components/GraphqlMutation.vue
+++ resources/js/components/GraphqlMutation.vue
@@ -13,10 +13,6 @@
                 type: Object,
                 default: () => ({}),
             },
-            changes: {
-                type: Object,
-                default: () => ({}),
-            },
             redirect: {
                 type: String,
                 default: '',
@@ -57,7 +53,6 @@
 
         render() {
             return this.$scopedSlots.default({
-                changes: this.changes,
                 mutate: this.mutate,
                 mutated: this.mutated,
                 error: this.error,
@@ -85,7 +80,6 @@
 
         methods: {
             async mutate() {
-                delete this.changes.id
                 this.error = false
 
                 try {
@@ -100,7 +94,7 @@
                     }
 
                     let response = await axios.post(config.magento_url + '/graphql', {
-                        query: this.query.replace('changes', this.queryfy(this.changes)),
+                        query: this.query,
                         variables: this.data,
                     }, options)
 
@@ -118,11 +112,10 @@
                     }
 
                     if (this.callback) {
-                        await this.callback(this.changes, this.data, response)
+                        await this.callback(this.data, response)
                     }
 
                     if (this.clear) {
-                        this.changes = {}
                         this.data = {}
                     }
 
@@ -159,29 +152,6 @@
                     })
                 })
             },
-
-            // Credits: https://stackoverflow.com/a/54262737/622945
-            queryfy (obj, key = null) {
-                if (typeof obj === 'number') {
-                    return obj
-                }
-
-                if (obj === null) {
-                    return JSON.stringify('')
-                }
-
-                if (typeof obj !== 'object' || Array.isArray(obj)) {
-                    return key == 'country_code' ? obj : JSON.stringify(obj)
-                }
-
-                let props = Object.keys(obj).map(key =>
-                     `${key}:${this.queryfy(obj[key], key)}`
-                ).join(',')
-
-                return Object.keys(this.changes).length === 1
-                    ? props
-                    : `{${props}}`
-            }
         }
     }
 </script>
