573b87aa2bfbc94b3b2f1a41bc7bbebcbc1f9233
Roy Duineveld <royduineveld@gmail.com>
2022-03-25 09:18:32 +0100

    Fixed the tests

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -9,17 +9,17 @@ class ProductTest extends TestCase
 {
     public function test_supported_product_types_can_be_received()
     {
-        foreach (['simple', 'configurable', 'downloadable'] as $unsupportedType) {
+        foreach (['simple', 'configurable', 'downloadable', 'grouped'] as $supportedType) {
             $this->assertInstanceOf(
                 Product::class,
-                Product::selectAttributes(['sku'])->where($this->flat.'.type_id', $unsupportedType)->first()
+                Product::selectAttributes(['sku'])->where($this->flat.'.type_id', $supportedType)->first()
             );
         }
     }
 
     public function test_unsupported_product_types_can_not_be_received()
     {
-        foreach (['grouped', 'bundle'] as $unsupportedType) {
+        foreach (['bundle'] as $unsupportedType) {
             $this->assertNull(Product::selectAttributes(['sku'])->where($this->flat.'.type_id', $unsupportedType)->first());
         }
     }
