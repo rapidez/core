9050c869ef6e4a926e88af2a9f6bfa04557315f6
Quinten <quinten@justbetter.nl>
2022-03-29 11:58:07 +0200

    Also implement for content variables

--- src/ContentVariables/Widget.php
+++ src/ContentVariables/Widget.php
@@ -20,13 +20,22 @@ class Widget
 
             $widgetClass = config('rapidez.widgets.'.$type);
 
-            if (!class_exists($widgetClass)) {
-                return !app()->environment('production')
+            if (class_exists($widgetClass)) {
+                return (new $widgetClass((object)$options))->render();
+            }
+
+            if (is_null($widgetClass)) {
+                return ! app()->environment('production')
                     ? '<hr>'.__('Widget not implemented (:type).', compact('type')).'<hr>'
                     : '';
             }
 
-            return (new $widgetClass((object) $options))->render();
+            // When the widgetClass is not a class instance we handle it as a view name
+            $viewName = $widgetClass;
+            $widgetClass = config('rapidez.view_only_widget');
+
+            return (new $widgetClass((object)$options))->render($viewName);
+
         }, $content);
     }
 }
