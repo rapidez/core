5728deaa954e41b2533da0dfd88bc2441515f13c
Roy Duineveld <royduineveld@gmail.com>
2022-04-22 13:22:53 +0200

    Slider scroll fix

--- resources/views/listing/partials/item.blade.php
+++ resources/views/listing/partials/item.blade.php
@@ -1,4 +1,4 @@
-<div class="contents" {!! isset($slider) ? '' : 'slot="renderItem" slot-scope="{ item }"' !!}>
+<template {!! isset($slider) ? '' : 'slot="renderItem" slot-scope="{ item }"' !!}>
     <div class="flex-none w-1/2 sm:w-1/3 lg:w-1/4 px-1 my-1">
         <div class="w-full bg-white rounded hover:shadow group relative" :key="item.id">
             <a :href="item.url" class="block">
@@ -20,4 +20,4 @@
             @includeWhen(Rapidez::config('catalog/frontend/show_swatches_in_product_list', 1), 'rapidez::listing.partials.item.addtocart')
         </div>
     </div>
-</div>
+</template>
