ec148db1588a765f4773f95cd5ad87396bd68bac
Bob Wezelman <bob.wezelman@hotmail.com>
2022-05-10 12:15:31 +0200

    Index command abstraction (#132)

---
+++ src/Commands/InteractsWithElasticsearchCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Rapidez\Core\Commands;
+
+use Illuminate\Console\Command;
+use Cviebrock\LaravelElasticsearch\Manager as Elasticsearch;
+
+abstract class InteractsWithElasticsearchCommand extends Command
+{
+    protected Elasticsearch $elasticsearch;
+
+    public function __construct(Elasticsearch $elasticsearch)
+    {
+        parent::__construct();
+
+        $this->elasticsearch = $elasticsearch;
+    }
+
+    public function createIndex(string $index, array $mapping = []): void
+    {
+        $this->elasticsearch->indices()->create([
+            'index' => $index,
+            'body' => [
+                'mappings' => $mapping
+            ]
+        ]);
+    }
+
+    public function switchAlias(string $alias, string $index): void
+    {
+        $this->elasticsearch->indices()->putAlias([
+            'index' => $index,
+            'name'  => $alias,
+        ]);
+
+        $aliases = $this->elasticsearch->indices()->getAlias(['name' => $alias]);
+        foreach ($aliases as $indexLinkedToAlias => $aliasData) {
+            if ($indexLinkedToAlias != $index) {
+                $this->elasticsearch->indices()->deleteAlias([
+                    'index' => $indexLinkedToAlias,
+                    'name'  => $alias,
+                ]);
+                $this->elasticsearch->indices()->delete(['index' => $indexLinkedToAlias]);
+            }
+        }
+    }
+}
