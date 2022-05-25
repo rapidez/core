e74c4fb84573219faed34c003b5e677362d58559
Bob <bob@justbetter.nl>
2022-05-25 14:58:05 +0200

    Update numbers

--- resources/views/listing/partials/stats.blade.php
+++ resources/views/listing/partials/stats.blade.php
@@ -6,7 +6,7 @@
 
     <div class="justify-self-end mr-1">
         <select class="css-oswg5g !outline-none !rounded shadow focus:ring focus:ring-green-500" v-model="$root.listingResultsPerPage">
-            <option v-for="s in [12, 24, 36, window.config.translations.results.all]" v-bind:value="s"> @{{ s }}</option>
+            <option v-for="s in [12, 36, 72, window.config.translations.results.all]" v-bind:value="s"> @{{ s }}</option>
         </select>
     </div>
 </div>
