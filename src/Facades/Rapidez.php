e0093e49219011aae376de9f97de4eb948f1299a
indykoning <15870933+indykoning@users.noreply.github.com>
2022-10-26 14:02:19 +0200

    Improve code readability (#164)

--- src/Facades/Rapidez.php
+++ src/Facades/Rapidez.php
@@ -4,6 +4,13 @@ namespace Rapidez\Core\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static ?string config(string $path, $default = null, bool $sensitive = false)
+ * @method static ?string content($content)
+ * @method static object  fancyMagentoSyntaxDecoder(string $encodedString)
+ *
+ * @see \Rapidez\Core\Rapidez
+ */
 class Rapidez extends Facade
 {
     protected static function getFacadeAccessor()
