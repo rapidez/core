22488b09dbc60feed629de48a21ce19274d34526
Claudio Ferraro <claudio@justbetter.nl>
2022-12-16 12:57:50 +0100

    Use Tailwind arbitrary values

--- resources/views/layouts/partials/footer/navigation.blade.php
+++ resources/views/layouts/partials/footer/navigation.blade.php
@@ -1,14 +1,27 @@
-<nav class="flex flex-wrap md:flex-nowrap justify-between w-full md:gap-8">
-    @foreach(config('rapidez.footer_navigation') as $item)
-        <ul role="list" class="w-1/2 md:w-1/4 mt-4 md:mt-0 space-y-4">
-            <p class="text-base font-medium text-gray-900">{{ $item['title'] }}</p>
-            @foreach($item['children'] as $child)
-                <li>
-                    <a href="{{ $child['url'] }}" {{ $child['target_blank'] ? 'target="_blank"' : '' }} class="text-base text-gray-500 hover:text-gray-900">
-                        {{ $child['title'] }}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
-    @endforeach
+<nav class="flex flex-wrap md:flex-nowrap justify-between w-full md:gap-8 [&>ul]:w-1/2 [&>ul]:md:w-1/4 [&>ul]:mt-4 [&>ul]:md:mt-0 [&>ul]:space-y-4 [&>ul>li]:text-base [&>ul>li]:text-gray-500 hover:[&>ul>li]:text-gray-900 [&>ul>p]:text-base [&>ul>p]:font-medium [&>ul>p]:text-gray-900">
+    <ul>
+        <p>Catalog</p>
+        <li><a href="/what-is-new.html">What's New</a></li>
+        <li><a href="/women.html">Women</a></li>
+        <li><a href="/men.html">Men</a></li>
+        <li><a href="/gear.html">Gear</a></li>
+        <li><a href="/training.html">Training</a></li>
+        <li><a href="/sale.html">Sale</a></li>
+    </ul>
+    <ul>
+        <p>Support</p>
+        <li><a href="/customer-service.html">Customer Service</a></li>
+        <li><a href="https://docs.rapidez.io/" target="_blank">Documentation</a></li>
+        <li><a href="https://rapidez.io/#faq" target="_blank">FAQ</a></li>
+    </ul>
+    <ul>
+        <p>Legal</p>
+        <li><a href="/privacy-policy-cookie-restriction-mode">Privacy</a></li>
+        <li><a href="/enable-cookies">Cookies</a></li>
+    </ul>
+    <ul>
+        <p>Rapidez</p>
+        <li><a href="https://rapidez.io/" target="_blank">Website</a></li>
+        <li><a href="https://github.com/rapidez" target="_blank">GitHub</a></li>
+    </ul>
 </nav>
