838ec8e542d6663e19088f33f768191314527d2e
Claudio Ferraro <claudio@justbetter.nl>
2022-12-16 10:08:24 +0100

    Improve footer templates

---
+++ resources/views/layouts/partials/footer/copyrights.blade.php
@@ -0,0 +1,3 @@
+<p class="text-base text-gray-400 md:order-1">
+    &copy; {{ date('Y') }} <a class="hover:text-gray-500" href="https://rapidez.io/" target="_blank">{{ config('app.name') }}</a>. @lang('All rights reserved.')
+</p>
---
+++ resources/views/layouts/partials/footer/navigation.blade.php
@@ -0,0 +1,83 @@
+<nav class="grid grid-cols-2 gap-8 xl:col-span-2">
+    <div class="md:grid md:grid-cols-2 md:gap-8">
+        <div>
+            <h3 class="text-base font-medium text-gray-900">@lang('Catalog')</h3>
+            <ul role="list" class="mt-4 space-y-4">
+                <li>
+                    <a href="/what-is-new.html" class="text-base text-gray-500 hover:text-gray-900">
+                        What's New
+                    </a>
+                </li>
+                <li>
+                    <a href="/women.html" class="text-base text-gray-500 hover:text-gray-900">
+                        Women
+                    </a>
+                </li>
+                <li>
+                    <a href="/men.html" class="text-base text-gray-500 hover:text-gray-900">
+                        Men
+                    </a>
+                </li>
+                <li>
+                    <a href="/gear.html" class="text-base text-gray-500 hover:text-gray-900">
+                        Gear
+                    </a>
+                </li>
+                <li>
+                    <a href="/training.html" class="text-base text-gray-500 hover:text-gray-900">
+                        Training
+                    </a>
+                </li>
+                <li>
+                    <a href="/sale.html" class="text-base text-gray-500 hover:text-gray-900">
+                        Sale
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="mt-12 md:mt-0">
+            <h3 class="text-base font-medium text-gray-900">@lang('Support')</h3>
+            <ul role="list" class="mt-4 space-y-4">
+                <li>
+                    <a href="/customer-service" class="text-base text-gray-500 hover:text-gray-900">@lang('Customer service')</a>
+                </li>
+                <li>
+                    <a href="https://docs.rapidez.io/" target="__blank" class="text-base text-gray-500 hover:text-gray-900">
+                        @lang('Documentation')
+                    </a>
+                </li>
+                <li>
+                    <a href="https://rapidez.io/#faq" target="__blank" class="text-base text-gray-500 hover:text-gray-900">
+                        @lang('FAQ')
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="md:grid md:grid-cols-2 md:gap-8">
+        <div class="mt-12 md:mt-0">
+            <h3 class="text-base font-medium text-gray-900">@lang('Legal')</h3>
+            <ul role="list" class="mt-4 space-y-4">
+                <li>
+                    <a href="/privacy-policy-cookie-restriction-mode" class="text-base text-gray-500 hover:text-gray-900">
+                        @lang('Privacy')
+                    </a>
+                </li>
+                <li>
+                    <a href="/enable-cookies" class="text-base text-gray-500 hover:text-gray-900">@lang('Cookies')</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="text-base font-medium text-gray-900">Rapidez</h3>
+            <ul role="list" class="mt-4 space-y-4">
+                <li>
+                    <a href="https://rapidez.io/" target="_blank" class="text-base text-gray-500 hover:text-gray-900">Website</a>
+                </li>
+                <li>
+                    <a href="https://github.com/rapidez" target="_blank" class="text-base text-gray-500 hover:text-gray-900">GitHub</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
