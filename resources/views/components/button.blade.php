50635afc62887e4817a68d3657e4638114b279c0
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-01-23 09:10:16 +0100

    Button/Input changes + productlist responsiveness

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $tag = $attributes->has('href') || $attributes->has(':href') ? 'a' : 'button';
-$baseClasses[] = 'inline-block font-semibold py-2 px-4 border rounded disabled:opacity-50 disabled:cursor-not-allowed hover:opacity-75';
+$baseClasses[] = 'inline-block font-semibold py-2 px-4 border rounded disabled:opacity-50 disabled:cursor-not-allowed hover:opacity-75 whitespace-nowrap';
 $variants = [
     'primary' => 'bg-primary border-primary text-white',
     'outline' => 'bg-transparent hover:bg-primary text-primary hover:text-white border-primary hover:border-transparent',
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,17 +1,22 @@
 @props(['name', 'label', 'wrapperClass', 'labelClass'])
 
 <div class="{{ $wrapperClass ?? '' }}">
-    @if(!isset($label) || (isset($label) && $label))
-        <x-rapidez::label for="{{ $name }}" class="{{ $labelClass ?? '' }}">
+    @if (!isset($label) || (isset($label) && $label))
+        <x-rapidez::label
+            class="{{ $labelClass ?? '' }} mb-2 block text-gray-600"
+            for="{{ $name }}"
+        >
             @lang($label ?? ucfirst($name))
         </x-rapidez::label>
     @endif
-    <input {{ $attributes->merge([
-        'id' => $name,
-        'name' => $name,
-        'type' => 'text',
-        'placeholder' => __($placeholder ?? ucfirst($name)),
-        'dusk' => $attributes->get('v-bind:dusk') ? null : $name,
-        'class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500 sm:text-sm',
-    ]) }}>
+    <input
+        {{ $attributes->merge([
+            'id' => $name,
+            'name' => $name,
+            'type' => 'text',
+            'placeholder' => __($placeholder ?? ucfirst($name)),
+            'dusk' => $attributes->get('v-bind:dusk') ? null : $name,
+            'class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500 sm:text-sm text-gray-700',
+        ]) }}
+    >
 </div>
