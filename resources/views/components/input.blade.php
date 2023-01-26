0cb58d32f052124bd4a1004e55e7adff9166e12b
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-01-26 12:04:33 +0100

    Update inputs & Some styling on PDP

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -3,7 +3,7 @@
 <div class="{{ $wrapperClass ?? '' }}">
     @if (!isset($label) || (isset($label) && $label))
         <x-rapidez::label
-            class="{{ $labelClass ?? '' }} mb-2 block text-gray-600"
+            class="{{ $labelClass ?? '' }} mb-2 block text-secondary"
             for="{{ $name }}"
         >
             @lang($label ?? ucfirst($name))
@@ -16,7 +16,7 @@
             'type' => 'text',
             'placeholder' => __($placeholder ?? ucfirst($name)),
             'dusk' => $attributes->get('v-bind:dusk') ? null : $name,
-            'class' => 'w-full py-2 px-3 border-gray-300 rounded focus:ring-green-500 focus:border-green-500 sm:text-sm text-gray-700',
+            'class' => 'w-full py-2 px-3 border-border rounded !ring-0 focus:!border-secondary sm:text-sm text-primary',
         ]) }}
     >
 </div>
