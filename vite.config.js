54e8d4dbaed4702cd5442c3bd7b56b8eed1fb5c1
Daniël Smink <smink83@gmail.com>
2023-01-26 15:09:18 +0100

    Check on VITE_DEBUG env variable instead

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import { createVuePlugin } from 'vite-plugin-vue2'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 export default ({ mode }) => {
-    let vueProduction = process.env?.NODE_ENV === 'production' ? '.min' : '';
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+    let vueProduction = process.env?.VITE_DEBUG === 'false' ? '.min' : ''
 
     return defineConfig({
         plugins: [
@@ -27,5 +28,5 @@ export default ({ mode }) => {
                 'vue': path.resolve(__dirname, `./node_modules/vue/dist/vue${vueProduction}.js`)
             },
         }
-    });
+    })
 }
