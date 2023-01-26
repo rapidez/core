37acc9fb22501da34b43536f18e8c8a059a62fab
Daniël Smink <smink83@gmail.com>
2023-01-26 14:35:14 +0100

    Use minified Vue so development mode is disabled for production

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,31 @@
-import path from 'path'
+import path from 'path';
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import { createVuePlugin } from 'vite-plugin-vue2'
 import { visualizer } from 'rollup-plugin-visualizer'
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        createVuePlugin(),
-        visualizer(),
-    ],
-    resolve: {
-        preserveSymlinks: true,
-        alias: {
-            '@': path.resolve(__dirname, './resources/js'),
-            'Vendor': path.resolve(__dirname, './vendor'),
-            'vue': path.resolve(__dirname, './node_modules/vue/dist/vue.js')
+export default ({ mode }) => {
+    let vueProduction = process.env?.NODE_ENV === 'production' ? '.min' : '';
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js'
+                ],
+                refresh: true,
+            }),
+            createVuePlugin(),
+            visualizer(),
+        ],
+        resolve: {
+            preserveSymlinks: true,
+            alias: {
+                '@': path.resolve(__dirname, './resources/js'),
+                'Vendor': path.resolve(__dirname, './vendor'),
+                'vue': path.resolve(__dirname, `./node_modules/vue/dist/vue${vueProduction}.js`)
+            },
         }
-    }
-});
+    });
+}
