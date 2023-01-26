94fa925b9199d4e179f988df5c571cbccf1dde4b
indykoning <15870933+indykoning@users.noreply.github.com>
2023-01-26 15:14:55 +0100

    Load Vue as ES Module

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,13 @@ export default defineConfig({
         alias: {
             '@': path.resolve(__dirname, './resources/js'),
             'Vendor': path.resolve(__dirname, './vendor'),
-            'vue': path.resolve(__dirname, './node_modules/vue/dist/vue.js')
+            'vue': path.resolve(__dirname, './node_modules/vue/dist/vue.esm.js')
         }
-    }
+    },
+    build: {
+        commonjsOptions: {
+            // Since vue-slider-component is not esm and receives vue esm this is required.
+            requireReturnsDefault: 'preferred',
+        },
+    },
 });
