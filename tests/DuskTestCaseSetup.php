6a2f7b3807c56f2051eb4979ded5aec9cd11a380
indy koning <indy@justbetter.nl>
2023-01-27 14:37:51 +0100

    Reset idle and loading check if either turn false within a duration

--- tests/DuskTestCaseSetup.php
+++ tests/DuskTestCaseSetup.php
@@ -15,26 +15,40 @@ trait DuskTestCaseSetup
     {
         parent::setUp();
 
+        Browser::macro('waitUntilTrueForDuration', function (string $expression = 'true', float $milliseconds = 500, int $intervalMs = 50, $timeoutSeconds = 120) {
+            // Waits until the expression is truthy for x milliseconds, supports await.
+            /** @var Browser $this */
+
+            $this->waitUntil('await new Promise((resolve, reject) => {
+                let counter = 0;
+                let interval = setInterval(async function () {
+                    let result = '.$expression.';
+                    counter = result ? counter + 1 : 0;
+                    if (counter >= '.($milliseconds / $intervalMs).') {
+                        clearInterval(interval)
+                        resolve(true)
+                    }
+                }, '.$intervalMs.');
+                setTimeout(() => resolve(false), '.($timeoutSeconds * 1000).')
+            }) === true', $timeoutSeconds);
+
+            return $this;
+        });
+
         Browser::macro('waitUntilAllAjaxCallsAreFinished', function ($pauseMs = false) {
             /** @var Browser $this */
-            $this->waitUntilIdle()
-                // Ensure we repeatedly check for ajax calls while js is not idle.
-                ->waitUntil('window.app.$data?.loading === false && await new Promise((resolve, reject) => window.requestIdleCallback((deadline) => resolve(!deadline.didTimeout), {timeout: 2}))', 10)
-                ->waitUntilIdle();
-
-            if ($pauseMs) {
-                $this->pause($pauseMs);
-            }
+            $this->waitUntilTrueForDuration('window.app.$data?.loading === false && await new Promise((resolve, reject) => window.requestIdleCallback((deadline) => resolve(!deadline.didTimeout), {timeout: 2}))', $pauseMs ?: 500);
 
             return $this;
         });
 
         Browser::macro('waitUntilIdle', function ($seconds = null) {
             /** @var Browser $this */
-            $this->waitUntil('await new Promise((resolve, reject) => window.requestIdleCallback((deadline) => resolve(!deadline.didTimeout), {timeout: '.($seconds ? 2 : 0).'}));', $seconds);
+            $this->waitUntilTrueForDuration('await new Promise((resolve, reject) => window.requestIdleCallback((deadline) => resolve(!deadline.didTimeout), {timeout: 2}))', null, null, $seconds ?: 120);
 
             return $this;
         });
+
         $this->flat = (new Product())->getTable();
 
         $this->testProduct = Product::selectAttributes([
