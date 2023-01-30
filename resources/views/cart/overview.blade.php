15b3f03bbe4195bd6ee3f3b7f9bcdc329d989944
Roene <roene@justbetter.nl>
2023-01-30 14:13:13 +0100

    Styling success page | Styling summary checkout / cart | Styling progressbar checkout

--- resources/views/cart/overview.blade.php
+++ resources/views/cart/overview.blade.php
@@ -69,17 +69,17 @@
                     <div class="flex mt-5">
                         <div class="flex flex-wrap justify-end w-full self-baseline lg:w-64">
                             <div class="flex flex-wrap w-full p-3 mb-5 border rounded-lg">
-                                <div class="w-1/2">@lang('Subtotal')</div>
-                                <div class="w-1/2 text-right">@{{ cart.subtotal | price }}</div>
-                                <div class="w-1/2" v-if="cart.tax > 0">@lang('Tax')</div>
-                                <div class="w-1/2 text-right" v-if="cart.tax > 0">@{{ cart.tax | price }}</div>
-                                <div class="w-1/2" v-if="cart.shipping_amount > 0">@lang('Shipping')<br><small>@{{ cart.shipping_description }}</small></div>
-                                <div class="w-1/2 text-right" v-if="cart.shipping_amount > 0">@{{ cart.shipping_amount | price }}</div>
-                                <div class="w-1/2" v-if="cart.discount_name && cart.discount_amount < 0">@lang('Discount'): @{{ cart.discount_name }}</div>
-                                <div class="w-1/2" v-if="!cart.discount_name && cart.discount_amount < 0">@lang('Discount')</div>
-                                <div class="w-1/2 text-right" v-if="cart.discount_amount < 0">@{{ cart.discount_amount | price }}</div>
-                                <div class="w-1/2 font-bold">@lang('Total')</div>
-                                <div class="w-1/2 text-right font-bold">@{{ cart.total | price }}</div>
+                                <div class="w-1/2 py-3 border-b">@lang('Subtotal')</div>
+                                <div class="w-1/2 text-right py-3 border-b">@{{ cart.subtotal | price }}</div>
+                                <div class="w-1/2 py-3 border-b" v-if="cart.tax > 0">@lang('Tax')</div>
+                                <div class="w-1/2 text-right py-3 border-b" v-if="cart.tax > 0">@{{ cart.tax | price }}</div>
+                                <div class="w-1/2 py-3 border-b" v-if="cart.shipping_amount > 0">@lang('Shipping')<br><small>@{{ cart.shipping_description }}</small></div>
+                                <div class="w-1/2 text-right py-3 border-b" v-if="cart.shipping_amount > 0">@{{ cart.shipping_amount | price }}</div>
+                                <div class="w-1/2 py-3 border-b" v-if="cart.discount_name && cart.discount_amount < 0">@lang('Discount'): @{{ cart.discount_name }}</div>
+                                <div class="w-1/2 py-3 border-b" v-if="!cart.discount_name && cart.discount_amount < 0">@lang('Discount')</div>
+                                <div class="w-1/2 text-right py-3 border-b" v-if="cart.discount_amount < 0">@{{ cart.discount_amount | price }}</div>
+                                <div class="w-1/2 font-bold py-3">@lang('Total')</div>
+                                <div class="w-1/2 text-right font-bold py-3">@{{ cart.total | price }}</div>
                             </div>
 
                             <x-rapidez::button href="/checkout" dusk="checkout">
--- resources/views/checkout/partials/sidebar.blade.php
+++ resources/views/checkout/partials/sidebar.blade.php
@@ -1,14 +1,14 @@
 <div v-if="cart" class="p-3 border rounded">
-    <table class="w-full my-3">
-        <tr class="py-3" v-for="item in cart.items">
-            <td>@{{ item.name }}</td>
-            <td class="text-right font-mono px-4">@{{ item.qty }}</td>
-            <td class="text-right font-mono">@{{ item.price | price }}</td>
-        </tr>
-        <tr class="py-3" v-for="total_segment in checkout.totals.total_segments" v-if="total_segment.value">
-            <td>@{{ total_segment.title }}</td>
-            <td></td>
-            <td class="text-right font-mono">@{{ total_segment.value | price }}</td>
-        </tr>
-    </table>
+    <div class="flex flex-col w-full">
+        <div class="flex gap-x-1 py-3 border-b" v-for="item in cart.items">
+            <div class="w-7/12">@{{ item.name }}</div>
+            <div class="w-2/12 text-right font-mono px-4">@{{ item.qty }}</div>
+            <div class="w-3/12 text-right font-mono">@{{ item.price | price }}</div>
+        </div>
+        <div class="flex gap-x-1 py-3 border-b last:font-bold last:border-b-0" v-for="total_segment in checkout.totals.total_segments" v-if="total_segment.value">
+            <div class="w-7/12">@{{ total_segment.title }}</div>
+            <div class="w-2/12 px-4"></div>
+            <div class="w-3/12 text-right font-mono">@{{ total_segment.value | price }}</div>
+        </div>
+    </div>
 </div>
