dea2a593f91f6da18e53734c10f190922cf88aa0
Daniël Smink <smink83@gmail.com>
2023-03-01 14:27:55 +0100

    Add history fallback if turbo is not present in state

--- resources/js/turbolinks.js
+++ resources/js/turbolinks.js
@@ -2,3 +2,9 @@ import * as Turbo from "@hotwired/turbo"
 
 import TurbolinksAdapter from 'vue-turbolinks'
 Vue.use(TurbolinksAdapter)
+
+window.addEventListener('popstate', (event) => {
+    if (typeof event.state.turbo === 'undefined') {
+        Turbo.visit(new URL(event.state.path));
+    }
+})
