2fc4c2c01f15ed7b1fab98fa017d2724fffc99d7
Daniël Smink <smink83@gmail.com>
2023-03-09 13:46:30 +0100

    Remove v-bind :: should be used when overriding a param, add v-cloak

--- resources/views/components/reactive-base.blade.php
+++ resources/views/components/reactive-base.blade.php
@@ -1,8 +1,10 @@
 <reactive-base
     {{ $attributes->merge([
-        'v-bind:app' => "config.es_prefix + '_products_' + config.store",
-        'v-bind:url' => "config.es_url",
-        'v-bind:set-search-params' => "(url) => {window.history.pushState({ path: url }, '', url); window.Turbo.navigator.history.push(url);}",
+        'v-cloak' => "",
+        ':app' => "config.es_prefix + '_products_' + config.store",
+        ':url' => "config.es_url",
+        ':set-search-params' => "(url) => {window.history.pushState({ path: url }, '', url); window.Turbo.navigator.history.push(url);}",
     ]) }}>
     {{ $slot }}
 </reactive-base>
+
