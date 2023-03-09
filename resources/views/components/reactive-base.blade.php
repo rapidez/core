8f6f0073f704c06912bf5e0fd0e46305f63a4986
Daniël Smink <smink83@gmail.com>
2023-03-09 13:49:51 +0100

    Always add v-cloak

--- resources/views/components/reactive-base.blade.php
+++ resources/views/components/reactive-base.blade.php
@@ -1,10 +1,10 @@
 <reactive-base
     {{ $attributes->merge([
-        'v-cloak' => "",
         ':app' => "config.es_prefix + '_products_' + config.store",
         ':url' => "config.es_url",
         ':set-search-params' => "(url) => {window.history.pushState({ path: url }, '', url); window.Turbo.navigator.history.push(url);}",
-    ]) }}>
+    ]) }}
+    v-cloak>
     {{ $slot }}
 </reactive-base>
 
