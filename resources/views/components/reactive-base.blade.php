e28c022ec8f51b61aef70d66dc792d64b162aa74
Daniël Smink <smink83@gmail.com>
2023-03-09 14:29:15 +0100

    Just pass the method

--- resources/views/components/reactive-base.blade.php
+++ resources/views/components/reactive-base.blade.php
@@ -2,7 +2,7 @@
     {{ $attributes->merge([
         ':app' => "config.es_prefix + '_products_' + config.store",
         ':url' => "config.es_url",
-        ':set-search-params' => "(url) => setSearchParams(url)",
+        ':set-search-params' => "setSearchParams",
     ]) }}
     v-cloak>
     {{ $slot }}
