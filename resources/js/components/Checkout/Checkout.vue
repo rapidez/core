0a1855aee13bd18adde9f265c84b01d22d4d61b2
Robin Mulder <rbn.mulder@gmail.com>
2023-03-21 12:19:57 +0100

    Update resources/js/components/Checkout/Checkout.vue
    Co-authored-by: Roy Duineveld <royduineveld@gmail.com>

--- resources/js/components/Checkout/Checkout.vue
+++ resources/js/components/Checkout/Checkout.vue
@@ -313,7 +313,9 @@
                 return this.removeUnusedAddressInfo(this.$root.checkout.billing_address)
             },
             currentShipmentMethod: function () {
-                return this.checkout.shipping_methods.find(obj => obj.carrier_code+'_'+ obj.method_code === this.checkout.shipping_method)
+                return this.checkout.shipping_methods.find((method) => {
+                    return method.carrier_code + '_' + method.method_code === this.checkout.shipping_method
+                })
             }
         },
         watch: {
