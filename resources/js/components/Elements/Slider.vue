0e673bcac1e0ce0e12b39e80519287434c3d9ada
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-04-21 11:03:13 +0200

    add autoplay

--- resources/js/components/Elements/Slider.vue
+++ resources/js/components/Elements/Slider.vue
@@ -1,90 +1,103 @@
 <script>
-export default {
-    render() {
-        return this.$scopedSlots.default({
-            navigate: this.navigate,
-            showLeft: this.showLeft,
-            showRight: this.showRight,
-            currentSlide: this.currentSlide,
-            slidesTotal: this.slidesTotal,
-        })
-    },
-    props: {
-        reference: {
-            type: String,
-            default: 'slider',
+    export default {
+        render() {
+            return this.$scopedSlots.default({
+                navigate: this.navigate,
+                showLeft: this.showLeft,
+                showRight: this.showRight,
+                currentSlide: this.currentSlide,
+                slidesTotal: this.slidesTotal,
+                navigate: this.navigate,
+                currentSlide: this.currentSlide,
+                slidesTotal: this.slidesTotal,
+                setHover: this.setHover
+            })
         },
-        vertical: {
-            type: Boolean,
-            default: false,
-        },
-        autoScrollInterval: {
+        props: {
+            reference: {
+                type: String,
+                default: 'slider'
+            },
+            vertical: {
+                type: Boolean,
+                default: false
+            },
+            interval: {
             type: Number,
             default: false,
+            },
+            autoplay: {
+                type: Boolean,
+                default: false,
+            },
         },
-    },
-    data: () => {
-        return {
-            position: 0,
-            showLeft: false,
-            showRight: false,
-            mounted: false,
-            hover: false,
-        }
-    },
-    mounted() {
-        this.slider.addEventListener('scroll', this.scroll)
-        this.slider.dispatchEvent(new CustomEvent('scroll'))
-        this.mounted = true
-        if (this.interval > 0) {
-            window.setInterval(this.autoScroll, this.autoScrollInterval * 1000)
-        }
-    },
-    beforeDestroy() {
-        this.slider.removeEventListener('scroll', this.scroll)
-        if (this.autoScrollInterval > 0) {
-            window.clearInterval(this.autoScroll)
-        }
-    },
-    methods: {
-        scroll(event) {
-            this.position = this.vertical ? event.currentTarget.scrollTop : event.currentTarget.scrollLeft
-            this.showLeft = this.position
-            this.showRight = this.slider.offsetWidth + this.position < this.slider.scrollWidth - 1
-        },
-
-        navigate(index) {
-            this.vertical
-                ? this.slider.scrollTo(
-                    0,
-                    this.slider.children[index]?.offsetTop
-                )
-                : this.slider.scrollTo(
-                    this.slider.children[0]?.offsetWidth * index,
-                    0
-                )
-        },
-
-        autoScroll() {
-            if (!this.hover) {
-                let next = this.currentSlide + 1
-                while (next >= this.slidesTotal) {
-                    next -= this.slidesTotal
-                }
-                this.navigate(next)
+        data: () => {
+            return {
+                position: 0,
+                showLeft: false,
+                showRight: false,
+                mounted: false,
+                hover: false
             }
         },
-    },
-    computed: {
-        slider() {
-            return this.$scopedSlots.default()[0].context.$refs[this.reference]
+        mounted() {
+            this.slider.addEventListener('scroll', this.scroll)
+            this.slider.dispatchEvent(new CustomEvent('scroll'))
+            this.mounted = true
+            if (this.autoplay && this.interval > 0) {
+                window.setInterval(this.autoScroll, this.interval * 1000)
+            }
         },
-        currentSlide() {
-            if (this.mounted) { return this.vertical ? Math.round(this.position / this.slider.children[0]?.offsetHeight) : Math.round(this.position / this.slider.children[0]?.offsetWidth) }
+        beforeDestroy() {
+            this.slider.removeEventListener('scroll', this.scroll)
+            if (this.autoplay && this.interval > 0) {
+                window.clearInterval(this.autoScroll)
+            }
         },
-        slidesTotal() {
-            if (this.mounted) { return this.vertical ? Math.round(this.slider.scrollHeight / this.slider.offsetHeight) : Math.round(this.slider.scrollWidth / this.slider.offsetWidth) }
+        methods: {
+            scroll(event) {
+                this.position  = this.vertical ? event.currentTarget.scrollTop : event.currentTarget.scrollLeft
+                this.showLeft = this.position
+                this.showRight = (this.slider.offsetWidth + this.position) < this.slider.scrollWidth - 1
+            },
+
+            setHover(value) {
+                this.hover = value
+            },
+            autoScroll() {
+                if (!this.hover) {
+                    let next = this.currentSlide + 1
+                    while (next >= this.slidesTotal) {
+                        next -= this.slidesTotal
+                    }
+                    this.navigate(next)
+                }
+            },
+
+            navigate(index) {
+                this.vertical
+                    ? this.slider.scrollTo(0, this.slider.children[index]?.offsetTop)
+                    : this.slider.scrollTo(this.slider.children[0]?.offsetWidth * index, 0)
+            }
         },
-    },
-}
+        computed: {
+            slider() {
+                return this.$scopedSlots.default()[0].context.$refs[this.reference]
+            },
+            currentSlide() {
+                if (this.mounted) {
+                    return this.vertical
+                        ? Math.round(this.position / this.slider.children[0]?.offsetHeight)
+                        : Math.round(this.position / this.slider.children[0]?.offsetWidth)
+                }
+            },
+            slidesTotal() {
+                if (this.mounted) {
+                    return this.vertical
+                        ? Math.round(this.slider.scrollHeight / this.slider.offsetHeight)
+                        : Math.round(this.slider.scrollWidth / this.slider.offsetWidth)
+                }
+            }
+        }
+    }
 </script>
