a4469efe525897d7cf252b4f66d1aa6d1758cb3a
Jade Geels <jade@justbetter.nl>
2023-05-30 11:47:39 +0200

    Just put it in the app layout

--- resources/views/layouts/title.blade.php
+++
@@ -1,9 +0,0 @@
-<title>
-    @hasSection('title')
-        {{ Rapidez::config('design/head/title_prefix') }}
-        @yield('title')
-        {{ Rapidez::config('design/head/title_suffix') }}
-    @else
-        {{ Rapidez::config('design/head/default_title') }}
-    @endif
-</title>
