503178a136fe80f53daa35b2b8ca3232e0fbd2d6
indykoning <15870933+indykoning@users.noreply.github.com>
2023-05-31 10:23:39 +0200

    Only refresh mask if it is missing (#269)

--- resources/views/layouts/partials/header/minicart.blade.php
+++ resources/views/layouts/partials/header/minicart.blade.php
@@ -3,7 +3,7 @@
         <div v-if="hasItems" v-on-click-away="close" slot-scope="{toggle, close, isOpen}">
             <button class="flex my-1 focus:outline-none" v-on:click="toggle">
                 <x-heroicon-o-shopping-cart class="h-6 w-6"/>
-                <span class="bg-primary rounded-full w-6 h-6 text-white text-center">@{{ Math.round(cart.items_qty) }}</span>
+                <span class="bg-primary rounded-full w-6 h-6 text-white text-center" dusk="minicart-count">@{{ Math.round(cart.items_qty) }}</span>
             </button>
             <div v-if="isOpen" class="absolute right-0 bg-white border shadow rounded p-3 mr-1 {{ config('rapidez.z-indexes.header-dropdowns') }}">
                 <table class="mb-3">
