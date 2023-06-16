5f20c491a0c1677c72e09cea34ff7edb4d42bf2e
Jade Geels <a@jgw.tf>
2023-06-16 09:56:58 +0200

    Generalized functionality for various indexer commands (#254)

---
+++ src/Commands/ElasticsearchIndexCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Rapidez\Core\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Rapidez\Core\Facades\Rapidez;
+use Rapidez\Core\Models\Store;
+
+abstract class ElasticsearchIndexCommand extends Command
+{
+    public ElasticsearchIndexer $indexer;
+
+    public function __construct(ElasticsearchIndexer $indexer)
+    {
+        parent::__construct();
+        $this->indexer = $indexer;
+    }
+
+    public function indexAllStores(string $indexName, callable|iterable $items, callable|array $mapping, callable|string $id = 'id'): void
+    {
+        $stores = Rapidez::getStores();
+        foreach ($stores as $store) {
+            $this->indexStore($store, $indexName, $items, $mapping, $id);
+        }
+    }
+
+    public function indexStore(Store|array $store, string $indexName, callable|iterable $items, callable|array $mapping, callable|string $id = 'id'): void
+    {
+        $this->line('Indexing `'.$indexName.'` for store '.$store['name']);
+
+        try {
+            $this->prepareIndexerWithStore($store, $indexName);
+            $this->indexer->index($this->dataFrom($items), $mapping, $id);
+            $this->indexer->finish();
+        } catch (Exception $e) {
+            $this->indexer->abort();
+
+            throw $e;
+        }
+    }
+
+    public function prepareIndexerWithStore(Store|array $store, string $indexName, array $mapping = [], array $settings = []): void
+    {
+        Rapidez::setStore($store);
+        $this->indexer->prepare(config('rapidez.es_prefix').'_'.$indexName.'_'.$store['store_id'], $mapping, $settings);
+    }
+
+    public function dataFrom(callable|iterable $items)
+    {
+        return value($items, config()->get('rapidez.store_code'));
+    }
+}
--- src/Models/Store.php
+++ src/Models/Store.php
@@ -14,12 +14,13 @@ class Store extends Model
 
     protected $primaryKey = 'store_id';
 
-    public static function getCachedWhere(callable $callback): array
+    public static function getCached(): array
     {
         if (! $stores = config('cache.app.stores')) {
             $stores = Cache::rememberForever('stores', function () {
                 return self::select([
                     'store_id',
+                    'store.name',
                     'store.code',
                     'store.website_id',
                     'store_group.root_category_id',
@@ -28,6 +29,13 @@ class Store extends Model
             });
             config(['cache.app.stores' => $stores]);
         }
+
+        return $stores;
+    }
+
+    public static function getCachedWhere(callable $callback): array
+    {
+        $stores = static::getCached();
 
         $store = Arr::first($stores, function ($store) use ($callback) {
             return $callback($store);
