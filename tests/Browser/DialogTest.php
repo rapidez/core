924708244596e175c508d870583a2af06cd9cd7a
Roene <roene@justbetter.nl>
2023-07-06 15:31:09 +0200

    Added cookie notice + test

---
+++ tests/Browser/DialogTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rapidez\Core\Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Rapidez\Core\Tests\DuskTestCase;
+
+class DialogTest extends DuskTestCase
+{
+    /**
+     * @test
+     */
+    public function test()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/?show-cookie-notice')
+                    ->waitUntilIdle()
+                    ->assertSee('Accept cookies')
+                    ->waitForReload(function (Browser $browser) {
+                        $browser->press('Accept cookies');
+                    })
+                    ->assertScript("window.localStorage['cookie-notice']");
+        });
+    }
+}
