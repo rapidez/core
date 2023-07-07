98955fe3e5fbb0f124e790d298bf1954c40db966
indy koning <indy@justbetter.nl>
2023-07-07 14:14:22 +0200

    Update cart check for 1.x compatibility

--- resources/views/checkout/partials/sidebar.blade.php
+++ resources/views/checkout/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div v-if="cart" class="p-3 border rounded">
+<div v-if="cart?.entity_id" class="p-3 border rounded">
     <table class="mb-3">
         <tr class="py-3" v-for="item in cart.items">
             <td>@{{ item.name }}</td>
