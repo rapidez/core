11b191b99c1a12f9462c77cfcd656aa621fcb7ee
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-08-02 10:28:12 +0200

    Update dusk test & gitignore

---
+++ tests/Browser/HomepageTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Rapidez\Core\Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Rapidez\Core\Tests\DuskTestCase;
+
+class HomepageTest extends DuskTestCase
+{
+    /**
+     * @test
+     */
+    public function homepage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->resize(1920, 1080)->visit('/')->assertSee('All rights reserved.');
+        });
+    }
+}
