4e21f2e13e1c185eed69ece1103a66c0206d8dd2
Roy Duineveld <royduineveld@gmail.com>
2023-08-23 11:55:46 +0200

    Do not show removed product options

--- src/Models/QuoteItemOption.php
+++ src/Models/QuoteItemOption.php
@@ -27,6 +27,10 @@ class QuoteItemOption extends Model
                 'info_buyRequest' => json_decode($value),
                 'option_ids'      => explode(',', $value),
                 default           => (function () use ($value) {
+                    if (!$this->option) {
+                        return;
+                    }
+
                     if (in_array($this->option->type, ['drop_down', 'radio'])) {
                         return config('rapidez.models.product_option_type_value')::find($value)->title;
                     }
