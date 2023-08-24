28b5912d38ba051aaa3142f3705018195b49184e
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2023-08-24 12:48:38 +0200

    feedback fixes

--- resources/views/listing/filters.blade.php
+++ resources/views/listing/filters.blade.php
@@ -1,9 +1,7 @@
 <x-rapidez::slideover :mobile-only="true">
     <x-slot:button>
         <x-rapidez::button
-            class="mb-3 w-full md:hidden"
-            v-on:click="toggle"
-        >
+            v-on:click="toggle" class="mb-3 w-full md:hidden">
             @lang('Filters')
         </x-rapidez::button>
     </x-slot:button>
@@ -12,23 +10,17 @@
     </x-slot:title>
     {{-- On mobile the filters aren't immedately visible so we should defer loading --}}
     <lazy>
-        <template>
-            <div class="lg:pr-4">
-                @include('rapidez::listing.partials.filter.selected')
-                @include('rapidez::listing.partials.filter.category')
-                <template v-for="filter in filters">
-                    @include('rapidez::listing.partials.filter.price')
-                    @include('rapidez::listing.partials.filter.swatch')
-                    @include('rapidez::listing.partials.filter.boolean')
-                    @include('rapidez::listing.partials.filter.select')
-                </template>
-            </div>
+        @include('rapidez::listing.partials.filter.selected')
+        @include('rapidez::listing.partials.filter.category')
+        <template v-for="filter in filters">
+            @include('rapidez::listing.partials.filter.price')
+            @include('rapidez::listing.partials.filter.swatch')
+            @include('rapidez::listing.partials.filter.boolean')
+            @include('rapidez::listing.partials.filter.select')
         </template>
     </lazy>
     <x-rapidez::button
-        class="w-full text-sm md:hidden"
-        v-on:click="toggle"
-    >
+        v-on:click="toggle" class="w-full text-sm md:hidden">
         @lang('Show results')
     </x-rapidez::button>
 </x-rapidez::slideover>
--- resources/views/product/partials/addtocart.blade.php
+++ resources/views/product/partials/addtocart.blade.php
@@ -6,7 +6,7 @@
         @else
             <div v-cloak v-for="(superAttribute, superAttributeId) in config.product.super_attributes">
                 <x-rapidez::label v-bind:for="'super_attribute_'+superAttributeId">@{{ superAttribute.label }}</x-rapidez::label>
-                <x-rapidez::select label="" v-bind:id="'super_attribute_'+superAttributeId" v-bind:name="superAttributeId" v-model="options[superAttributeId]" class="mb-3 block w-64">
+                <x-rapidez::select required label="" v-bind:id="'super_attribute_'+superAttributeId" v-bind:name="superAttributeId" v-model="options[superAttributeId]" class="mb-3 block w-64">
                     <option disabled selected hidden :value="undefined">@lang('Select') @{{ superAttribute.label.toLowerCase() }}</option>
                     <option
                         v-for="option in Object.values(config.product['super_'+superAttribute.code]).sort((a, b) => a.sort_order - b.sort_order)"
