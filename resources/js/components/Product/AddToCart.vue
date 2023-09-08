fe6e3a36120456f0944fb52007f5e7f9ba49b22d
indykoning <15870933+indykoning@users.noreply.github.com>
2023-09-08 14:26:55 +0200

    Expose entire Vue component through a scoped slot (#333)

--- resources/js/components/Product/AddToCart.vue
+++ resources/js/components/Product/AddToCart.vue
@@ -49,7 +49,7 @@ export default {
     },
 
     render() {
-        return this.$scopedSlots.default(Object.assign(this, { self: this }))
+        return this.$scopedSlots.default(this)
     },
 
     methods: {
