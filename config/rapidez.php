98c2a6fbc170739bf4f05056e7d9dbe63ade039b
indykoning <15870933+indykoning@users.noreply.github.com>
2023-09-22 12:00:29 +0200

    Implement healthchecks (#341)

--- config/rapidez.php
+++ config/rapidez.php
@@ -156,4 +156,13 @@ return [
     'additional_filters' => [
         // Attribute codes
     ],
+
+    'healthcheck' => [
+        // Authenticate using the function passed, if null we will check ips.
+        'auth'        => null,
+        'allowed_ips' => [
+            '127.0.0.1/8',
+            ...explode(',', env('HEALTHCHECK_ALLOWED_IPS', '')),
+        ],
+    ],
 ];
