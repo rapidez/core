b90a3ac840d9f9a991ac2e4ce45fd631f1f70809
Robin Mulder <robin@justbetter.nl>
2023-10-03 11:11:44 +0200

    firstOrCreate search_query item

--- src/Http/Controllers/SearchController.php
+++ src/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace Rapidez\Core\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
@@ -10,17 +11,19 @@ class SearchController
 {
     public function __invoke(Request $request)
     {
-        if ($searchQuery = Cache::rememberForever('search.query.' . Str::slug($request->q), function () use ($request) {
+        $query = Str::lower($request->q);
+        if ($searchQuery = Cache::remember('search.query.' . Str::slug($query), Carbon::now()->addHour(), function () use ($query) {
             $searchQueryModel = config('rapidez.models.search_query');
 
-            return $searchQueryModel::where('query_text', $request->q)
-                ->where('store_id', config('rapidez.store'))
-                ->whereNotNull('redirect')
-                ->first();
+            return $searchQueryModel::firstOrCreate(
+                ['query_text' => $query],
+                ['store_id' => config('rapidez.store')]
+            );
         })) {
             $searchQuery->increment('popularity');
-
-            return redirect($searchQuery->redirect, 301);
+            if ($searchQuery->redirect) {
+                return redirect($searchQuery->redirect, 301);
+            }
         }
 
         return view('rapidez::search.overview');
