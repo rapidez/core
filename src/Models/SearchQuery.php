78c5ee4d516418f0ce7ac1d482e1286b7c6c0891
royduin <royduin@users.noreply.github.com>
2023-10-11 15:04:19 +0000

    Apply fixes from Duster

--- src/Models/SearchQuery.php
+++ src/Models/SearchQuery.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class SearchQuery extends Model
 {
     const CREATED_AT = null;
+
     protected $table = 'search_query';
 
     protected $primaryKey = 'query_id';
