2d64e152dea6185b2f6cb600b240d854e7e2088f
Roy Duineveld <royduineveld@gmail.com>
2023-10-11 17:02:09 +0200

    Update src/Http/Controllers/SearchController.php

--- src/Http/Controllers/SearchController.php
+++ src/Http/Controllers/SearchController.php
@@ -9,17 +9,10 @@ class SearchController
 {
     public function __invoke(Request $request)
     {
-        $query = Str::lower($request->q);
-        $searchQueryModel = config('rapidez.models.search_query');
-        $searchQuery = $searchQueryModel::firstOrNew(
-            [
-                'query_text' => $query,
-                'store_id'   => config('rapidez.store'),
-            ],
-            [
-                'popularity' => 1,
-            ]
-        );
+        $searchQuery = config('rapidez.models.search_query')::firstOrNew([
+            'query_text' => Str::lower($request->q),
+            'store_id'   => config('rapidez.store'),
+        ], ['popularity' => 1]);
 
         if (! $searchQuery->exists) {
             $searchQuery->save();
@@ -28,6 +21,7 @@ class SearchController
         }
 
         $searchQuery->increment('popularity');
+
         if ($searchQuery->is_active === 1 && $searchQuery->redirect) {
             return redirect($searchQuery->redirect, 301);
         }
