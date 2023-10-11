68b75c8c9c0dbd8ff18771a03e03269543b3b330
Roy Duineveld <royduineveld@gmail.com>
2023-10-11 17:02:02 +0200

    Update src/Models/SearchQuery.php

--- src/Models/SearchQuery.php
+++ src/Models/SearchQuery.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class SearchQuery extends Model
 {
-    const CREATED_AT = null;
     protected $table = 'search_query';
 
     protected $primaryKey = 'query_id';
 
     protected $guarded = [];
+    
+    const CREATED_AT = null;
 }
