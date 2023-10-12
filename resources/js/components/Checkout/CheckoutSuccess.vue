793268a49fdb86b65dbadfb9ab096e4cc395ee3c
Jade Geels <jade@justbetter.nl>
2023-10-12 08:02:17 +0200

    Checkout success changes from checkout-theme

--- resources/js/components/Checkout/CheckoutSuccess.vue
+++ resources/js/components/Checkout/CheckoutSuccess.vue
@@ -39,6 +39,44 @@ export default {
                 .get(window.url('/api/order'), { headers: { Authorization: 'Bearer ' + (this.token || this.mask) } })
                 .then((response) => (this.order = response.data))
         },
+
+        serialize(address) {
+            return JSON.stringify({
+                firstname: address.firstname ?? '',
+                lastname: address.lastname ?? '',
+                postcode: address.postcode ?? '',
+                street: address.street ?? '',
+                city: address.city ?? '',
+                country_id: address.country_id ?? '',
+                telephone: address.telephone ?? '',
+            })
+        },
+        sameAddress(a1, a2) {
+            return this.serialize(a1) == this.serialize(a2)
+        },
+    },
+
+    computed: {
+        hideBilling() {
+            return this.shipping && this.billing && this.sameAddress(this.shipping, this.billing)
+        },
+        shipping() {
+            if (!this.order?.sales_order_addresses) {
+                return null
+            }
+            let shipping = this.order.sales_order_addresses.filter((e) => e.address_type == 'shipping')
+            return shipping.length > 1 ? null : shipping.at(-1)
+        },
+        billing() {
+            if (!this.order?.sales_order_addresses) {
+                return null
+            }
+            let billing = this.order.sales_order_addresses.filter((e) => e.address_type == 'billing')
+            return billing.at(-1)
+        },
+        items() {
+            return this.order.sales_order_items.filter((item) => !item.parent_item_id)
+        },
     },
 }
 </script>
--- resources/views/product/partials/options.blade.php
+++ resources/views/product/partials/options.blade.php
@@ -1,7 +1,9 @@
-<div class="flex flex-col space-y-3">
-    @foreach($product->options->sortBy('sort_order') as $option)
-        <div>
-            @include('rapidez::product.partials.options.'.$option->type_id)
-        </div>
-    @endforeach
-</div>
+@if($product->options->isNotEmpty())
+    <div class="flex flex-col space-y-3">
+        @foreach($product->options->sortBy('sort_order') as $option)
+            <div>
+                @include('rapidez::product.partials.options.'.$option->type_id)
+            </div>
+        @endforeach
+    </div>
+@endif
