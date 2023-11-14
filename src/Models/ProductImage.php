5999d7e7d415fff3ef60a6eba1cfc2c2fc6c2afc
indy koning <indy@justbetter.nl>
2023-11-14 13:49:40 +0100

    Add support for image disabled option

--- src/Models/ProductImage.php
+++ src/Models/ProductImage.php
@@ -2,9 +2,30 @@
 
 namespace Rapidez\Core\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
+
 class ProductImage extends Model
 {
     protected $table = 'catalog_product_entity_media_gallery';
 
     protected $primaryKey = 'value_id';
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(
+            'enabled',
+            fn (Builder $query) => $query
+                ->where($query->qualifyColumn('disabled'), 0)
+                ->whereHas(
+                    'productImageValue',
+                    fn($query) => $query
+                        ->where($query->qualifyColumn('disabled'), 0)
+                )
+        );
+    }
+
+    public function productImageValue()
+    {
+        return $this->belongsTo(config('rapidez.models.product_image_value'), 'value_id', 'value_id');
+    }
 }
---
+++ src/Models/Scopes/ForCurrentStoreWithoutLimitScope.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Rapidez\Core\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+/**
+ * Remove results from the default store when store view specific is found.
+ */
+class ForCurrentStoreWithoutLimitScope implements Scope
+{
+    public function __construct(public $uniquePerStoreKey, public $storeIdColumn = 'store_id')
+    {}
+
+    public function apply(Builder $query, Model $model)
+    {
+        if (!config('rapidez.store')) {
+            return $query
+                ->where($query->qualifyColumn($this->storeIdColumn), 0);
+        }
+
+        return $query
+            // Pre-filter results to be default and current store only.
+            ->whereIn($query->qualifyColumn($this->storeIdColumn), [0, config('rapidez.store')])
+            // Remove values from the default store where values for the current store exist.
+            ->where(fn($query) => $query
+                // Remove values where we already have values in the current store.
+                ->whereNotIn($query->qualifyColumn($this->uniquePerStoreKey), fn($query) => $query
+                    ->from($model->getTable())
+                    ->select($model->qualifyColumn($this->uniquePerStoreKey))
+                    ->whereColumn($model->qualifyColumn($this->uniquePerStoreKey), $model->qualifyColumn($this->uniquePerStoreKey))
+                    ->where($model->qualifyColumn($this->storeIdColumn), config('rapidez.store'))
+                )
+                // Unless the value IS the current store.
+                ->orWhere($query->qualifyColumn($this->storeIdColumn), config('rapidez.store'))
+            );
+    }
+}
