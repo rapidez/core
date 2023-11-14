da372174f53652c228c2a34ef196b3727bb7741b
Roy Duineveld <royduineveld@gmail.com>
2023-11-14 13:15:44 +0100

    Update resources/views/product/partials/microdata.blade.php

--- resources/views/product/partials/microdata.blade.php
+++ resources/views/product/partials/microdata.blade.php
@@ -8,7 +8,7 @@
 <div itemprop="offers" itemtype="https://schema.org/Offer" itemscope>
     <meta itemprop="availability" content="https://schema.org/{{ $product->in_stock ? 'InStock' : 'OutOfStock' }}" />
     <meta itemprop="priceCurrency" content="@config('currency/options/default')" />
-    <meta itemprop="price" content="{{ round($product->price, 2) }}" />
+    <meta itemprop="price" content="{{ round($product->special_price ?: $product->price, 2) }}" />
     <meta itemprop="url" content="{{ url($product->url) }}" />
     @if($product->special_to_date && $product->special_to_date > now()->toDateTimeString())
         <meta itemprop="priceValidUntil" content="{{ $product->special_to_date }}" />
