66245fe4ebc2da900af0342f9271a165fd7d3937
indy koning <indy@justbetter.nl>
2023-12-05 11:40:47 +0100

    Wait longer for cart-content to show

--- tests/Browser/CartTest.php
+++ tests/Browser/CartTest.php
@@ -15,7 +15,7 @@ class CartTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $this->addProduct($browser, $this->testProduct->url)
                 ->visit('/cart')
-                ->waitFor('@cart-content')
+                ->waitFor('@cart-content', 15)
                 ->waitUntilIdle()
                 ->assertSee($this->testProduct->name);
         });
@@ -41,7 +41,7 @@ class CartTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/cart')
-                ->waitFor('@cart-content')
+                ->waitFor('@cart-content', 15)
                 ->waitUntilIdle()
                 ->type('@qty-0', 5)
                 ->click('@item-update-0')
@@ -57,7 +57,7 @@ class CartTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/cart')
-                ->waitFor('@cart-content')
+                ->waitFor('@cart-content', 15)
                 ->waitUntilIdle()
                 ->click('@item-delete-0')
                 ->waitUntilIdle()
