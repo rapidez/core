d4044a5780f7e55c22639cdccae82fe6ca71bd5b
Jade Geels <jade@justbetter.nl>
2024-02-14 10:51:10 +0100

    Fix overrideables & twMerge

--- resources/views/components/radio.blade.php
+++ resources/views/components/radio.blade.php
@@ -1,5 +1,12 @@
+@props(['id' => null, 'name' => null])
+
 <label class="flex items-center">
-    <input type="radio" {{ $attributes->merge(['class' => 'focus:ring-neutral h-4 w-4 text-neutral border']) }}>
+    <input
+        type="radio"
+        {{ $attributes->twMerge('focus:ring-neutral h-4 w-4 text-neutral border') }}
+        id="{{ $id ?? $name }}"
+        name="{{ $name ?? $id }}"
+    >
     <div class="ml-2 text-gray-700">
         {{ $slot }}
     </div>
