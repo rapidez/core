2fdccbceae14ef384915c5f2a7b724b660b29815
Jade Geels <jade@justbetter.nl>
2024-03-06 13:18:44 +0100

    Fix radio & checkbox

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,13 +1,8 @@
 @props(['id' => null, 'name' => null])
 
-<label class="flex items-center">
-    <input
-        type="checkbox"
-        {{ $attributes->twMerge('focus:ring-neutral h-4 w-4 text-neutral border rounded') }}
-        id="{{ $id ?? $name }}"
-        name="{{ $name ?? $id }}"
-    >
-    <div class="ml-2 text-gray-600">
-        {{ $slot }}
-    </div>
-</label>
+<input
+    type="checkbox"
+    {{ $attributes->twMerge('focus:ring-neutral h-4 w-4 text-neutral border rounded mr-2') }}
+    id="{{ $id ?? $name }}"
+    name="{{ $name ?? $id }}"
+>
