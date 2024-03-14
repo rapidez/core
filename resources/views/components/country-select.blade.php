0f8dd2bcbaa0f8e46f3dc0eb211c82905b1107f9
Roy Duineveld <royduineveld@gmail.com>
2024-03-14 13:19:03 +0100

    Update resources/views/components/country-select.blade.php
    Co-authored-by: Jade Geels <jade@justbetter.nl>

--- resources/views/components/country-select.blade.php
+++ resources/views/components/country-select.blade.php
@@ -1,6 +1,6 @@
 <graphql query="{ countries { two_letter_abbreviation full_name_locale } }" cache="countries">
     <div v-if="data" slot-scope="{ data }">
-        <x-rapidez::input.select {{ $attributes }}>
+        <x-rapidez::input.select :$attributes>
             <option v-for="country in data.countries" :value="country.two_letter_abbreviation.toUpperCase()">
                 @{{ country.full_name_locale }}
             </option>
