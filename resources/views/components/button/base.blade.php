c299d9dcac9330f6b7c99710c4149793ce159a08
Roene <roene@justbetter.nl>
2024-03-27 14:40:37 +0100

    Removed classes from base button | Placed paddings on buttons where needed

--- resources/views/components/button/base.blade.php
+++ resources/views/components/button/base.blade.php
@@ -8,7 +8,7 @@
 <x-tag
     is="{{ $tag }}"
     {{ $attributes->merge([
-        'class' => 'flex items-center justify-center font-semibold py-2 px-4 border rounded disabled:opacity-50 disabled:cursor-not-allowed hover:opacity-75 whitespace-nowrap transition',
+        'class' => 'inline-flex items-center justify-center font-semibold gap-x-2 transition disabled:opacity-50 disabled:cursor-not-allowed',
         ':disabled' => $attributes->has('href') || $attributes->has(':href') || !$disableWhenLoading ? null : '$root.loading' ]) }}
 >
     {{ $slot }}
--- resources/views/components/button/primary.blade.php
+++ resources/views/components/button/primary.blade.php
@@ -1,3 +1,3 @@
-<x-rapidez::button.base {{ $attributes->class('bg-primary text-primary-text') }}>
+<x-rapidez::button.base {{ $attributes->class('bg-primary text-primary-text py-2 px-4 hover:opacity-75') }}>
     {{ $slot }}
 </x-rapidez::button.base>
