6f54a95e37cb039d09903eb2dfa1ab1141c9a5a5
Jade Geels <jade@justbetter.nl>
2024-04-09 09:48:50 +0200

    Fix :key

--- resources/views/layouts/partials/header/autocomplete/products.blade.php
+++ resources/views/layouts/partials/header/autocomplete/products.blade.php
@@ -2,7 +2,7 @@
     <li
         v-for="suggestion in suggestions"
         :key="suggestion.source._id">
-        <a :href="suggestion.source.url | url" class="flex flex-wrap flex-1" key="suggestion.source._id">
+        <a :href="suggestion.source.url | url" class="flex flex-wrap flex-1" :key="suggestion.source._id">
             <img :src="'/storage/{{ config('rapidez.store') }}/resizes/80x80/magento/catalog/product' + suggestion.source.thumbnail + '.webp'" class="self-center object-contain w-14 aspect-square" />
             <div class="flex flex-1 flex-wrap px-2">
                 <strong class="hyphens block w-full" v-html="highlight(suggestion, 'name')"></strong>
