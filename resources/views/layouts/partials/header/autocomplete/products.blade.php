fecb509784baf1271663244e0d9b25fb12304023
Jade Geels <jade@justbetter.nl>
2024-04-16 09:18:19 +0200

    Remove unnecessary extra key

--- resources/views/layouts/partials/header/autocomplete/products.blade.php
+++ resources/views/layouts/partials/header/autocomplete/products.blade.php
@@ -1,8 +1,6 @@
 <ul class="gap-5 grid md:grid-cols-2">
-    <li
-        v-for="suggestion in suggestions"
-        :key="suggestion.source._id">
-        <a :href="suggestion.source.url | url" class="flex flex-wrap flex-1" :key="suggestion.source._id">
+    <li v-for="suggestion in suggestions" :key="suggestion.source._id">
+        <a :href="suggestion.source.url | url" class="flex flex-wrap flex-1">
             <img :src="'/storage/{{ config('rapidez.store') }}/resizes/80x80/magento/catalog/product' + suggestion.source.thumbnail + '.webp'" class="self-center object-contain w-14 aspect-square" />
             <div class="flex flex-1 flex-wrap px-2">
                 <strong class="hyphens block w-full" v-html="highlight(suggestion, 'name')"></strong>
