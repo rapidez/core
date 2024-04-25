eabec7f263bbd81d8710394d7b9fecc80eec72a9
Jade Geels <jade@justbetter.nl>
2024-04-25 14:23:23 +0200

    Round prices before multiplying by qty

--- resources/js/components/Product/AddToCart.vue
+++ resources/js/components/Product/AddToCart.vue
@@ -136,8 +136,8 @@ export default {
         },
 
         calculatePrices: function () {
-            this.price = parseFloat(this.simpleProduct.price) + this.priceAddition(this.simpleProduct.price)
-            this.specialPrice = parseFloat(this.simpleProduct.special_price) + this.priceAddition(this.simpleProduct.special_price)
+            this.price = Math.round((parseFloat(this.simpleProduct.price) + this.priceAddition(this.simpleProduct.price)) * 100) / 100
+            this.specialPrice = Math.round((parseFloat(this.simpleProduct.special_price) + this.priceAddition(this.simpleProduct.special_price)) * 100) / 100
         },
 
         getOptions: function (superAttributeCode) {
