45ed487dddeec22b162b9173a30c2613f8ce5c51
Jade Geels <jade@justbetter.nl>
2024-05-01 13:12:40 +0200

    Add mobile slideover documentation

--- resources/views/components/slideover/mobile.blade.php
+++ resources/views/components/slideover/mobile.blade.php
@@ -1,1 +1,5 @@
+{{--
+    This component adds some styling on the slideover to hide the slideover on desktop and placing the content on the page itself.
+    By using this, you have the ability to have something that becomes a slider on mobile.
+--}}
 @include('rapidez::components.slideover.index', ['attributes' => $attributes->class('lg:contents [&>.slideover-wrapper]:lg:contents [&>.slideover-header]:lg:hidden')])
