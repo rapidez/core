9421ff8d5044ed1ee22921ef5048e3a03ea0f3cf
Jade Geels <jade@justbetter.nl>
2024-05-01 13:01:47 +0200

    Add comment & fix togglescroll on child slideovers

--- resources/views/components/slideover/index.blade.php
+++ resources/views/components/slideover/index.blade.php
@@ -1,4 +1,20 @@
-@props(['id', 'title', 'hasParent' => false, 'position' => 'left', 'tag' => 'div', 'open' => false])
+{{--
+    This is a slideover component using pure CSS, by making use of checkboxes, and
+    potentially form reset logic for nested slideovers.
+
+    Properties:
+        has-parent:     Used for nested slideovers. Set this to true when this slideover is a child of another slideover.
+        id:             Unique identifier for the checkbox input used
+        open:           Use this property when you want the slideover to be open by default.
+        position:       Position of the slideover on the screen. Can be either `left` or `right`. Defaults to `left`.
+        tag:            The base tag of the slideover. Set this to `form` on the parent of a nested slideover. Defaults to `div`.
+        title:          A title that gets put into the header. Can be a slot or a property.
+
+    Slots:
+        headerbutton:   This slot can be defined if you want a custom button placed in the header instead of the left arrow.
+        title:          A title that gets put into the header. Can be a slot or a property.
+--}}
+@props(['id' => uniqid('slideover-'), 'title', 'hasParent' => false, 'position' => 'left', 'tag' => 'div', 'open' => false])
 @slots(['title', 'headerbutton'])
 
 @php
@@ -7,10 +23,12 @@
 @endphp
 
 <x-tag v-on:reset="toggleScroll(false)" :is="$tag">
-    <input @checked($open) id="{{ $id }}" v-on:change="toggleScroll($event.target.checked)" class="peer hidden" type="checkbox">
     <input id="{{ 'close-' . $id }}" class="hidden" type="reset">
     @if (!$hasParent)
+        <input @checked($open) id="{{ $id }}" v-on:change="toggleScroll($event.target.checked)" class="peer hidden" type="checkbox">
         <label for="{{ $closeId }}" class="pointer-events-none fixed inset-0 z-40 cursor-pointer bg-neutral/50 opacity-0 transition peer-checked:pointer-events-auto peer-checked:opacity-100"></label>
+    @else
+        <input @checked($open) id="{{ $id }}" class="peer hidden" type="checkbox">
     @endif
     <div {{ $attributes->class([
         'fixed inset-y-0 transition-all bg-white z-40 flex flex-col max-w-md w-full',
