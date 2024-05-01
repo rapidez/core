8070c9b28a7a16a879768bf75c148dbdd605466f
indykoning <15870933+indykoning@users.noreply.github.com>
2024-05-01 15:49:09 +0200

    Implement macroable on all Rapidez Models

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -3,9 +3,32 @@
 namespace Rapidez\Core\Models;
 
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Support\Traits\Macroable;
 use Rapidez\Core\Models\Traits\HasEventyGlobalScopeFilter;
 
 class Model extends BaseModel
 {
     use HasEventyGlobalScopeFilter;
+    use Macroable {
+        Macroable::__call as macroCall;
+        Macroable::__callStatic as macroCallStatic;
+    }
+
+    public function __call($method, $parameters)
+    {
+        if (static::hasMacro($method)) {
+            return static::macroCall($method, $parameters);
+        }
+
+        return parent::__call($method, $parameters);
+    }
+
+    public static function __callStatic($method, $parameters)
+    {
+        if (static::hasMacro($method)) {
+            return static::macroCallStatic($method, $parameters);
+        }
+
+        return parent::__callStatic($method, $parameters);
+    }
 }
