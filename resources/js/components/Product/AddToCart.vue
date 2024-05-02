7bb8068d7781ef86f1e04f5fd752ea442bbbfd7e
indykoning <15870933+indykoning@users.noreply.github.com>
2024-05-02 16:52:48 +0200

    Also calculate product price on selected product option changes (#485)

--- resources/js/components/Product/AddToCart.vue
+++ resources/js/components/Product/AddToCart.vue
@@ -183,21 +183,24 @@ export default {
         priceAddition: function (basePrice) {
             let addition = 0
 
-            Object.entries(this.customOptions).forEach(([key, val]) => {
-                if (!val) {
-                    return
-                }
+            let optionEntries = Object.entries(this.customOptions)
+            let selectedOptionEntries = Object.entries(this.customSelectedOptions)
 
-                let option = this.product.options.find((option) => option.option_id == key)
-                let optionPrice = ['drop_down', 'radio'].includes(option.type)
-                    ? option.values.find((value) => value.option_type_id == val).price
-                    : option.price
+            ;[...optionEntries, ...selectedOptionEntries].forEach(([key, vals]) => {
+                ;[vals].flat().forEach((val) => {
+                    if (!val) {
+                        return
+                    }
 
-                if (optionPrice.price_type == 'fixed') {
-                    addition += parseFloat(optionPrice.price)
-                } else {
-                    addition += (parseFloat(basePrice) * parseFloat(optionPrice.price)) / 100
-                }
+                    let option = this.product.options.find((option) => option.option_id == key)
+                    let optionPrice = option.price || option.values?.find((value) => value.option_type_id == val)?.price
+
+                    if (optionPrice.price_type == 'fixed') {
+                        addition += parseFloat(optionPrice.price)
+                    } else {
+                        addition += (parseFloat(basePrice) * parseFloat(optionPrice.price)) / 100
+                    }
+                })
             })
 
             return addition
@@ -324,6 +327,12 @@ export default {
 
     watch: {
         customOptions: {
+            handler() {
+                this.calculatePrices()
+            },
+            deep: true,
+        },
+        customSelectedOptions: {
             handler() {
                 this.calculatePrices()
             },
