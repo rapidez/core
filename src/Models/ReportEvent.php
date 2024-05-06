3c07d025707c3f9a7b1a45387cdfe65aef957c6c
Bob <bob@justbetter.nl>
2024-05-06 13:21:51 +0200

    Insert the Report Event into the magento database

---
+++ src/Models/ReportEvent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Rapidez\Core\Models;
+
+class ReportEvent extends Model
+{
+    protected $table = 'report_event';
+
+    protected $primaryKey = 'event_id';
+
+    protected $guarded = ['event_id'];
+
+    const CREATED_AT = 'logged_at';
+
+    const UPDATED_AT = null;
+}
