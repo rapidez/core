c846f8a165accb01a0563d61e96dfe6c55df4ea9
Roene Verbeek <85165259+Roene-JustBetter@users.noreply.github.com>
2024-05-08 15:21:37 +0200

    Rearranged the button classes (#460)

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -1,3 +1,3 @@
-<x-rapidez::button.base {{ $attributes->class('bg-inactive text-white') }}>
+<x-rapidez::button.base {{ $attributes->class('bg-inactive text-white py-2 px-4 hover:opacity-75') }}>
     {{ $slot }}
 </x-rapidez::button.base>
