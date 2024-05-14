44f81f370083652c5b5befcd57c1b9599a39cc61
Jade Geels <jade@justbetter.nl>
2024-05-14 15:03:50 +0000

    Checkout agreements from the database instead of GraphQL (#492)

--- resources/views/checkout/steps/payment.blade.php
+++ resources/views/checkout/steps/payment.blade.php
@@ -1,3 +1,5 @@
+@php($checkoutAgreements = \Rapidez\Core\Models\CheckoutAgreement::all())
+
 <h1 class="font-bold text-4xl mb-5">@lang('Payment method')</h1>
 <form class="bg-highlight p-8 rounded mt-6" v-on:submit.prevent="save(['payment_method'], 4)">
     <div class="my-2 border bg-white p-4 rounded" v-for="(method, index) in checkout.payment_methods">
@@ -18,38 +20,47 @@
         </x-rapidez::radio>
     </div>
 
-    <graphql query="{ checkoutAgreements { agreement_id name checkbox_text content is_html mode } }" cache="checkout.agreements">
-        <div slot-scope="{ data }" v-if="data" class="mt-5">
-            <div v-for="agreement in data.checkoutAgreements" :key="agreement.agreement_id">
-                <x-rapidez::slideover position="right" v-bind:id="agreement.agreement_id + '_agreement'">
-                    <x-slot:title>
-                        @{{ agreement.name }}
-                    </x-slot:title>
-                    <div v-if="agreement.is_html" v-html="agreement.content"></div>
-                    <div v-else v-text="agreement.content" class="whitespace-pre-line"></div>
+    @if (count($checkoutAgreements))
+        <div>
+            @foreach ($checkoutAgreements as $agreement)
+                <x-rapidez::slideover position="right" id="{{ $agreement->agreement_id }}_agreement">
+                    <x-slot name="title">
+                        {{ $agreement->name }}
+                    </x-slot>
+
+                    @if ($agreement->is_html)
+                        <div>
+                            {!! $agreement->content !!}
+                        </div>
+                    @else
+                        <div class="whitespace-pre-line">
+                            {{ $agreement->content }}
+                        </div>
+                    @endif
                 </x-rapidez::slideover>
 
-                <template v-if="agreement.mode == 'AUTO'">
-                    <label class="text-gray-700 cursor-pointer underline hover:no-underline" v-bind:for="agreement.agreement_id + '_agreement'">
-                        @{{ agreement.checkbox_text }}
+                @if ($agreement->mode == 'AUTO')
+                    <label class="text-gray-700 cursor-pointer underline hover:no-underline" for="{{ $agreement->agreement_id }}_agreement">
+                        {{ $agreement->checkbox_text }}
                     </label>
-                </template>
-                <div v-else>
-                    <x-rapidez::checkbox
-                        name="agreement_ids[]"
-                        v-bind:value="agreement.agreement_id"
-                        v-model="checkout.agreement_ids"
-                        dusk="agreements"
-                        required
-                    >
-                        <label class="text-gray-700 cursor-pointer underline hover:no-underline" v-bind:for="agreement.agreement_id + '_agreement'">
-                            @{{ agreement.checkbox_text }}
-                        </label>
-                    </x-rapidez::checkbox>
-                </div>
-            </div>
+                @else
+                    <div>
+                        <x-rapidez::checkbox
+                            name="agreement_ids[]"
+                            :value="$agreement->agreement_id"
+                            v-model="checkout.agreement_ids"
+                            dusk="agreements"
+                            required
+                        >
+                            <label class="text-gray-700 cursor-pointer underline hover:no-underline" for="{{ $agreement->agreement_id }}_agreement">
+                                {{ $agreement->checkbox_text }}
+                            </label>
+                        </x-rapidez::checkbox>
+                    </div>
+                @endif
+            @endforeach
         </div>
-    </graphql>
+    @endif
 
     <x-rapidez::button type="submit" class="mt-5" dusk="continue">
         @lang('Place order')
