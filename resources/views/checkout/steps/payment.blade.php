02ba90e17a3030738034c652c48dc56cc5b63f93
Jade Geels <jade@justbetter.nl>
2024-05-14 06:34:05 +0000

    Refactored the slideover component (#464)

--- resources/views/checkout/steps/payment.blade.php
+++ resources/views/checkout/steps/payment.blade.php
@@ -18,34 +18,35 @@
         </x-rapidez::radio>
     </div>
 
-    <graphql query="{ checkoutAgreements { agreement_id name checkbox_text content is_html mode } }">
-        <div v-if="data" slot-scope="{ data }" class="mt-5">
-            <div v-for="agreement in data.checkoutAgreements">
-                <x-rapidez::slideover>
-                    <x-slot name="button">
-                        <a class="text-gray-700" href="#" v-on:click.prevent="toggle" v-if="agreement.mode == 'AUTO'">
-                            @{{ agreement.checkbox_text }}
-                        </a>
-                        <div v-else>
-                            <x-rapidez::checkbox
-                                name="agreement_ids[]"
-                                v-bind:value="agreement.agreement_id"
-                                v-model="checkout.agreement_ids"
-                                dusk="agreements"
-                                required
-                            >
-                                <a href="#" v-on:click.prevent="toggle">@{{ agreement.checkbox_text }}</a>
-                            </x-rapidez::checkbox>
-                        </div>
-                    </x-slot>
-
-                    <x-slot name="title">
+    <graphql query="{ checkoutAgreements { agreement_id name checkbox_text content is_html mode } }" cache="checkout.agreements">
+        <div slot-scope="{ data }" v-if="data" class="mt-5">
+            <div v-for="agreement in data.checkoutAgreements" :key="agreement.agreement_id">
+                <x-rapidez::slideover position="right" v-bind:id="agreement.agreement_id + '_agreement'">
+                    <x-slot:title>
                         @{{ agreement.name }}
-                    </x-slot>
-
+                    </x-slot:title>
                     <div v-if="agreement.is_html" v-html="agreement.content"></div>
                     <div v-else v-text="agreement.content" class="whitespace-pre-line"></div>
                 </x-rapidez::slideover>
+
+                <template v-if="agreement.mode == 'AUTO'">
+                    <label class="text-gray-700 cursor-pointer underline hover:no-underline" v-bind:for="agreement.agreement_id + '_agreement'">
+                        @{{ agreement.checkbox_text }}
+                    </label>
+                </template>
+                <div v-else>
+                    <x-rapidez::checkbox
+                        name="agreement_ids[]"
+                        v-bind:value="agreement.agreement_id"
+                        v-model="checkout.agreement_ids"
+                        dusk="agreements"
+                        required
+                    >
+                        <label class="text-gray-700 cursor-pointer underline hover:no-underline" v-bind:for="agreement.agreement_id + '_agreement'">
+                            @{{ agreement.checkbox_text }}
+                        </label>
+                    </x-rapidez::checkbox>
+                </div>
             </div>
         </div>
     </graphql>
