8e67b2227a38cce059cef341415a7a98c737e474
Roy Duineveld <royduineveld@gmail.com>
2024-05-22 15:47:28 +0200

    Correct rapidez/menu config path location

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
 
         './config/rapidez/frontend.php',
         './vendor/rapidez/core/config/rapidez/frontend.php',
-        './vendor/rapidez/menu/config/menu.php',
+        './config/rapidez/menu.php',
+        './vendor/rapidez/menu/config/rapidez/menu.php',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/tailwind.blade.php',
     ],
     theme: {
