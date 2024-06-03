bd5047db74c15a3fc2713dda3c8f9b587b0a4485
indykoning <15870933+indykoning@users.noreply.github.com>
2024-06-03 13:24:42 +0200

    Add dusk selectors for coupons

--- resources/views/cart/coupon/list.blade.php
+++ resources/views/cart/coupon/list.blade.php
@@ -7,7 +7,7 @@
         v-slot="{ mutate }"
     >
         <div class="flex">
-            <button v-on:click="mutate">
+            <button v-on:click="mutate" v-bind:dusk="'remove-coupon-' + coupon.code">
                 <x-heroicon-s-x-mark class="h-4 w-4 text-black-400"/>
             </button>
             @{{ coupon.code }}
