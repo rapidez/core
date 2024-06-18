cda0bd974c38763f0df39f6f4fcaac013eb9239c
Bob <bob@justbetter.nl>
2024-06-18 07:37:58 +0200

    feedback

--- resources/views/cart/item.blade.php
+++ resources/views/cart/item.blade.php
@@ -28,11 +28,11 @@
                         </template>
                     </div>
                     @include('rapidez::cart.item.remove')
-                    <div v-if="item.backorder_count" class="flex gap-2">
+                    <div v-if="item.qty_backordered" class="flex gap-2">
                         <x-heroicon-o-exclamation-circle class="mt-px w-5" />
                         <span>
-                            <template v-if="item.backorder_count == item.qty">
-                                @lang(':count of the requested quantity will be backordered', ['count' => '@{{ item.backorder_count }}'])
+                            <template v-if="item.qty_backordered > item.qty">
+                                @lang(':count of the requested quantity will be backordered', ['count' => '@{{ item.qty_backordered }}'])
                             </template>
                             <template v-else>
                                 @lang('This product will be backordered')
