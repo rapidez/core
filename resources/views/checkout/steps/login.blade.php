6ee93a1877d5d529999910774a5ef711627dec6e
indy koning <indy@justbetter.nl>
2024-06-18 13:38:35 +0200

    Fix template

--- resources/views/checkout/steps/login.blade.php
+++ resources/views/checkout/steps/login.blade.php
@@ -1,59 +1,52 @@
 <checkout-login v-slot="checkoutLogin">
     <fieldset data-function="go" v-cloak>
-            <div>
-                <x-rapidez::input
-                    label="Email"
-                    name="email"
-                    type="email"
-                    v-model="checkoutLogin.email"
-                    v-bind:disabled="loggedIn"
-                    required
-                />
-                <template v-if="!loggedIn && (!checkoutLogin.isEmailAvailable || checkoutLogin.createAccount)">
-                    <x-rapidez::input
-                        label="Password"
-                        name="password"
-                        type="password"
-                        v-model="checkoutLogin.password"
-                        required
-                    />
-                </template>
-                @if (App::providerIsLoaded('Rapidez\Account\AccountServiceProvider'))
-                    <a href="{{ route('account.forgotpassword') }}" class="inline-block text-sm hover:underline mt-5" v-if="!checkoutLogin.isEmailAvailable">
-                        @lang('Forgot your password?')
-                    </a>
-                @endif
-                <template v-if="!loggedIn && checkoutLogin.createAccount">
-                    <x-rapidez::input
-                        label="Repeat password"
-                        name="password_repeat"
-                        type="password"
-                        v-model="checkoutLogin.password_repeat"
-                        required
-                    />
-                    <x-rapidez::input
-                        label="Firstname"
-                        name="firstname"
-                        type="text"
-                        v-model="checkoutLogin.firstname"
-                        required
-                    />
-                    <x-rapidez::input
-                        label="Lastname"
-                        name="lastname"
-                        type="text"
-                        v-model="checkoutLogin.lastname"
-                        required
-                    />
-                </template>
-                <template v-if="!loggedIn && checkoutLogin.isEmailAvailable">
-                    <x-rapidez::checkbox v-model="checkoutLogin.createAccount">@lang('Create an account')</x-rapidez::checkbox>
-                </template>
-            </div>
-
-
-        <x-rapidez::button type="submit">
-            @lang('Next')
-        </x-rapidez::button>
-    </form>
+        <x-rapidez::input
+            label="Email"
+            name="email"
+            type="email"
+            v-model="checkoutLogin.email"
+            v-bind:disabled="loggedIn"
+            required
+        />
+        <template v-if="!loggedIn && (!checkoutLogin.isEmailAvailable || checkoutLogin.createAccount)">
+            <x-rapidez::input
+                label="Password"
+                name="password"
+                type="password"
+                v-model="checkoutLogin.password"
+                required
+            />
+        </template>
+        @if (App::providerIsLoaded('Rapidez\Account\AccountServiceProvider'))
+            <a href="{{ route('account.forgotpassword') }}" class="inline-block text-sm hover:underline mt-5" v-if="!checkoutLogin.isEmailAvailable">
+                @lang('Forgot your password?')
+            </a>
+        @endif
+        <template v-if="!loggedIn && checkoutLogin.createAccount">
+            <x-rapidez::input
+                label="Repeat password"
+                name="password_repeat"
+                type="password"
+                v-model="checkoutLogin.password_repeat"
+                required
+            />
+            <x-rapidez::input
+                label="Firstname"
+                name="firstname"
+                type="text"
+                v-model="checkoutLogin.firstname"
+                required
+            />
+            <x-rapidez::input
+                label="Lastname"
+                name="lastname"
+                type="text"
+                v-model="checkoutLogin.lastname"
+                required
+            />
+        </template>
+        <template v-if="!loggedIn && checkoutLogin.isEmailAvailable">
+            <x-rapidez::checkbox v-model="checkoutLogin.createAccount">@lang('Create an account')</x-rapidez::checkbox>
+        </template>
+    </fieldset>
 </checkout-login>
