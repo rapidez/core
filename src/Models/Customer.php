4e1196b12cf98992d350c70c9eda98e34e4393bb
Jade Geels <jade@justbetter.nl>
2024-07-08 16:22:19 +0200

    Add customer group relation

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -27,6 +27,11 @@ class Customer extends Model implements AuthenticatableContract
         return $this->hasMany(config('rapidez.models.oauth_token'), 'customer_id');
     }
 
+    public function group()
+    {
+        return $this->belongsTo(CustomerGroup::class, 'group_id');
+    }
+
     public function getRememberTokenName()
     {
         return '';
