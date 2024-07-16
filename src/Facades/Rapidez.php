f3737420ef2b2c71f3fe8c3b3fb520d16bd4abc7
Roene-JustBetter <Roene-JustBetter@users.noreply.github.com>
2024-07-16 09:14:02 +0000

    Apply fixes from Duster

--- src/Facades/Rapidez.php
+++ src/Facades/Rapidez.php
@@ -5,15 +5,15 @@ namespace Rapidez\Core\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static self       addFallbackRoute($action, $position = 9999)
- * @method static self       removeFallbackRoute($action)
+ * @method static self addFallbackRoute($action, $position = 9999)
+ * @method static self removeFallbackRoute($action)
  * @method static Collection getAllFallbackRoutes()
- * @method static ?string    config(string $path, $default = null, bool $sensitive = false)
- * @method static ?string    content($content)
- * @method static object     fancyMagentoSyntaxDecoder(string $encodedString)
- * @method static array      getStores($storeId = null)
- * @method static array      getStore($storeId)
- * @method static void       setStore($store)
+ * @method static ?string config(string $path, $default = null, bool $sensitive = false)
+ * @method static ?string content($content)
+ * @method static object fancyMagentoSyntaxDecoder(string $encodedString)
+ * @method static array getStores($storeId = null)
+ * @method static array getStore($storeId)
+ * @method static void setStore($store)
  *
  * @see \Rapidez\Core\Rapidez
  */
