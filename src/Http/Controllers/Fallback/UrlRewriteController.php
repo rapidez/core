98638f95b85200ca0ac1d2c3ab7b52149efce664
Bob Wezelman <bob@justbetter.nl>
2024-07-23 14:36:24 +0200

    Redirect all redirect type rewrites (#534)

--- src/Http/Controllers/Fallback/UrlRewriteController.php
+++ src/Http/Controllers/Fallback/UrlRewriteController.php
@@ -13,6 +13,10 @@ class UrlRewriteController
             return;
         }
 
+        if ($rewrite->redirect_type !== 0) {
+            return redirect($rewrite->target_path, $rewrite->redirect_type ?: 302);
+        }
+
         if ($rewrite->entity_type == 'category') {
             $categoryController = config('rapidez.routing.controllers.category');
 
@@ -23,10 +27,6 @@ class UrlRewriteController
             $productController = config('rapidez.routing.controllers.product');
 
             return (new $productController)->show($rewrite->entity_id);
-        }
-
-        if ($rewrite->entity_type == 'custom') {
-            return redirect($rewrite->target_path, $rewrite->redirect_type ?: 302);
         }
     }
 }
