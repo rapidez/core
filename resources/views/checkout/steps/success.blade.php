ff02d97f20729ba513ac4fc866038583ebb6a823
indy koning <indy@justbetter.nl>
2024-08-07 17:27:49 +0200

    Add current order status

--- resources/views/checkout/steps/success.blade.php
+++ resources/views/checkout/steps/success.blade.php
@@ -1,11 +1,13 @@
 <checkout-success>
-    <template slot-scope="{ order, hideBilling, shipping, billing, items }">
+    <template slot-scope="{ order, refreshOrder, hideBilling, shipping, billing, items }">
         <div dusk="checkout-success" v-cloak v-if="order">
             <h1 class="font-bold text-4xl mb-5">@lang('Order placed succesfully')</h1>
             <div class="bg-highlight rounded p-8">
                 <p>@lang('We will get to work for you right away')</p>
                 <p>@lang('We will send a confirmation of your order to') <span class="font-bold">@{{ order.email }}</span></p>
+                <p>@lang('Your order is currently'): <span class="font-bold">@{{ order.status }}</span> <a class="inline-block" href="#" v-on:click.prevent="(e) => {e.target.classList.add('animate-spin'); refreshOrder().finally(() => e.target.classList.remove('animate-spin'))}">&#8635;</a></p>
             </div>
+
             <div class="mt-4">
                 <div
                     class="flex flex-wrap items-center mb-4 border-b pb-2"
