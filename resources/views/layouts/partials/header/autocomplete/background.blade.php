cafc6461aabf0d27ba134b642fc38f112f330fe7
Roene <roene@justbetter.nl>
2024-08-20 11:41:29 +0200

    Make resize larger | Fix comment

--- resources/views/layouts/partials/header/autocomplete/background.blade.php
+++ resources/views/layouts/partials/header/autocomplete/background.blade.php
@@ -1,3 +1,3 @@
-{{-- This is used for the hover state when you hover over category or producs --}}
+{{-- This is used for the hover state, it is used on all items in the autocomplete. --}}
 
 <div class="-z-10 bg-inactive-100 opacity-0 group-hover:opacity-100 transition absolute inset-y-0 -inset-x-6 pointer-events-none"></div>
--- resources/views/layouts/partials/header/autocomplete/products.blade.php
+++ resources/views/layouts/partials/header/autocomplete/products.blade.php
@@ -2,7 +2,7 @@
     <li v-for="suggestion in suggestions" :key="suggestion.source._id">
         <a :href="suggestion.source.url | url" class="group relative flex flex-wrap py-2">
             @include('rapidez::layouts.partials.header.autocomplete.background')
-            <img :src="'/storage/{{ config('rapidez.store') }}/resizes/80x80/magento/catalog/product' + suggestion.source.thumbnail + '.webp'" class="shrink-0 self-center object-contain w-14 h-20 mix-blend-multiply" />
+            <img :src="'/storage/{{ config('rapidez.store') }}/resizes/200/magento/catalog/product' + suggestion.source.thumbnail + '.webp'" class="shrink-0 self-center object-contain w-14 h-20 mix-blend-multiply" />
             <div class="flex flex-1 justify-center flex-col px-2">
                 <strong class="text-sm font-sans hyphens block w-full" v-html="highlight(suggestion, 'name')"></strong>
                 <div class="flex items-center gap-x-0.5">
