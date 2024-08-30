8ede01bdd986897ff126a5ae664b06a5f0134063
Roy Duineveld <royduineveld@gmail.com>
2024-08-30 14:47:23 +0200

    Redirect when the order can not be fetched from the success page

--- resources/js/components/Checkout/CheckoutSuccess.vue
+++ resources/js/components/Checkout/CheckoutSuccess.vue
@@ -47,7 +47,7 @@ export default {
                 {
                     headers: { Authorization: 'Bearer ' + (this.token || this.mask) },
                 },
-            )
+            ).catch((error) => Turbo.visit(window.url('/')))
         },
 
         serialize(address) {
