6e55e6b7bade93f221c3bb63ec5496c7295e63b4
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2024-09-06 14:39:13 +0200

    remove heading prop

--- resources/views/components/filter/heading.blade.php
+++ resources/views/components/filter/heading.blade.php
@@ -1,20 +1,14 @@
-@props(['heading'])
-
 <input class="peer hidden" type="checkbox" checked :set="id = Math.random().toString(36).slice(2)" :id="id" />
 
 <label class="flex cursor-pointer items-center justify-between gap-x-2 border-t pb-2.5 pt-4 text-neutral peer-checked:[&>.chevron]:rotate-180" :for="id">
     <span class="block font-sans text-base font-semibold text-neutral">
-        @isset($heading)
-            {{ $heading }}
-        @else
-            @{{ filter?.name?.replace('_', ' ') }}
-        @endisset
+        @{{ filter?.name?.replace('_', ' ') }}
     </span>
 
     <x-heroicon-o-chevron-down class="size-4 chevron transition" />
 </label>
-<div class="grid grid-rows-[0fr] transition-all peer-checked:grid-rows-[1fr] peer-checked:*:-my-1 peer-checked:*:py-1">
-    <div class="overflow-hidden px-1 -mx-1">
+<div class="peer-checked:*:-my-1 peer-checked:*:py-1 grid grid-rows-[0fr] transition-all peer-checked:grid-rows-[1fr]">
+    <div class="-mx-1 overflow-hidden px-1">
         {{ $slot }}
     </div>
 </div>
