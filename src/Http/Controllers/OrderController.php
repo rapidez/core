3b2d5863bab612295f206e43f93264db4238c8dd
indy koning <indy@justbetter.nl>
2024-09-24 12:04:20 +0200

    Add debugging on js side

--- src/Http/Controllers/OrderController.php
+++ src/Http/Controllers/OrderController.php
@@ -8,10 +8,6 @@ class OrderController
 {
     public function __invoke(Request $request)
     {
-        if (! $request->bearerToken()) {
-            \Illuminate\Support\Facades\Log::debug($request->headers->__toString());
-        }
-
         abort_unless($request->bearerToken(), 401);
 
         $salesOrder = config('rapidez.models.sales_order')::with('sales_order_addresses', 'sales_order_items.product', 'sales_order_payments')
