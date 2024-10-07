d46822a9db9dd95ea42a45480cfe6c30d5afa7a4
Bob <bob@justbetter.nl>
2024-10-07 15:34:15 +0200

    Moved x

--- resources/views/listing/partials/filter/selected.blade.php
+++ resources/views/listing/partials/filter/selected.blade.php
@@ -20,8 +20,8 @@
                     <div class="flex flex-wrap gap-2 relative cursor-pointer" v-for="filter in activeFilters">
                         <div v-on:click="setValue(filter.code, null)" class="flex justify-between items-center transition hover:opacity-80">
                             <span class="font-sans flex gap-1 p-1 items-center text-xs test-neutral rounded-lg bg-inactive-100">
-                                <x-heroicon-o-x-mark class="size-3 shrink-0 text-neutral"/>
                                 @{{ filter.value }}
+                                <x-heroicon-o-x-mark class="size-3 shrink-0 text-neutral"/>
                             </span>
                         </div>
                     </div>
