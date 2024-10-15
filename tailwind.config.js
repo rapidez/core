8b1620427c9f6f8291ef59324891df439691577d
jordythevulder <jordythevulder@users.noreply.github.com>
2024-10-15 09:03:54 +0000

    Apply fixes from Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,22 +22,22 @@ export default {
         extend: {
             colors: {
                 primary: {
-                    DEFAULT: 'rgb(var(--primary) / <alpha-value>)',     // 1st theme color - `bg-primary`
-                    text: 'rgb(var(--primary-text) / <alpha-value>)',   // Text color that goes onto the 1st theme color - `text-primary-text`
+                    DEFAULT: 'rgb(var(--primary) / <alpha-value>)', // 1st theme color - `bg-primary`
+                    text: 'rgb(var(--primary-text) / <alpha-value>)', // Text color that goes onto the 1st theme color - `text-primary-text`
                 },
                 secondary: {
-                    DEFAULT: 'rgb(var(--secondary) / <alpha-value>)',   // 2nd theme color - `bg-secondary`
+                    DEFAULT: 'rgb(var(--secondary) / <alpha-value>)', // 2nd theme color - `bg-secondary`
                     text: 'rgb(var(--secondary-text) / <alpha-value>)', // Text color that goes onto the 2nd theme color - `text-secondary-text`
                 },
-                default: { 
-                    DEFAULT: 'rgb(var(--default) / <alpha-value>)',     // Default text color - `text-default`
-                    100: 'rgb(var(--default-100) / <alpha-value>)',     // Default light background color - `bg-default-100`
-                    110: 'rgb(var(--default-110) / <alpha-value>)',     // Default border color - `border-default-110`
+                default: {
+                    DEFAULT: 'rgb(var(--default) / <alpha-value>)', // Default text color - `text-default`
+                    100: 'rgb(var(--default-100) / <alpha-value>)', // Default light background color - `bg-default-100`
+                    110: 'rgb(var(--default-110) / <alpha-value>)', // Default border color - `border-default-110`
                 },
-                inactive: 'rgb(var(--inactive) / <alpha-value>)',       // Inactive muted less prominent text color - `text-inactive`
-                enhanced: 'rgb(var(--enhanced) / <alpha-value>)',       // Color to stimulate anything conversion related - `bg-enhanced`
+                inactive: 'rgb(var(--inactive) / <alpha-value>)', // Inactive muted less prominent text color - `text-inactive`
+                enhanced: 'rgb(var(--enhanced) / <alpha-value>)', // Color to stimulate anything conversion related - `bg-enhanced`
             },
-            borderColor: 'rgb(var(--default-110) / <alpha-value>)',     // Assigning the default border color - `border`
+            borderColor: 'rgb(var(--default-110) / <alpha-value>)', // Assigning the default border color - `border`
         },
         container: {
             center: true,
