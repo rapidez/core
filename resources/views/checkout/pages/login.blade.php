c7d3edb9daa04dc362b3b8237dd66cfd3693733e
indy koning <indy@justbetter.nl>
2024-11-04 14:24:42 +0100

    Rename submit fieldsets function

--- resources/views/checkout/pages/login.blade.php
+++ resources/views/checkout/pages/login.blade.php
@@ -10,7 +10,7 @@
         <form
             v-if="hasCart"
             v-on:submit.prevent="(e) => {
-                submitFieldsets(e.target?.form ?? e.target)
+                submitPartials(e.target?.form ?? e.target)
                     .then((result) =>
                         window.Turbo.visit(window.url('{{ route('checkout', ['step' => 'credentials']) }}'))
                     ).catch();
