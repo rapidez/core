d78616bfa17d3fbd5a38c8e657a72f1818cdf6c6
Roy Duineveld <royduineveld@gmail.com>
2024-11-15 15:18:02 +0100

    Accessibility

--- resources/views/components/quantity.blade.php
+++ resources/views/components/quantity.blade.php
@@ -8,22 +8,22 @@ is present on the input and the quantity select component.
         <x-rapidez::button.outline
             v-on:click.prevent="qtySelect.decrease"
             v-bind:disabled="!qtySelect.decreasable"
+            aria-label="@lang('Decrease')"
         >
             <div class="bg-neutral h-0.5 w-3 hover:bg-white"></div>
         </x-rapidez::button.outline>
-        <label class="flex items-center">
-            <span class="px-2 sr-only">@lang('Quantity')</span>
-            <input
-                name="qty"
-                type="number"
-                dusk="qty-select"
-                class="w-14 px-1 text-center"
-                {{ $attributes }}
-            />
-        </label>
+        <input
+            name="qty"
+            type="number"
+            dusk="qty"
+            class="w-14 px-1 text-center"
+            aria-label="@lang('Quantity')"
+            {{ $attributes }}
+        />
         <x-rapidez::button.outline
             v-on:click.prevent="qtySelect.increase"
             v-bind:disabled="!qtySelect.increasable"
+            aria-label="@lang('Increase')"
         >
             <div class="relative">
                 <div class="bg-neutral absolute h-0.5 w-3 rotate-90"></div>
