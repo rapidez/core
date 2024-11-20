9edbaaff463ab5f26b78e6ddf3bcb09da793c580
Roy Duineveld <royduineveld@gmail.com>
2024-11-20 10:50:44 +0100

    Update config/rapidez/routing.php

--- config/rapidez/routing.php
+++ config/rapidez/routing.php
@@ -24,6 +24,7 @@ return [
         'cache_duration' => 3600,
     ],
 
+    // See: https://docs.rapidez.io/3.x/configuration.html#early-hints
     'earlyhints' => [
         'enabled' => env('EARLY_HINTS_ENABLED', true),
     ]
