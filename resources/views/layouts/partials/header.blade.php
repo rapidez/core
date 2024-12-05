5794063690ad11510f7451fe354a0e2754133d32
Roene <roene@justbetter.nl>
2024-12-05 15:37:58 +0100

    Added padding bottom in header for small screens

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="relative mb-5 border-b z-header">
+<header class="relative mb-5 max-md:pb-4 border-b z-header">
     <div class="container flex flex-wrap items-center max-sm:px-3">
         <input id="navigation" class="peer hidden" type="checkbox" />
         <div class="py-2">
