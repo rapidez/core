411d0c929b1560bbec3e43fb3c6de19f69df4cd0
indy koning <indy@justbetter.nl>
2024-12-11 11:41:38 +0100

    Implement new global slideover

--- resources/views/checkout/steps/agreements.blade.php
+++ resources/views/checkout/steps/agreements.blade.php
@@ -1,31 +1,25 @@
 <graphql query="{ checkoutAgreements { agreement_id name checkbox_text content is_html mode } }" cache="checkout.agreements" v-cloak>
     <div slot-scope="{ data }" v-if="data">
         <div v-for="agreement in data.checkoutAgreements" :key="agreement.agreement_id">
-            <x-rapidez::slideover position="right" v-bind:id="agreement.agreement_id + '_agreement'">
-                <x-slot:title>
-                    @{{ agreement.name }}
-                </x-slot:title>
-                <div v-if="agreement.is_html" v-html="agreement.content" class="p-3"></div>
-                <div v-else v-text="agreement.content" class="whitespace-pre-line p-3"></div>
-            </x-rapidez::slideover>
-
-            <template v-if="agreement.mode == 'AUTO'">
-                <label class="text-gray-700 cursor-pointer underline hover:no-underline" v-bind:for="agreement.agreement_id + '_agreement'">
-                    @{{ agreement.checkbox_text }}
-                </label>
-            </template>
-            <div v-else>
-                <x-rapidez::checkbox
-                    name="agreement_ids[]"
-                    v-bind:value="agreement.agreement_id"
-                    dusk="agreements"
-                    required
-                >
-                    <label class="text-gray-700 cursor-pointer underline hover:no-underline" v-bind:for="agreement.agreement_id + '_agreement'">
+            <global-slideover :title="agreement.name" :content="'<div class=&quot;p-3' + (!agreement.is_html ? ' whitespace-pre-line' : '') + '&quot;>' + agreement.content + '</div>'" v-slot="slideover">
+                <template v-if="agreement.mode == 'AUTO'">
+                    <label class="text-gray-700 cursor-pointer underline hover:no-underline" v-on:click="slideover.open">
                         @{{ agreement.checkbox_text }}
                     </label>
-                </x-rapidez::checkbox>
-            </div>
+                </template>
+                <div v-else>
+                    <x-rapidez::input.checkbox
+                        name="agreement_ids[]"
+                        v-bind:value="agreement.agreement_id"
+                        dusk="agreements"
+                        required
+                    >
+                        <label class="text-gray-700 cursor-pointer underline hover:no-underline" v-on:click="slideover.open">
+                            @{{ agreement.checkbox_text }}
+                        </label>
+                    </x-rapidez::input.checkbox>
+                </div>
+            </global-slideover>
         </div>
     </div>
 </graphql>
--- resources/views/components/slideover/partials/header.blade.php
+++ resources/views/components/slideover/partials/header.blade.php
@@ -2,13 +2,7 @@
     <div class="px-5">
         <div class="relative flex items-center justify-center">
             @if ($hasParent)
-                <label
-                    @if($idBind)
-                        v-bind:for="{{ $idBind }}"
-                    @else
-                        for="{{ $id }}"
-                    @endif
-                    class="absolute left-0 top-1/2 -translate-y-1/2 cursor-pointer text-white">
+                <label for="{{ $id }}" class="absolute left-0 top-1/2 -translate-y-1/2 cursor-pointer text-white">
                     <x-heroicon-o-arrow-left class="size-6" />
                 </label>
             @elseif ($headerbutton->isNotEmpty())
@@ -19,13 +13,7 @@
                     {{ $title }}
                 </span>
             @endif
-            <label
-                @if($idBind)
-                    v-bind:for="{{ $isInForm ? $closeId : $idBind }}"
-                @else
-                    for="{{ $isInForm ? $closeId : $id }}"
-                @endif
-                class="absolute right-0 top-1/2 -translate-y-1/2 cursor-pointer text-white">
+            <label for="{{ $closeId }}" class="absolute right-0 top-1/2 -translate-y-1/2 cursor-pointer text-white">
                 <x-heroicon-o-x-mark class="size-6" />
             </label>
         </div>
