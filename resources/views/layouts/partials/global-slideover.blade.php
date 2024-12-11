0868229a0173d39f048b813fc87fffc7672b7120
Jade Geels <jade@justbetter.nl>
2024-12-11 09:58:19 +0000

    Global slideover (#672)

---
+++ resources/views/layouts/partials/global-slideover.blade.php
@@ -0,0 +1,14 @@
+<global-slideover-instance>
+    <template v-slot="{ content, title, position }" v-cloak>
+        <x-rapidez::slideover
+            id="slideover-global"
+            v-bind:class="{ '-right-full peer-checked:right-0 !left-auto': (position ?? 'left') === 'right' }"
+        >
+            <x-slot:title>
+                <div v-html="title"></div>
+            </x-slot:title>
+
+            <div v-html="content"></div>
+        </x-rapidez::slideover>
+    </template>
+</global-slideover-instance>
