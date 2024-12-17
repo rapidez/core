c7e11267f5a7a5480bacf16f4b778efe55a08949
Roene <roene@justbetter.nl>
2024-12-17 14:10:35 +0100

    Small frontend fixes POP

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,9 +61,9 @@ export default {
             },
             zIndex: {
                 'header': '100',
-                'header-dropdown': '10',
                 'header-autocomplete-overlay': '10',
                 'header-autocomplete': '20',
+                'header-dropdown': '30',
                 'header-minicart': '30',
                 'header-autocomplete-button': '30',
 
