0ade3855b013e35728b0a4cfcd4f90f96eb52f2e
Jade Geels <jade@justbetter.nl>
2025-01-23 16:20:28 +0100

    Add range slider & use it for the price slider

--- resources/views/listing/partials/filter/price.blade.php
+++ resources/views/listing/partials/filter/price.blade.php
@@ -4,35 +4,11 @@
 >
     <template v-slot="{ currentRefinement, range, canRefine, refine, sendEvent }">
         <x-rapidez::filter.heading>
-            {{-- TODO: This should become a slider --}}
-            <div class="flex">
-                <input
-                    class="w-1/2"
-                    type="number"
-                    :min="range.min"
-                    :max="range.max"
-                    :placeholder="range.min"
-                    :disabled="!canRefine"
-                    :value="currentRefinement.min"
-                    v-on:input="refine({
-                      min: $event.currentTarget.value,
-                      max: currentRefinement.max,
-                    })"
-                />
-                <input
-                    class="w-1/2"
-                    type="number"
-                    :min="range.min"
-                    :max="range.max"
-                    :placeholder="range.max"
-                    :disabled="!canRefine"
-                    :value="currentRefinement.max"
-                    v-on:input="refine({
-                      min: currentRefinement.min,
-                      max: $event.currentTarget.value,
-                    })"
-                />
-            </div>
+            <x-rapidez::input.range-slider
+                v-bind:range="range"
+                v-bind:current="currentRefinement"
+                v-on:change="refine"
+            />
         </x-rapidez::filter.heading>
     </template>
 </ais-range-input>
