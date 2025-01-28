71b1c354d6fa789cc9219d1555ee8fb811989731
indykoning <15870933+indykoning@users.noreply.github.com>
2025-01-28 15:13:48 +0100

    Standalone checkout signed endpoint (#711)

--- resources/js/stores/useUser.js
+++ resources/js/stores/useUser.js
@@ -173,6 +173,10 @@ export const user = computed({
 
 // If token gets changed or emptied we should update the user.
 watch(token, refresh)
+if (userStorage.value?.email && !token.value) {
+    token.value = ''
+    userStorage.value = {}
+}
 
 document.addEventListener('vue:loaded', function (event) {
     event.detail.vue.$on('logout', async function (data = {}) {
