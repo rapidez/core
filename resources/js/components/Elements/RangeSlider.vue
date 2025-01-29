68526a03b7b05da914100fad3487026d1b11e5cd
Jade Geels <jade@justbetter.nl>
2025-01-29 14:43:02 +0000

    Range slider (#713)

---
+++ resources/js/components/Elements/RangeSlider.vue
@@ -0,0 +1,85 @@
+<script>
+export default {
+    props: {
+        range: {
+            type: Object,
+            default: () => ({ min: 0, max: 100 }),
+        },
+        current: {
+            type: Object,
+            default: () => ({ min: undefined, max: undefined }),
+        },
+    },
+
+    render() {
+        return this.$scopedSlots.default(this)
+    },
+
+    data() {
+        return {
+            minValue: null,
+            maxValue: null,
+        }
+    },
+
+    watch: {
+        minValue() {
+            if (this.minValue >= this.maxValue) {
+                this.minValue = this.maxValue - 1
+            }
+        },
+
+        maxValue() {
+            if (this.maxValue <= this.minValue) {
+                this.maxValue = this.minValue + 1
+            }
+        },
+
+        range() {
+            if (this.range.min == this.range.max && this.range.min == 0) {
+                return
+            }
+
+            if (this.minValue === null && this.maxValue === null) {
+                this.updateFromProps()
+            }
+        },
+
+        current() {
+            this.updateFromProps()
+        },
+    },
+
+    mounted() {
+        this.updateFromProps()
+    },
+
+    methods: {
+        updateFromProps() {
+            this.minValue = this.current.min ?? this.range.min
+            this.maxValue = this.current.max ?? this.range.max
+        },
+
+        percentage(amount) {
+            return ((amount - this.range.min) / (this.range.max - this.range.min)) * 100
+        },
+
+        updateRefinement() {
+            this.$emit('change', {
+                min: this.minValue,
+                max: this.maxValue,
+            })
+        },
+    },
+
+    computed: {
+        minThumb() {
+            return Math.max(this.percentage(this.minValue), 0)
+        },
+
+        maxThumb() {
+            return Math.max(100 - this.percentage(this.maxValue), 0)
+        },
+    },
+}
+</script>
--- resources/views/listing/partials/filter/price.blade.php
+++ resources/views/listing/partials/filter/price.blade.php
@@ -4,34 +4,36 @@
 >
     <template v-slot="{ currentRefinement, range, canRefine, refine, sendEvent }">
         <x-rapidez::filter.heading>
-            {{-- TODO: This should become a slider --}}
-            <div class="flex">
-                <input
-                    class="w-1/2"
-                    type="number"
-                    :min="range.min"
-                    :max="range.max"
-                    :placeholder="range.min"
-                    :disabled="!canRefine"
-                    :value="currentRefinement.min"
-                    v-on:input="refine({
-                      min: $event.currentTarget.value,
-                      max: currentRefinement.max,
-                    })"
-                />
-                <input
-                    class="w-1/2"
-                    type="number"
-                    :min="range.min"
-                    :max="range.max"
-                    :placeholder="range.max"
-                    :disabled="!canRefine"
-                    :value="currentRefinement.max"
-                    v-on:input="refine({
-                      min: currentRefinement.min,
-                      max: $event.currentTarget.value,
-                    })"
-                />
+            <div class="flex flex-col">
+                <div class="flex flex-1">
+                    <x-rapidez::input.range-slider
+                        v-bind:range="range"
+                        v-bind:current="currentRefinement"
+                        v-on:change="refine"
+                    />
+                </div>
+                <div class="flex">
+                    <input
+                        class="w-1/2"
+                        type="number"
+                        :min="range.min"
+                        :max="range.max"
+                        :placeholder="range.min"
+                        :disabled="!canRefine"
+                        :value="currentRefinement.min ?? range.min"
+                        v-on:input="refine({ min: $event.currentTarget.value, max: currentRefinement.max })"
+                    />
+                    <input
+                        class="w-1/2"
+                        type="number"
+                        :min="range.min"
+                        :max="range.max"
+                        :placeholder="range.max"
+                        :disabled="!canRefine"
+                        :value="currentRefinement.max ?? range.max"
+                        v-on:input="refine({ min: currentRefinement.min, max: $event.currentTarget.value })"
+                    />
+                </div>
             </div>
         </x-rapidez::filter.heading>
     </template>
