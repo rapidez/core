c38a71c29b694347863923070d7c2061216efd47
Jimmy <101801301+JimmyHoenderdaal@users.noreply.github.com>
2025-01-30 08:51:20 +0100

    Use the backdrop color variable (#696)

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,6 +64,7 @@ export default {
                     DEFAULT: color('--background', colors.slate[100]),
                     muted: color('--background-muted', colors.slate[50]),
                 },
+                backdrop: color('--backdrop', 'rgba(0, 0, 0, 0.4)'),
             },
             zIndex: {
                 'header': '100',
