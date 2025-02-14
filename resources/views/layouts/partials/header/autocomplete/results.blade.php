71de0b1b1bb589a9e7994e44076fd6eda86319c7
indy koning <indy@justbetter.nl>
2025-02-14 15:54:58 +0100

    Add back dynamic autocomplete fields

--- resources/views/layouts/partials/header/autocomplete/results.blade.php
+++ resources/views/layouts/partials/header/autocomplete/results.blade.php
@@ -1,3 +1,5 @@
-@include('rapidez::layouts.partials.header.autocomplete.categories')
+@foreach (config('rapidez.frontend.autocomplete.additionals') as $key => $fields)
+    @includeIf('rapidez::layouts.partials.header.autocomplete.' . $key)
+@endforeach
 @include('rapidez::layouts.partials.header.autocomplete.products')
 @include('rapidez::layouts.partials.header.autocomplete.all-results')
