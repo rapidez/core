a1bd7c3766fdfc540dcae810e0312daad8d3b97b
Jade Geels <jade@justbetter.nl>
2025-02-20 14:48:39 +0100

    Update all uses of deprecated getCachedByPath()

--- src/helpers.php
+++ src/helpers.php
@@ -1,11 +1,12 @@
 <?php
 
+use Rapidez\Core\Facades\Rapidez;
+
 if (! function_exists('price')) {
     function price($price)
     {
-        $configModel = config('rapidez.models.config');
-        $currency = $configModel::getCachedByPath('currency/options/default');
-        $locale = $configModel::getCachedByPath('general/locale/code', 'en_US');
+        $currency = Rapidez::config('currency/options/default');
+        $locale = Rapidez::config('general/locale/code', 'en_US');
         $formatter = new NumberFormatter($locale, NumberFormatter::CURRENCY);
 
         return $formatter->formatCurrency($price, $currency);
