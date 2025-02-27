48fe800a90920e78697708ba8305620793ae3dc0
Roy Duineveld <royduineveld@gmail.com>
2025-02-27 14:23:18 +0100

    Revert "Always render the listing on categories by default (#749)"
    This reverts commit b2fd81ab1f8846034f714579bfd3e759621d3860.

--- resources/views/category/overview.blade.php
+++ resources/views/category/overview.blade.php
@@ -10,10 +10,9 @@
         @include('rapidez::category.partials.breadcrumbs')
         <h1 class="mb-5 text-3xl font-bold">{{ $category->name }}</h1>
 
-        <x-rapidez::listing query="{ bool: { must: [{ terms: { visibility: [2, 4] } }, { terms: { category_ids: [config.category.entity_id] } }] } }" />
-
-        {{-- It's also possible to render the Magento widgets:
-        @if (!$category->is_anchor)
+        @if ($category->is_anchor)
+            <x-rapidez::listing query="{ bool: { must: [{ terms: { visibility: [2, 4] } }, { terms: { category_ids: [config.category.entity_id] } }] } }" />
+        @else
             <div class="flex max-md:flex-col">
                 <div class="xl:w-1/5">
                     @widget('sidebar.main', 'anchor_categories', 'catalog_category_view_type_layered', $category->entity_id)
@@ -23,7 +22,6 @@
                 </div>
             </div>
         @endif
-        --}}
 
         <div class="prose prose-green max-w-none">
             {!! $category->description !!}
