6583b177e039ef6acbff9ff00571d28ddc72cfc3
Jade-GG <Jade-GG@users.noreply.github.com>
2025-03-11 10:46:01 +0000

    Apply fixes from Duster

--- src/Models/Traits/Searchable.php
+++ src/Models/Traits/Searchable.php
@@ -2,7 +2,6 @@
 
 namespace Rapidez\Core\Models\Traits;
 
-use Exception;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
