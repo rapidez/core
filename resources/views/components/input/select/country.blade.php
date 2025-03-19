648545cc3a86093f327f56304f23d6b74736483a
Jade Geels <jade@justbetter.nl>
2025-03-19 14:50:22 +0100

    Use toSorted

--- resources/views/components/input/select/country.blade.php
+++ resources/views/components/input/select/country.blade.php
@@ -1,7 +1,7 @@
 <graphql query="{ countries { two_letter_abbreviation full_name_locale } }" cache="countries">
     <div v-if="data" slot-scope="{ data }">
         <x-rapidez::input.select {{ $attributes }}>
-            <option v-for="country in Object.values(data.countries).filter(country => country.full_name_locale).sort((a, b) => a.full_name_locale.localeCompare(b.full_name_locale))" :value="country.two_letter_abbreviation.toUpperCase()">
+            <option v-for="country in Object.values(data.countries).filter(country => country.full_name_locale).toSorted((a, b) => a.full_name_locale.localeCompare(b.full_name_locale))" :value="country.two_letter_abbreviation.toUpperCase()">
                 @{{ country.full_name_locale }}
             </option>
         </x-rapidez::input.select>
