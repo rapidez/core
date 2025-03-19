0d2f9ff272096e23aae542cd0ed30547ebf40a8a
Jade-GG <Jade-GG@users.noreply.github.com>
2025-03-19 09:41:28 +0000

    Apply fixes from Duster

--- src/helpers.php
+++ src/helpers.php
@@ -43,6 +43,7 @@ if (! function_exists('trans_fallback')) {
     function trans_fallback($key, $fallback, $replace = [], $locale = null)
     {
         $translator = app('translator');
+
         return $translator->has($key) ? $translator->get($key, $replace, $locale) : $fallback;
     }
 }
