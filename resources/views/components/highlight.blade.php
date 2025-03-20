fecd91296e989a751602223a62048aea3dba3598
Jade Geels <jade@justbetter.nl>
2025-03-20 16:14:11 +0100

    Simplify & allow custom tag

--- resources/views/components/highlight.blade.php
+++ resources/views/components/highlight.blade.php
@@ -1,12 +1,11 @@
-@props(['attribute', 'item' => 'item'])
+@props(['attribute', 'item' => 'item', 'highlightTag' => 'mark'])
 
 @if (in_array($attribute, config('rapidez.searchkit.highlight_attributes')))
-    <ais-highlight
-        {{ $attributes }}
-        attribute="{{ $attribute }}"
-        :hit="{{ $item }}"
-        highlighted-tag-name="mark"
-    />
+    <ais-highlight {{ $attributes->merge([
+        'v-bind:hit' => $item,
+        'attribute' => $attribute,
+        'highlighted-tag-name' => $highlightTag,
+    ]) }}/>
 @else
     <span {{ $attributes }} v-text="{{ $item }}.{{ $attribute }}"></span>
 @endif
