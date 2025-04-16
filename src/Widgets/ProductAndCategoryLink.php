26fe42c19fb25d5f7a7ddcd5275f05839f6619ed
Tjeerd <114989224+Tjeerd-JW@users.noreply.github.com>
2025-04-16 08:55:00 +0200

    Magento link widgets (#792)

---
+++ src/Widgets/ProductAndCategoryLink.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Rapidez\Core\Widgets;
+
+class ProductAndCategoryLink
+{
+    protected $url;
+
+    protected $title;
+
+    protected $anchorText;
+
+    public function __construct($vars)
+    {
+        $vars->id_path = collect(explode('/', $vars->id_path))->take(2)->implode('/');
+        $type = str($vars->id_path)->before('/')->value();
+        $id = str($vars->id_path)->after('/');
+
+        $this->url = match ($type) {
+            'category' => $id->prepend('catalog/category/view/id/'),
+            'product' => $id->prepend('catalog/product/view/id/'),
+        };
+
+        $this->title = $vars->title ?? $type ?? '';
+        $this->anchorText = $vars->anchor_text ?? $type ?? 'link';
+    }
+
+    public function render()
+    {
+        return view('rapidez::widget.link', [
+            'title' => $this->title,
+            'url' => $this->url,
+            'anchorText' => $this->anchorText,
+        ]);
+    }
+}
