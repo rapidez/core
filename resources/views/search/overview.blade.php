5d5845abcadb2a3f006876c4fecc2d9ca53dc0e7
Roy Duineveld <royduineveld@gmail.com>
2025-04-18 13:40:36 +0200

    Reactive search page h1 with the state results component

--- resources/views/search/overview.blade.php
+++ resources/views/search/overview.blade.php
@@ -2,13 +2,27 @@
 
 @section('robots', 'NOINDEX,NOFOLLOW')
 
-@section('content')
-    <div class="container" v-cloak>
-        <h1 class="font-bold text-3xl">
-            @lang('Search')
-            {{-- TODO: Re-add the search term reactive? --}}
-        </h1>
+@pushOnce('head', 'search-overview')
+    @vite(vite_filename_paths(['StateResults.vue']))
+@endPushOnce
 
-        <x-rapidez::listing filter-query-string="visibility:(3 OR 4)"/>
+@section('content')
+    <div class="container">
+        <x-rapidez::listing filter-query-string="visibility:(3 OR 4)">
+            <x-slot:before>
+                <ais-state-results>
+                    <template v-slot="{ state: { query } }">
+                        <h1 class="font-bold text-3xl">
+                            <template v-if="query">
+                                @lang('Search for'): @{{ query }}
+                            </template>
+                            <template v-else>
+                                @lang('Search')
+                            </template>
+                        </h1>
+                    </template>
+                </ais-state-results>
+            </x-slot:before>
+        </x-rapidez::listing>
     </div>
 @endsection
