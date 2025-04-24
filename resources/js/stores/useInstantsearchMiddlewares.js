bb5d935eca43a9ba0b9f8f3035d41bffcab3ebd7
indy koning <indy@justbetter.nl>
2025-04-24 15:49:58 +0200

    Fixed typo

--- resources/js/stores/useInstantsearchMiddlewares.js
+++ resources/js/stores/useInstantsearchMiddlewares.js
@@ -1,9 +1,9 @@
 export let instantsearchMiddlewares = []
 
-export async function addInstansearchMiddleware(middlewareFn) {
+export async function addInstantsearchMiddleware(middlewareFn) {
     instantsearchMiddlewares.push(middlewareFn)
 }
 
-export async function removeInstansearchMiddleware(middlewareFn) {
+export async function removeInstantsearchMiddleware(middlewareFn) {
     instantsearchMiddlewares = instantsearchMiddlewares.filter((registeredMiddleware) => registeredMiddleware !== middlewareFn)
 }
