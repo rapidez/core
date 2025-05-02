cb0e0f3d12f067b7710081eae7b81f7cadcb8bda
Roene <roene@justbetter.nl>
2025-05-02 10:08:09 +0200

    Move read more / less button in accordion

--- resources/views/listing/partials/filter/category.blade.php
+++ resources/views/listing/partials/filter/category.blade.php
@@ -14,7 +14,7 @@
                     <ul class="relative before:absolute before:left-0 before:border-l before:bottom-2.5 before:-translate-y-1 before:top-1.5 -my-1">
                         <li class="pl-6 relative before:absolute before:left-0 before:w-3 before:border-b before:top-3 before:translate-y-0.5" v-for="(item, index) in data" :key="item.value">
                             <a
-                                class="hover:text inline-block py-1"
+                                class="break-words inline-block py-1 hover:text"
                                 :href="createURL(item.value)"
                                 :class="{
                                     'font-medium': item.isRefined,
@@ -30,20 +30,20 @@
                         </li>
                     </ul>
                 </recursion>
-            </x-rapidez::filter.heading>
 
-            <button
-                v-if="canToggleShowMore"
-                v-on:click="toggleShowMore"
-                class="text-sm text-primary font-medium mt-3 hover:underline"
-            >
-                <span v-if="isShowingMore" class="flex gap-x-4">
-                    @lang('Less options')
-                </span>
-                <span v-else class="flex gap-x-4">
-                    @lang('More options')
-                </span>
-            </button>
+                <button
+                    v-if="canToggleShowMore"
+                    v-on:click="toggleShowMore"
+                    class="text-sm text-primary font-medium mt-3 hover:underline"
+                >
+                    <span v-if="isShowingMore" class="flex gap-x-4">
+                        @lang('Less options')
+                    </span>
+                    <span v-else class="flex gap-x-4">
+                        @lang('More options')
+                    </span>
+                </button>
+            </x-rapidez::filter.heading>
         </div>
     </template>
 </ais-hierarchical-menu>
