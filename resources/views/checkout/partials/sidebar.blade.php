e99019c17018fcfe1b6805090eb75957a628fc7b
Jimmy Hoenderdaal <jimmy@justbetter.nl>
2025-05-06 14:30:29 +0200

    Improve sidebar styling

--- resources/views/checkout/partials/sidebar.blade.php
+++ resources/views/checkout/partials/sidebar.blade.php
@@ -1,61 +1,64 @@
-<div class="flex flex-col gap-5">
-    <div class="rounded border p-3">
-        <div class="flex w-full flex-col">
-            <div v-for="item in cart.items" class="flex gap-x-1 border-b last:border-b-0 py-3">
-                <div class="w-7/12">@{{ item.product.name }}</div>
-                <div class="w-2/12 px-4 text-right">@{{ item.quantity }}</div>
-                <div class="w-3/12 text-right">@{{ item.prices.row_total.value | price }}</div>
+<div class="shrink-0 max-lg:mt-6 max-lg:hidden max-lg:border-t max-lg:border-dashed max-lg:pt-6 lg:w-80">
+    <strong class="font-heading text-primary-emphasis mb-4 block text-xl font-bold">@lang('Order summary')</strong>
+    <div class="mb-4 flex flex-col gap-1">
+        <div v-for="item in cart.items" class="flex justify-between text-xs">
+            <span class="truncate">@{{ item.quantity }}x @{{ item.product.name }}</span>
+            <span>@{{ item.prices.row_total.value | price }}</span>
+        </div>
+    </div>
+    <dl class="flex w-full flex-col gap-4 text-sm first:*:pt-0 last:*:pb-0">
+        <div class="flex flex-wrap items-baseline justify-between font-medium">
+            <dt class="flex-1">@lang('Subtotal')</dt>
+            <dd class="text-right">@{{ cart.prices.subtotal_including_tax.value | price }}</dd>
+        </div>
+        <div class="w-full border-t border-dashed"></div>
+        <div v-if="cart.prices.applied_taxes.length" class="flex flex-wrap items-baseline justify-between font-medium">
+            <dt class="flex-1">@lang('Tax')</dt>
+            <dd class="text-right">@{{ cart.prices.applied_taxes[0].amount.value | price }}</dd>
+        </div>
+        <div v-if="cart.shipping_addresses.length && cart.shipping_addresses[0]?.selected_shipping_method" class="flex flex-wrap items-baseline justify-between font-medium">
+            <dt class="flex-1">
+                @lang('Shipping')
+                <div class="text-xs text-muted mt-1">
+                    @{{ cart.shipping_addresses[0]?.selected_shipping_method.carrier_title }} - @{{ cart.shipping_addresses[0]?.selected_shipping_method.method_title }}
+                </div>
+            </dt>
+            <dd class="text-right">
+                <span v-if="cart.shipping_addresses[0]?.selected_shipping_method.amount.value === 0" class="font-semibold text-green-700">@lang('Free')</span>
+                <span v-else>@{{ cart.shipping_addresses[0]?.selected_shipping_method.amount.value | price }}</span>
+            </dd>
+        </div>
+        <div v-for="discount in cart.prices.discounts" class="flex flex-wrap items-baseline justify-between font-medium">
+            <dt class="flex-1">@{{ discount.label }}</dt>
+            <dd class="text-right">-@{{ discount.amount.value | price }}</dd>
+        </div>
+        <div class="w-full border-t"></div>
+        <div class="flex flex-wrap items-baseline justify-between font-bold">
+            <dt class="flex-1">@lang('Total')</dt>
+            <dd class="text-right text-base">@{{ cart.prices.grand_total.value | price }}</dd>
+        </div>
+        <dd v-if="cart.prices.applied_taxes.length" class="flex w-full flex-col gap-1 pr-0 text-right text-xs text-muted">
+            @lang('Including') @{{ cart.prices.applied_taxes[0].amount.value | price }} @lang('tax')
+        </dd>
+        <div v-if="cart.shipping_addresses[0]" class="w-full border-t border-dashed"></div>
+        <div v-if="cart.shipping_addresses[0]" class="text-xs text-muted">
+            <div class="flex flex-col gap-1 pr-24">
+                <span v-if="cart.shipping_addresses[0]?.company">@{{ cart.shipping_addresses[0]?.company }}</span>
+                <span>@{{ cart.shipping_addresses[0]?.prefix }} @{{ cart.shipping_addresses[0]?.firstname }} @{{ cart.shipping_addresses[0]?.middlename }} @{{ cart.shipping_addresses[0]?.lastname }} @{{ cart.shipping_addresses[0]?.suffix }}</span>
+                <span>@{{ cart.shipping_addresses[0]?.street[0] }} @{{ cart.shipping_addresses[0]?.street[1] }} @{{ cart.shipping_addresses[0]?.street[2] }}</span>
+                <span>@{{ cart.shipping_addresses[0]?.postcode }} @{{ cart.shipping_addresses[0]?.city }} @{{ cart.shipping_addresses[0]?.country.label }}</span>
+                <span v-if="cart.shipping_addresses[0]?.telephone">@{{ cart.shipping_addresses[0]?.telephone }}</span>
             </div>
         </div>
-    </div>
-
-    <dl class="flex w-full flex-col rounded border *:flex *:flex-wrap *:justify-between *:p-3 *:border-b last:*:border-none">
-        <div>
-            <dt>@lang('Subtotal')</dt>
-            <dd>@{{ cart.prices.subtotal_including_tax.value | price }}</dd>
-        </div>
-        <div v-if="cart.prices.applied_taxes.length">
-            <dt>@lang('Tax')</dt>
-            <dd>@{{ cart.prices.applied_taxes[0].amount.value | price }}</dd>
-        </div>
-        <div v-if="cart.shipping_addresses.length && cart.shipping_addresses[0]?.selected_shipping_method">
-            <dt>
-                @lang('Shipping')<br>
-                <small>@{{ cart.shipping_addresses[0]?.selected_shipping_method.carrier_title }} - @{{ cart.shipping_addresses[0]?.selected_shipping_method.method_title }}</small>
-            </dt>
-            <dd>@{{ cart.shipping_addresses[0]?.selected_shipping_method.amount.value | price }}</dd>
-        </div>
-        <div v-for="discount in cart.prices.discounts">
-            <dt>@{{ discount.label }}</dt>
-            <dd>-@{{ discount.amount.value | price }}</dd>
-        </div>
-        <div class="font-bold">
-            <dt>@lang('Total')</dt>
-            <dd>@{{ cart.prices.grand_total.value | price }}</dd>
+        <div v-if="cart.billing_address && !cart.billing_address?.same_as_shipping" class="w-full border-t border-dashed"></div>
+        <div v-if="cart.billing_address && !cart.billing_address?.same_as_shipping" class="text-xs text-muted">
+            <div class="flex flex-col gap-1">
+                <span v-if="cart.billing_address.company">@{{ cart.billing_address.company }}</span>
+                <span>@{{ cart.billing_address.prefix }} @{{ cart.billing_address.firstname }} @{{ cart.billing_address.middlename }} @{{ cart.billing_address.lastname }} @{{ cart.billing_address.suffix }}</span>
+                <span>@{{ cart.billing_address.street[0] }} @{{ cart.billing_address.street[1] }} @{{ cart.billing_address.street[2] }}</span>
+                <span>@{{ cart.billing_address.postcode }} @{{ cart.billing_address.city }} @{{ cart.billing_address.country.label }}</span>
+                <span v-if="cart.billing_address.telephone">@{{ cart.billing_address.telephone }}</span>
+            </div>
         </div>
     </dl>
-
-    <div v-if="cart.shipping_addresses[0]" class="flex w-full flex-col gap-x-1 border p-3 rounded">
-        <p class="font-lg mb-2 font-bold">
-            <template v-if="cart.billing_address?.same_as_shipping">@lang('Shipping & billing address')</template>
-            <template v-else>@lang('Shipping address')</template>
-        </p>
-        <ul>
-            <li v-if="cart.shipping_addresses[0]?.company">@{{ cart.shipping_addresses[0]?.company }}</li>
-            <li>@{{ cart.shipping_addresses[0]?.prefix }} @{{ cart.shipping_addresses[0]?.firstname }} @{{ cart.shipping_addresses[0]?.middlename }} @{{ cart.shipping_addresses[0]?.lastname }} @{{ cart.shipping_addresses[0]?.suffix }}</li>
-            <li>@{{ cart.shipping_addresses[0]?.street[0] }} @{{ cart.shipping_addresses[0]?.street[1] }} @{{ cart.shipping_addresses[0]?.street[2] }}</li>
-            <li>@{{ cart.shipping_addresses[0]?.postcode }} - @{{ cart.shipping_addresses[0]?.city }} - @{{ cart.shipping_addresses[0]?.country.label }}</li>
-            <li v-if="cart.shipping_addresses[0]?.telephone">@{{ cart.shipping_addresses[0]?.telephone }}</li>
-        </ul>
-    </div>
-    <div v-if="cart.billing_address && !cart.billing_address?.same_as_shipping" class="mt-4 flex w-full flex-col gap-x-1 border p-3">
-        <p class="font-lg mb-2 font-bold">@lang('Billing address')</p>
-        <ul>
-            <li v-if="cart.billing_address.company">@{{ cart.billing_address.company }}</li>
-            <li>@{{ cart.billing_address.prefix }} @{{ cart.billing_address.firstname }} @{{ cart.billing_address.middlename }} @{{ cart.billing_address.lastname }} @{{ cart.billing_address.suffix }}</li>
-            <li>@{{ cart.billing_address.street[0] }} @{{ cart.billing_address.street[1] }} @{{ cart.billing_address.street[2] }}</li>
-            <li>@{{ cart.billing_address.postcode }} - @{{ cart.billing_address.city }} - @{{ cart.billing_address.country.label }}</li>
-            <li v-if="cart.billing_address.telephone">@{{ cart.billing_address.telephone }}</li>
-        </ul>
-    </div>
 </div>
