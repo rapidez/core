f94c90d867eaf79dcd8dbbdb8cb300207c515ebf
Jade Geels <jade@justbetter.nl>
2025-05-12 15:37:03 +0200

    Use parent:: instead

--- src/Models/Traits/Product/Searchable.php
+++ src/Models/Traits/Product/Searchable.php
@@ -125,7 +125,7 @@ trait Searchable
      */
     public static function getIndexMappings(): ?array
     {
-        return Eventy::filter('index.' . static::getIndexName() . '.mapping', [
+        return array_merge([
             'properties' => [
                 'price' => [
                     'type' => 'double',
@@ -140,6 +140,6 @@ trait Searchable
                     'type' => 'flattened',
                 ],
             ],
-        ]);
+        ], parent::getIndexMappings());
     }
 }
