09fb85c443a79fa82995f60e0afc0695469ce046
Roene-JustBetter <Roene-JustBetter@users.noreply.github.com>
2025-05-13 15:33:10 +0000

    Apply fixes from Duster

--- resources/views/listing/partials/no-results.blade.php
+++ resources/views/listing/partials/no-results.blade.php
@@ -14,7 +14,7 @@
                     <ul class="flex flex-col font-sans">
                         <li v-for="(item, count) in items" class="flex flex-1 items-center w-full">
                             <a
-                                v-bind:href="window.url(item.redirect || '{{ route('search', ['q' => 'searchPlaceholder']) }}'.replace('searchPlaceholder', encodeURIComponent(item.query_text)))" 
+                                v-bind:href="window.url(item.redirect || '{{ route('search', ['q' => 'searchPlaceholder']) }}'.replace('searchPlaceholder', encodeURIComponent(item.query_text)))"
                                 class="flex items-center group py-1 gap-x-0.5 hover:underline"
                             >
                                 <x-heroicon-o-chevron-right class="text-primary size-3.5 shrink-0"/> <span v-text="item.query_text"></span>
