8c0224be265efbdd9e7a6ee5eef12a4c91dc2ca3
indy koning <indy@justbetter.nl>
2025-05-14 17:07:46 +0200

    Revert setting missing values to undefined

--- resources/js/helpers/object.js
+++ resources/js/helpers/object.js
@@ -9,11 +9,7 @@
  */
 export function objectDiff(target, source) {
     return Object.fromEntries(
-        Object.entries({
-            // Ensure keys no longer in source will be set to undefined
-            ...Object.fromEntries(Object.keys(target).map((key) => [key, undefined])),
-            ...source,
-        })
+        Object.entries(source)
             .map(([key, val]) => {
                 if (!target || !(key in target)) {
                     return [key, val]
