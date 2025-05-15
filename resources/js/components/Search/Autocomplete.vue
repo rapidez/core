af94b732c0a2f00fc830a5b37babd81a248d18de
indykoning <indykoning@users.noreply.github.com>
2025-05-15 07:40:06 +0000

    Apply fixes from Prettier

--- resources/js/components/Search/Autocomplete.vue
+++ resources/js/components/Search/Autocomplete.vue
@@ -43,7 +43,7 @@ export default {
     },
     mounted() {
         this.$nextTick(() => {
-            this.$emit('mounted');
+            this.$emit('mounted')
             setTimeout(() => {
                 requestAnimationFrame(() => {
                     let element = null
