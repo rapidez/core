3d3e960aba5bf4463bae007eb18ee0846aaf1fc0
indy koning <indy@justbetter.nl>
2025-05-16 12:04:21 +0200

    Added basic view transition

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="view-transition" content="same-origin" />
 
     <title>
         @hasSection('title')
