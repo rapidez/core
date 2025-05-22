35a9b26447131f9e76ca9bae7b183e0734ee6dcc
Roene <roene@justbetter.nl>
2025-05-22 11:49:03 +0200

    Add ms debug back

--- resources/views/listing/partials/toolbar/stats.blade.php
+++ resources/views/listing/partials/toolbar/stats.blade.php
@@ -2,4 +2,7 @@
     <template v-slot="{ nbPages, nbHits, page, processingTimeMS }">
         @{{ nbHits }} @lang('products')
     </template>
+    <template v-if="window.debug">
+        - @{{ processingTimeMS }}ms
+    </template>
 </ais-stats>
