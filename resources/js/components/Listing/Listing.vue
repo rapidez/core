1dfc1401c9baf1ac26a4ab1254e067d7ad6b6729
Jade-GG <Jade-GG@users.noreply.github.com>
2025-06-05 13:41:59 +0000

    Apply fixes from Prettier

--- resources/js/components/Listing/Listing.vue
+++ resources/js/components/Listing/Listing.vue
@@ -135,7 +135,7 @@ export default {
         },
 
         windowTitle(routeState) {
-            if(this.destroyed) {
+            if (this.destroyed) {
                 return null
             }
 
