6a4b4e460e45be8331ba25b94d30ff9f7f881d66
Roene <roene@justbetter.nl>
2025-06-06 15:51:22 +0200

    Make use of @slotsdefault for slider

--- resources/views/components/productlist.blade.php
+++ resources/views/components/productlist.blade.php
@@ -27,21 +27,7 @@
                                     @lang($title)
                                 </strong>
                             @endif
-                            <x-rapidez::slider>
-                                <template v-for="(item, count) in items">
-                                    @include('rapidez::listing.partials.item', ['slider' => true])
-                                </template>
-                                <x-slot:indicator>
-                                    <div v-show="slidesTotal > 1" class="flex flex-row justify-center w-full mt-9">
-                                        <div
-                                            v-for="slide, index in slidesTotal"
-                                            v-on:click="navigate(index)"
-                                            class="relative bg rounded-full border size-4 mx-1 cursor-pointer"
-                                            :class="{ 'bg-active border-active': index === currentSlide }">
-                                        </div>
-                                    </div>
-                                </x-slot:indicator>
-                            </x-rapidez::slider>
+                            <x-rapidez::slider />
                         </div>
                     </ais-hits>
                 </ais-instant-search>
--- resources/views/components/slider/slider.blade.php
+++ resources/views/components/slider/slider.blade.php
@@ -1,10 +1,14 @@
-@slots(['indicator'])
+@slots(['items', 'indicator'])
 
 <slider>
     <div slot-scope="{ navigate, showLeft, showRight, currentSlide, slidesTotal }">
-        <div class="relative">
-            <div ref="slider" {{ $attributes->twMerge('*:w-1/2 *:md:w-1/3 *:xl:w-1/4 *:px-5 *:shrink-0 *:snap-start -mx-5 flex snap-x snap-mandatory overflow-x-auto scrollbar-hide') }}>
-                {{ $slot }}
+        <div {{ $attributes->twMerge('relative') }}>
+            <div ref="slider" {{ $items->attributes->twMerge('*:w-1/2 *:md:w-1/3 *:xl:w-1/4 *:px-5 *:shrink-0 *:snap-start -mx-5 flex snap-x snap-mandatory overflow-x-auto scrollbar-hide') }}>
+                @slotdefault('items')
+                    <template v-for="(item, count) in items">
+                        @include('rapidez::listing.partials.item', ['slider' => true])
+                    </template>
+                @endslotdefault
             </div>
             <x-rapidez::button.slider
                 class="absolute left-0 top-1/2 sm:-translate-x-1/2 -translate-y-1/2"
@@ -23,6 +27,15 @@
                 <x-heroicon-o-chevron-right class="size-6 shrink-0"/>
             </x-rapidez::button.slider>
         </div>
-        {{ $indicator }}
+        @slotdefault('indicator')
+            <div v-show="slidesTotal > 1" class="flex flex-row justify-center w-full mt-9">
+                <div
+                    v-for="slide, index in slidesTotal"
+                    v-on:click="navigate(index)"
+                    class="relative bg rounded-full border size-4 mx-1 cursor-pointer"
+                    :class="{ 'bg-active border-active': index === currentSlide }">
+                </div>
+            </div>
+        @endslotdefault
     </div>
 </slider>
