478aa98659ea599ed7be153f92df1f52ce24823a
Roy Duineveld <royduineveld@gmail.com>
2025-06-13 10:48:20 +0200

    Use localhost

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'http://localhost:8000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,7 +74,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: './vendor/bin/testbench-dusk serve',
-    url: 'http://127.0.0.1:8000',
+    url: 'http://localhost:8000',
     reuseExistingServer: !process.env.CI,
   },
 });
