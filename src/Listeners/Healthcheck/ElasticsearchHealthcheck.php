720ea7ccede0612f306fe31216eaecc71338ee98
indy koning <indy@justbetter.nl>
2025-07-17 15:28:03 +0200

    Updated healthcheck to check opensearch as well

--- src/Listeners/Healthcheck/ElasticsearchHealthcheck.php
+++ src/Listeners/Healthcheck/ElasticsearchHealthcheck.php
@@ -2,6 +2,9 @@
 
 namespace Rapidez\Core\Listeners\Healthcheck;
 
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Http;
+
 class ElasticsearchHealthcheck extends Base
 {
     protected $esVersion = '7.6';
@@ -13,9 +16,44 @@ class ElasticsearchHealthcheck extends Base
             'messages' => [],
         ];
 
-        $data = json_decode(@file_get_contents(config('rapidez.es_url')));
-        if (is_object($data)
-            && property_exists($data, 'version')
+        if (config('elasticsearch.backend_type') !== 'elasticsearch') {
+            return $response;
+        }
+
+        try {
+            $data = Http::withBasicAuth(
+                config('elasticsearch.user'),
+                config('elasticsearch.password')
+            )
+                ->get(config('rapidez.es_url'))
+                ->object();
+        } catch (ConnectionException $e) {
+            $data = null;
+        }
+
+        if (! is_object($data) || ! property_exists($data, 'version')) {
+            $response['healthy'] = false;
+            $response['messages'][] = [
+                'type'  => 'error',
+                'value' => __('Elasticsearch is not reachable at: :url', ['url' => config('rapidez.es_url')]),
+            ];
+
+            return $response;
+        }
+
+        if (property_exists($data->version, 'distribution')
+            && $data->version->distribution === 'opensearch'
+        ) {
+            $response['healthy'] = false;
+            $response['messages'][] = [
+                'type'  => 'error',
+                'value' => __('SCOUT_SEARCH_BACKEND is set to elasticsearch, but the given ELASTICSEARCH_URL seems to be opensearch!'),
+            ];
+
+            return $response;
+        }
+
+        if (!property_exists($data->version, 'distribution')
             && $data->version->build_flavor !== 'oss'
             && ! version_compare($data->version->number, $this->esVersion, '>=')
         ) {
@@ -25,12 +63,6 @@ class ElasticsearchHealthcheck extends Base
                 'value' => __('Your Elasticsearch version is too low!') . PHP_EOL .
                     __('Your version: :version (:build_flavour)', ['version' => $data->version->number, 'build_flavour' => $data->version->build_flavor]) . PHP_EOL .
                     __('You need at least: :version basic/default (non OSS version)', ['version' => $this->esVersion]),
-            ];
-        } elseif (! is_object($data) || ! property_exists($data, 'version')) {
-            $response['healthy'] = false;
-            $response['messages'][] = [
-                'type'  => 'error',
-                'value' => __('Elasticsearch is not reachable at: :url', ['url' => config('rapidez.es_url')]),
             ];
         }
 
