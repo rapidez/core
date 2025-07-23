34ce49333145dc88199418cccbb9e749e3e597e6
Roy Duineveld <royduineveld@gmail.com>
2025-07-23 15:46:29 +0200

    Playwright reports on Github pages + screenshots in LFS (#940)

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: process.env.CI ? 'blob' : 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
@@ -70,11 +70,4 @@ export default defineConfig({
         //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
         // },
     ],
-
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //     command: './vendor/bin/testbench-dusk serve',
-    //     url: 'http://localhost:8000',
-    //     reuseExistingServer: !process.env.CI,
-    // },
 })
