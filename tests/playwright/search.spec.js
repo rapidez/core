87340670335518621c34b67749bb2505d119f455
Roy Duineveld <royduineveld@gmail.com>
2025-07-29 16:42:14 +0200

    Search tests

---
+++ tests/playwright/search.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'
+import { ProductPage } from './pages/ProductPage'
+
+test('search page', async ({ page }) => {
+    const product = await new ProductPage(page).goto(process.env.PRODUCT_URL_SIMPLE)
+    await page.goto('/search?q=' + product.name)
+    await expect(page.getByTestId('listing-item').first()).toContainText(product.name)
+    await expect(page).toHaveScreenshot()
+})
+
+test('autocomplete', async ({ page }) => {
+    const product = await new ProductPage(page).goto(process.env.PRODUCT_URL_SIMPLE)
+    await page.goto('/')
+    await page.getByTestId('autocomplete-input').fill(product.name)
+    await expect(page.getByTestId('autocomplete-item').first()).toContainText(product.name)
+    await expect(page).toHaveScreenshot()
+})
