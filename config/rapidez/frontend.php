676a256f9b7b12b4b9d7a5a9ddaa4820291cd43a
Roy Duineveld <royduineveld@gmail.com>
2025-07-29 14:36:26 +0200

    Update config/rapidez/frontend.php

--- config/rapidez/frontend.php
+++ config/rapidez/frontend.php
@@ -28,7 +28,7 @@ return [
     ],
 
     // If set to false, you will be required to log in when attempting to check out with an existing email address
-    'allow_guest_on_existing_account' => true,
+    'allow_guest_on_existing_account' => false,
 
     'autocomplete' => [
         // Attach additional indexes to the autocomplete
