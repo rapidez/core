0a0537c01191a5017b4cf4bda43f8d733b60b58a
Roy Duineveld <royduineveld@gmail.com>
2025-07-31 17:10:40 +0200

    Keep feature tests publish

--- src/Commands/InstallTestsCommand.php
+++ src/Commands/InstallTestsCommand.php
@@ -23,6 +23,19 @@ class InstallTestsCommand extends Command
             base_path('playwright.config.js')
         );
 
+        File::copyDirectory(
+            base_path('vendor/rapidez/core/tests/Feature'),
+            base_path('tests/Feature')
+        );
+
+        foreach (File::files(base_path('tests/Feature')) as $file) {
+            file_put_contents($file->getPathname(), str_replace(
+                'use Rapidez\Core\Tests\TestCase;',
+                'use Tests\TestCase;',
+                $file->getContents()
+            ));
+        }
+
         $this->info('Done 🚀 you can now run the browser tests with: yarn playwright test');
     }
 }
