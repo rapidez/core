39d43d9d7b45998f718f31053f01af457b40f1b4
Roy Duineveld <royduineveld@gmail.com>
2025-08-01 14:42:33 +0200

    Coupon title fix

--- resources/views/cart/coupon.blade.php
+++ resources/views/cart/coupon.blade.php
@@ -1,5 +1,5 @@
 <div class="bg-white rounded-lg border p-3">
-    <strong class="text-lg leading-6 font-medium text-gray-900 mb-5">
+    <strong class="block text-lg leading-6 font-medium text-gray-900 mb-5">
         @lang('Apply coupon code')
     </strong>
 
