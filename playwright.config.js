46c69c37758baeb944de64c7e7cb263a8cd13270
Roy Duineveld <royduineveld@gmail.com>
2025-08-01 11:33:55 +0200

    Url from env

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: process.env.CI ? 'http://localhost:8000' : 'http://rapidez.test',
+        baseURL: process.env.APP_URL,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
