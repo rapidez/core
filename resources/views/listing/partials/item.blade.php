de569ff2ef4638fb7de84150da40d00730672202
Jade Geels <jade@justbetter.nl>
2025-08-13 13:08:57 +0200

    Swatches on the product page and listings (#898)

--- resources/views/listing/partials/item.blade.php
+++ resources/views/listing/partials/item.blade.php
@@ -1,6 +1,6 @@
 <div v-bind:key="item.entity_id" class="px-5 py-10" data-testid="listing-item">
     <add-to-cart v-bind:product="item" v-slot="addToCart" v-cloak>
-        <div class="group relative flex flex-1 flex-col rounded bg-white h-full">
+        <div class="group flex flex-1 flex-col rounded bg-white h-full">
             <a :href="addToCart.productUrl | url" v-on:click="sendEvent('click', item, 'Hit Clicked')" class="block mb-auto">
                 <img
                     v-if="addToCart.currentThumbnail"
