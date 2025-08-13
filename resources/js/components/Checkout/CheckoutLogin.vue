1a15733c7f62390e67bc704da8e6e4597fa65338
Jade Geels <jade@justbetter.nl>
2025-08-13 15:00:31 +0200

    Set guest email on change (#971)

--- resources/js/components/Checkout/CheckoutLogin.vue
+++ resources/js/components/Checkout/CheckoutLogin.vue
@@ -5,6 +5,7 @@ import { useDebounceFn } from '@vueuse/core'
 
 const debouncePromise = useDebounceFn(async function (self) {
     self.isEmailAvailable = await isEmailAvailable(self.email || '')
+    await self.handleGuest()
 }, 300)
 
 export default {
