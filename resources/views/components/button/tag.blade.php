1824dc8fb6a9162cd360355b67a67405361a92e8
Roene <roene@justbetter.nl>
2025-08-25 14:17:06 +0200

    Added loading animation for other buttons

--- resources/views/components/button/tag.blade.php
+++ resources/views/components/button/tag.blade.php
@@ -7,17 +7,10 @@
 
 <x-rapidez::tag
     is="{{ $tag }}"
+    :v-bind:class="$loader ? '{ \'button-loading\': $root.loading }' : ''"
     {{ $attributes->merge([
         ':disabled' => $attributes->has('href') || $attributes->has(':href') || !$disableWhenLoading ? null : '$root.loading',
     ]) }}
 >
-    <span class="contents" @if ($loader) v-bind:class="{'invisible': loading}" @endif>
-        {{ $slot }}
-    </span>
-
-    @if ($loader)
-        <div v-if="loading" class="absolute right-1/2 bottom-1/2 translate-x-1/2 translate-y-1/2" v-cloak>
-            <x-heroicon-o-arrow-path class="animate-spin size-5"/>
-        </div>
-    @endif
+    {{ $slot }}
 </x-rapidez::tag>
