b785c9430f493e6166906b590bcb0db666023813
Jordy de Vulder <jordy@justbetter.nl>
2025-08-26 14:27:26 +0200

    Resolved more naming underscores

--- resources/views/product/partials/super-attributes.blade.php
+++ resources/views/product/partials/super-attributes.blade.php
@@ -1,9 +1,9 @@
 @foreach ($product->super_attributes ?: [] as $superAttributeId => $superAttribute)
     @if ($superAttribute->visual_swatch)
-        @include('rapidez::product.partials.super-attributes.visual_swatch')
+        @include('rapidez::product.partials.super-attributes.visual-swatch')
     @elseif ($superAttribute->text_swatch)
-        @include('rapidez::product.partials.super-attributes.text_swatch')
+        @include('rapidez::product.partials.super-attributes.text-swatch')
     @else
-        @include('rapidez::product.partials.super-attributes.drop_down')
+        @include('rapidez::product.partials.super-attributes.drop-down')
     @endif
 @endforeach
