d9d9d79a1f88096294c196ff87d931a0e828c11c
Jordy de Vulder <jordy@justbetter.nl>
2025-08-26 17:24:30 +0200

    Uniform Blade file naming with dashes (#955)

--- resources/views/listing/partials/item/super-attributes.blade.php
+++ resources/views/listing/partials/item/super-attributes.blade.php
@@ -1,11 +1,11 @@
 <div v-for="(superAttribute, superAttributeId) in item.super_attributes" class="mt-2">
     <template v-if="superAttribute.visual_swatch">
-        @include('rapidez::listing.partials.item.super-attributes.visual_swatch')
+        @include('rapidez::listing.partials.item.super-attributes.visual-swatch')
     </template>
     <template v-else-if="superAttribute.text_swatch">
-        @include('rapidez::listing.partials.item.super-attributes.text_swatch')
+        @include('rapidez::listing.partials.item.super-attributes.text-swatch')
     </template>
     <template v-else>
-        @include('rapidez::listing.partials.item.super-attributes.drop_down')
+        @include('rapidez::listing.partials.item.super-attributes.drop-down')
     </template>
 </div>
--- resources/views/product/partials/super_attributes.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach ($product->super_attributes ?: [] as $superAttributeId => $superAttribute)
-    @if ($superAttribute->visual_swatch)
-        @include('rapidez::product.partials.super-attributes.visual_swatch')
-    @elseif ($superAttribute->text_swatch)
-        @include('rapidez::product.partials.super-attributes.text_swatch')
-    @else
-        @include('rapidez::product.partials.super-attributes.drop_down')
-    @endif
-@endforeach
