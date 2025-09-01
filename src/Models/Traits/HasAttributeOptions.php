9602fe3339c64d018068a8455231d89310e83747
Jade <jade@justbetter.nl>
2025-09-01 13:55:55 +0200

    Fix performance

--- src/Models/Traits/HasAttributeOptions.php
+++ src/Models/Traits/HasAttributeOptions.php
@@ -8,18 +8,29 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 trait HasAttributeOptions
 {
     protected static $hasAttributeOptions = true;
+    protected static $attributeCache = [];
 
     protected function options(): Attribute
     {
-        // Sort by store_id first to always get the higher store id if there are two.
-        return Attribute::get(fn () => $this->attributeOptions
-            ->sortBy('store_id')
-            ->keyBy('option_id')
-        );
+        return Attribute::get(function() {
+            $store = config('rapidez.store');
+            $key = "store_$store.$this->attribute_code";
+
+            $value = data_get(static::$attributeCache, $key);
+            if ($value) {
+                return $value;
+            }
+
+            $value = $this->attributeOptions->keyBy('option_id');
+            data_set(static::$attributeCache, $key, $value);
+            
+            return $value;
+        });
     }
 
     public function attributeOptions(): HasMany
     {
-        return $this->hasMany(config('rapidez.models.attribute_option'), 'attribute_id', 'attribute_id');
+        // Sort by store_id first to always get the higher store id if there are two.
+        return $this->hasMany(config('rapidez.models.attribute_option'), 'attribute_id', 'attribute_id')->orderBy('store_id');
     }
 }
