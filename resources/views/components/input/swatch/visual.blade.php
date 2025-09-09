43739cdec3247fc12e6108d9245f0c8b8ab04f8c
Jade Geels <jade@justbetter.nl>
2025-09-09 09:06:56 +0200

    Initial product swatches rendering from Blade (#982)

--- resources/views/components/input/swatch/visual.blade.php
+++ resources/views/components/input/swatch/visual.blade.php
@@ -1,7 +1,11 @@
-@props(['color' => '\'none\''])
+@props(['color' => null, 'vBindColor' => $attributes['v-bind:color'] ?? null])
 
 <label class="group/swatch cursor-pointer flex items-center justify-center p-1 rounded-full ring-inset relative ring-default ring-1 hover:ring-emphasis has-[:checked]:ring-active has-[:checked]:ring-2 has-[:checked]:hover:ring-active group">
-    <span class="size-5 block border border-black/15 rounded-full m-px" v-bind:style="{ background: {{ $color }} }"></span>
+    <span @attributes([
+        'class' => 'size-5 block border border-black/15 rounded-full m-px',
+        'style' => $color ? "background:$color" : null,
+        'v-bind:style' => $vBindColor ? "{ background:$vBindColor }" : null,
+    ])></span>
     <input {{ $attributes->class('opacity-0 size-0 absolute') }}>
     @if ($slot->isNotEmpty())
         <span class="pointer-events-none absolute left-0 bottom-full mb-1 rounded px-1.5 py-0.5 bg-active text-white opacity-0 group-hover/swatch:opacity-100 transition-opacity">
--- resources/views/product/partials/super-attributes/text-swatch.blade.php
+++ resources/views/product/partials/super-attributes/text-swatch.blade.php
@@ -4,19 +4,21 @@
     </x-rapidez::label>
 
     <ul class="flex flex-wrap gap-x-1.5 gap-y-2 items-center pr-14">
-        <li v-for="option in Object.values(config.product.super_{{ $superAttribute->code }}).sort((a, b) => a.label.localeCompare(b.label)).sort((a, b) => a.sort_order - b.sort_order)">
-            <x-rapidez::input.swatch.text
-                type="radio"
-                name="{{ $superAttribute->code }}"
-                v-model="addToCart.options[{{ $superAttributeId }}]"
-                v-bind:value="option.value"
-                v-bind:disabled="addToCart.disabledOptions.super_{{ $superAttribute->code }}.includes(option.value)"
-                v-bind:aria-label="option.label"
-                v-bind:id="option.label"
-                required
-            >
-                @{{ option.label }}
-            </x-rapidez::input.swatch.text>
-        </li>
+        @foreach ($product->{'super_' . $superAttribute->code} as $optionId => $option)
+            <li>
+                <x-rapidez::input.swatch.text
+                    type="radio"
+                    name="{{ $superAttribute->code }}"
+                    v-model="addToCart.options[{{ $superAttributeId }}]"
+                    v-bind:disabled="addToCart.disabledOptions.super_{{ $superAttribute->code }}.includes({{ $option->value }})"
+                    :value="$option->value"
+                    :aria-label="$option->label"
+                    :id="$option->label"
+                    required
+                >
+                    {{ $option->label }}
+                </x-rapidez::input.swatch.text>
+            </li>
+        @endforeach
     </ul>
 </div>
