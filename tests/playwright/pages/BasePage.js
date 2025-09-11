9a965c8097be5b31588862334209df217502beb0
Roy Duineveld <royduineveld@gmail.com>
2025-09-11 17:00:42 +0200

    Playwright Lighthouse score tests

--- tests/playwright/pages/BasePage.js
+++ tests/playwright/pages/BasePage.js
@@ -1,5 +1,8 @@
 import { expect } from '@playwright/test'
 import AxeBuilder from '@axe-core/playwright'
+import { playAudit } from 'playwright-lighthouse'
+import playwright from 'playwright'
+import lighthouseMobileConfig from 'lighthouse/core/config/lr-mobile-config.js';
 
 export class BasePage {
     constructor(page) {
@@ -52,5 +55,49 @@ export class BasePage {
         })
 
         expect(accessibilityScanResults.violations).toEqual([])
+    }
+
+    async lighthouse(url) {
+        const browser = await playwright['chromium'].launch({
+            args: ['--remote-debugging-port=9222'],
+        });
+        const page = await browser.newPage();
+
+        await page.goto(url)
+
+        await playAudit({
+            page: page,
+            port: 9222,
+            thresholds: {
+                performance: 90,
+                accessibility: 100,
+                'best-practices': 100,
+                seo: 100,
+            },
+            reports: {
+                formats: {
+                    html: true,
+                }
+            },
+            config: {
+                ...lighthouseMobileConfig,
+                settings: {
+                    ...lighthouseMobileConfig.settings,
+                    skipAudits: [
+                        ...lighthouseMobileConfig.settings.skipAudits,
+                        // Skip everything that's not fixed within CI tests.
+                        'meta-description',
+                        'is-on-https',
+                        'redirects-http',
+                        'uses-long-cache-ttl',
+                        'uses-optimized-images',
+                        'cache-insight',
+                        'image-delivery-insight',
+                    ]
+                }
+            },
+        });
+
+        await browser.close()
     }
 }
