c56e601b86c066c1fd7ba7522927e3924e4a9784
rapidez-actions <rapidez-actions@users.noreply.github.com>
2025-09-11 06:26:14 +0000

    Apply fixes from Prettier

--- resources/js/components/RecentlyViewed.vue
+++ resources/js/components/RecentlyViewed.vue
@@ -29,7 +29,7 @@ export default {
 
     methods: {
         add(id) {
-            if (! this.products.includes(id)) {
+            if (!this.products.includes(id)) {
                 this.products.push(id)
             }
 
