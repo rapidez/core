21eaed3ad512e0bdd7a0b35724b7e22cf2978c71
indy koning <indy@justbetter.nl>
2025-10-20 12:08:50 +0200

    Prevent logout flow being triggert when incorrect credentials are used

--- resources/js/stores/useUser.js
+++ resources/js/stores/useUser.js
@@ -136,6 +136,7 @@ export const login = async function (email, password) {
         },
         {
             notifyOnError: false,
+            redirectOnExpiration: false
         },
     ).then(async (response) => {
         await loginByToken(response.data.generateCustomerToken.token)
