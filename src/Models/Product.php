aaabaf9281dcf5456d4ace09ada798e99b6c5edf
indykoning <indykoning@users.noreply.github.com>
2025-11-06 14:26:50 +0000

    Apply fixes from Duster

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -217,7 +217,7 @@ class Product extends Model
             return [
                 'media_type' => $value->media_type,
                 'image'      => $value->value,
-                'video_url'  => isset($value->video) ? Str::embedUrl($value->video->url) : null
+                'video_url'  => isset($value->video) ? Str::embedUrl($value->video->url) : null,
             ];
         })->values()->toArray();
     }
