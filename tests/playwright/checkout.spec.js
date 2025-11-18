1d9758a70a1fd009cf54d542621b5946c8d8a428
indy koning <indy@justbetter.nl>
2025-11-18 15:18:54 +0100

    Added test

--- tests/playwright/checkout.spec.js
+++ tests/playwright/checkout.spec.js
@@ -39,3 +39,29 @@ checkoutTypes.forEach((type) => {
         await checkoutPage.checkout(email, password)
     })
 })
+
+test('incorrect password login', BasePage.tags, async ({ page }) => {
+        const productPage = new ProductPage(page)
+        const checkoutPage = new CheckoutPage(page, 'default')
+        const accountPage = new AccountPage(page)
+
+        const email = `wayne+${crypto.randomUUID()}@enterprises.com`
+        const password = 'IronManSucks.91939'
+
+        // Register
+        await productPage.addToCart(process.env.PRODUCT_URL_SIMPLE)
+        await checkoutPage.checkout(email, password, true, ['credentials'])
+
+        await accountPage.logout()
+
+        // Login
+        await productPage.addToCart(process.env.PRODUCT_URL_SIMPLE)
+        await checkoutPage.gotoCheckout()
+        await checkoutPage.login(email, password + '!')
+        await this.page.getByTestId('continue').click()
+        await this.page.waitForTimeout(500)
+        await this.page.waitForLoadState('networkidle')
+        await expect(page.getByTestId('notifications')).toHaveText('The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.')
+
+        await checkoutPage.checkout(email, password)
+    })
