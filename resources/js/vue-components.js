e8a4e14aa16238ecf876693bc85f06058f664def
indykoning <indykoning@users.noreply.github.com>
2025-11-24 14:39:45 +0000

    Apply fixes from Prettier

--- resources/js/vue-components.js
+++ resources/js/vue-components.js
@@ -62,7 +62,7 @@ Vue.component('autocomplete', () => ({
 if (import.meta.env.VITE_DISABLE_DOUBLE_CLICK_FIX !== 'true') {
     document.addEventListener('vue:loaded', function () {
         // Workaround double click bug on ipad & iphone: https://stackoverflow.com/questions/71535540/keyboard-doesnt-open-in-ios-on-focus
-        let userAgent = window.navigator.userAgent;
+        let userAgent = window.navigator.userAgent
         if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
             // Simply load the autocomplete ~600ms after pageload so it won't impact pageload but will
             // Be fast enough that most users won't notice
