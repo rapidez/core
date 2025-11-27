81c4e608b8d4c186b2353c9102ca08d669b02bd8
Jade <jade@justbetter.nl>
2025-11-27 15:39:23 +0100

    Fix unit tests

--- src/Models/Traits/HasToArrayData.php
+++ src/Models/Traits/HasToArrayData.php
@@ -7,11 +7,13 @@ trait HasToArrayData
     public function toArray(): array
     {
         return array_merge(
-            parent::toArray(),
-            ...collect(get_class_methods($this))
-                ->filter(fn($function) => str_ends_with($function, 'ToArrayData'))
-                ->map(fn($function) => $this->{$function}())
-                ->toArray(),
+            array_merge(
+                ...collect(get_class_methods($this))
+                    ->filter(fn($function) => str_ends_with($function, 'ToArrayData'))
+                    ->map(fn($function) => $this->{$function}())
+                    ->toArray(),
+            ),
+            parent::toArray()
         );
     }
 }
