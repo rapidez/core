e1e646df57c198441f46ba7e91918a632ffd996a
Jade Geels <jade@justbetter.nl>
2025-11-27 16:26:03 +0100

    Shipping address fallbacks (#1084)

--- resources/views/checkout/steps/shipping-method.blade.php
+++ resources/views/checkout/steps/shipping-method.blade.php
@@ -2,7 +2,7 @@
     :query="config.queries.setShippingMethodsOnCart"
     :variables="{
         cart_id: mask,
-        method: cart.shipping_addresses[0]?.selected_shipping_method?.carrier_code+'/'+cart.shipping_addresses[0]?.selected_shipping_method?.method_code,
+        method: cart.shipping_addresses?.[0]?.selected_shipping_method?.carrier_code+'/'+cart.shipping_addresses?.[0]?.selected_shipping_method?.method_code,
     }"
     group="shipping"
     :callback="updateCart"
@@ -17,10 +17,10 @@
     v-if="!cart.is_virtual"
 >
     <fieldset class="flex flex-col gap-3" partial-submit="mutate" v-on:change="window.app.$emit('setShippingAddressesOnCart')">
-        <label class="flex items-center p-5 border rounded relative bg-white" v-if="!cart.shipping_addresses[0]?.uid">
+        <label class="flex items-center p-5 border rounded relative bg-white" v-if="!cart.shipping_addresses?.[0]?.uid">
             <span>@lang('Please enter a shipping address first')</span>
         </label>
-        <label class="flex items-center gap-x-1.5 p-5 border rounded bg-white cursor-pointer text-sm text" v-for="(method, index) in cart.shipping_addresses[0]?.available_shipping_methods">
+        <label class="flex items-center gap-x-1.5 p-5 border rounded bg-white cursor-pointer text-sm text" v-for="(method, index) in cart.shipping_addresses?.[0]?.available_shipping_methods">
             <template v-if="false"></template>
                 @stack('shipping_methods')
             <template v-else>
@@ -39,4 +39,3 @@
         </label>
     </fieldset>
 </graphql-mutation>
-
