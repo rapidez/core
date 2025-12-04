40ae846aad1aa21df94af5ca724cc1cf907470f4
Roy Duineveld <royduineveld@gmail.com>
2025-12-04 14:45:47 +0100

    Customer group pricing in the autocomplete

--- resources/js/filters.js
+++ resources/js/filters.js
@@ -1,3 +1,5 @@
+import { user } from './stores/useUser'
+
 window.truncate = function (value, limit) {
     if (value.length > limit) {
         value = value.substring(0, limit - 3) + '...'
@@ -12,6 +14,25 @@ window.price = function (value, extra = {}) {
         currency: config.currency,
         ...extra,
     }).format(value)
+}
+
+window.productPrice = function (product) {
+    return product.price
+}
+
+window.productSpecialPrice = function (product) {
+    let groupId = user?.value?.group_id
+    let specialPrice = groupId
+        ? product.prices[groupId].min_price
+        : product.special_price
+
+    return window.productPrice(product) > specialPrice
+        ? specialPrice
+        : null
+}
+
+window.sumPrices = function (price1, price2) {
+    return Math.round((parseFloat(price1) + parseFloat(price2)) * 100) / 100
 }
 
 window.url = function (path = '') {
