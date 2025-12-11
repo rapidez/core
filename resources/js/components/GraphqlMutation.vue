572dde64d1950015730509feaeb7f74ffd81810e
indykoning <15870933+indykoning@users.noreply.github.com>
2025-12-11 13:32:18 +0100

    Create a writable redirectUrl

--- resources/js/components/GraphqlMutation.vue
+++ resources/js/components/GraphqlMutation.vue
@@ -78,6 +78,7 @@ export default {
         initialVariables: {},
         data: {},
         mutate: () => null,
+        redirectUrl: this.redirect,
     }),
 
     render() {
@@ -202,11 +203,11 @@ export default {
                     self.mutated = false
                 }, 2500)
 
-                if (!this.redirect && this.notify.message) {
+                if (!this.redirectUrl && this.notify.message) {
                     Notify(this.notify.message, this.notify.type ?? 'success')
                 }
 
-                if (this.redirect) {
+                if (this.redirectUrl) {
                     if (this.notify.message) {
                         document.addEventListener(
                             'vue:loaded',
@@ -216,7 +217,7 @@ export default {
                             { once: true },
                         )
                     }
-                    Turbo.visit(window.url(this.redirect))
+                    Turbo.visit(window.url(this.redirectUrl))
                 }
 
                 return response
