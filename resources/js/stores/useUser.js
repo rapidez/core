b44b6dfef52a3d580533c75b0af01c8432770b2c
Jade Geels <jade@justbetter.nl>
2025-12-31 11:22:22 +0100

    Undo extra change

--- resources/js/stores/useUser.js
+++ resources/js/stores/useUser.js
@@ -142,7 +142,6 @@ export const login = async function (email, password) {
         },
         {
             notifyOnError: false,
-            redirectOnExpiration: false,
         },
     ).then(async (response) => {
         await loginByToken(response.data.generateCustomerToken.token)
