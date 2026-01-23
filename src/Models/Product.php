d95a9134c8508a0c69538be28a3c98aff3cd766e
Jade Geels <jade@justbetter.nl>
2026-01-23 09:10:40 +0000

    Category model flat table migration (#1169)

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -41,6 +41,7 @@ class Product extends Model
 
     protected $table = 'catalog_product_entity';
     protected $primaryKey = 'entity_id';
+    protected $entityTypeId = EavAttribute::ENTITY_TYPE_CATALOG_PRODUCT;
 
     protected $with = [
         'stock',
