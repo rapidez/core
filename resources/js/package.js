fe019b5f9f6f7e43a6121389aec84a9dd8860b5a
indykoning <indykoning@users.noreply.github.com>
2026-01-29 10:17:25 +0000

    Apply fixes from Prettier

--- resources/js/package.js
+++ resources/js/package.js
@@ -129,8 +129,7 @@ function init() {
                     }
 
                     let url = new URL(imagePath)
-                    url = url.pathname.replace('/media', '')
-                        .replace('/.renditions/', '/')
+                    url = url.pathname.replace('/media', '').replace('/.renditions/', '/')
 
                     return window.url(`/storage/${store}/resizes/${size}/magento${url}`)
                 },
