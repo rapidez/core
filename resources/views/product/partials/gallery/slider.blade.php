742d22902999993c6ea4a3cd2d1981ddc07e0356
indykoning <15870933+indykoning@users.noreply.github.com>
2026-02-11 11:18:52 +0100

    Updated check

--- resources/views/product/partials/gallery/slider.blade.php
+++ resources/views/product/partials/gallery/slider.blade.php
@@ -8,7 +8,7 @@
             {{-- src should always be above v-bind:src --}}
             v-if="media[active].media_type === 'image'"
             v-bind:src="window.url('/storage/{{ config('rapidez.store') }}/resizes/400/magento/catalog/product' + media[active].image + '.webp')"
-            @if (($selectedChild->media ?? []) !== [])
+            @if (count($selectedChild->media))
                 src="{{ url('/storage/'.config('rapidez.store').'/resizes/400/magento/catalog/product'.Arr::first($selectedChild->media)['image'].'.webp') }}"
             @endif
             alt="{{ $product->name }}"
