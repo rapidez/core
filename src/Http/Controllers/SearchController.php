59681c9cb9873308563bf220aa10188a44310d95
Jade <jade@justbetter.nl>
2026-02-11 11:39:31 +0100

    Don't error out on search page

--- src/Http/Controllers/SearchController.php
+++ src/Http/Controllers/SearchController.php
@@ -4,28 +4,33 @@ namespace Rapidez\Core\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class SearchController
 {
     public function __invoke(Request $request)
     {
-        $request->validate(['q' => 'required|string|max:255']);
+        try {
+            $request->validate(['q' => 'required|string|max:255']);
 
-        $searchQuery = config('rapidez.models.search_query')::firstOrNew([
-            'query_text' => Str::lower($request->q),
-            'store_id'   => config('rapidez.store'),
-        ], ['popularity' => 1]);
+            $searchQuery = config('rapidez.models.search_query')::firstOrNew([
+                'query_text' => Str::lower($request->q),
+                'store_id'   => config('rapidez.store'),
+            ], ['popularity' => 1]);
 
-        if (! $searchQuery->exists) {
-            $searchQuery->save();
+            if (! $searchQuery->exists) {
+                $searchQuery->save();
 
-            return view('rapidez::search.overview');
-        }
+                return view('rapidez::search.overview');
+            }
 
-        $searchQuery->increment('popularity');
+            $searchQuery->increment('popularity');
 
-        if ($searchQuery->is_active === 1 && $searchQuery->redirect) {
-            return redirect($searchQuery->redirect, 301);
+            if ($searchQuery->is_active === 1 && $searchQuery->redirect) {
+                return redirect($searchQuery->redirect, 301);
+            }
+        } catch (ValidationException $e) {
+            //
         }
 
         return view('rapidez::search.overview');
