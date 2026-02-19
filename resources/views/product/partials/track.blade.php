27214dd80b7e0c02277521bda97bf286bb9e98be
Roy Duineveld <royduineveld@gmail.com>
2026-02-19 15:30:25 +0100

    Track products views with full page caching

---
+++ resources/views/product/partials/track.blade.php
@@ -0,0 +1,5 @@
+<script async>
+document.addEventListener('vue:loaded', function() {
+    window.rapidezAPI('POST', '/track/product/' + window.config.product.entity_id)
+})
+</script>
--- src/Http/Controllers/ProductController.php
+++ src/Http/Controllers/ProductController.php
@@ -3,8 +3,10 @@
 namespace Rapidez\Core\Http\Controllers;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\View;
 use Rapidez\Core\Events\ProductViewEvent;
 use Rapidez\Core\Models\Product;
+use Rapidez\Core\Facades\Rapidez;
 use TorMorten\Eventy\Facades\Eventy;
 
 class ProductController
@@ -63,7 +65,9 @@ class ProductController
         $selectedChild = $product->children->firstWhere(fn ($child) => count($selectedOptions) && collect($selectedOptions)->every(fn ($value, $code) => $child->getCustomAttribute($code)->value == $value)
         ) ?? $product;
 
-        ProductViewEvent::dispatch($product);
+        if (Rapidez::config('reports/options/enabled') && Rapidez::config('reports/options/product_view_enabled')) {
+            View::composer('rapidez::layouts.app', fn($view) => $view->getFactory()->startPush('foot', view('rapidez::product.partials.track')));
+        }
 
         config(['frontend.product' => $data]);
 
@@ -72,5 +76,15 @@ class ProductController
         return $response
             ->setEtag(md5($response->getContent() ?? ''))
             ->setLastModified($product->updated_at);
+    }
+
+    public function track(int $productId)
+    {
+        $productModel = config('rapidez.models.product');
+
+        /** @var \Rapidez\Core\Models\Product $product */
+        $product = $productModel::findOrFail($productId);
+
+        ProductViewEvent::dispatch($product);
     }
 }
