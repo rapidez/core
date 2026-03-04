400583a6437616eb9f791dcdbfd9a596ecb5f250
Roy Duineveld <royduineveld@gmail.com>
2026-03-04 09:11:01 +0100

    Removed the tailwind.config.js

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-// TODO: Double check if we need this somewhere,
-// for example with the text/bg utilities.
-function color(variable, fallback) {
-    return 'color-mix(in srgb, var(' + variable + ', ' + fallback + ') calc(100% * <alpha-value>), transparent)'
-}
-
-// TODO: Remove this files when everything is migrated.
