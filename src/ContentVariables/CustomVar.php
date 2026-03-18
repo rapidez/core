907290f78e63790371adee1d42c8cbf9e2ab82d1
indy koning <indy@justbetter.nl>
2026-03-18 13:45:17 +0100

    Added CustomVar ContentVariable, prevent breakage if block not found

---
+++ src/ContentVariables/CustomVar.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Rapidez\Core\ContentVariables;
+
+use Illuminate\Support\Facades\DB;
+
+class CustomVar
+{
+    private array $cache = [];
+
+    public function __invoke($content): ?string
+    {
+        return preg_replace_callback('/{{customVar code=(.*?)}}/ms', function (array $matches): string {
+            [$full, $code] = $matches;
+
+            return $this->cache[$code] ??= DB::table('variable')
+                ->leftJoin('variable_value', 'variable_value.variable_id', '=', 'variable.variable_id')
+                ->where('store_id', 0)
+                ->where('code', $code)
+                ->first()?->html_value ?? '';
+        }, (string) $content);
+    }
+}
