dd43cb1a975a86ddd5479f9a279eee48ff1a5a54
Jade-GG <Jade-GG@users.noreply.github.com>
2026-04-21 07:54:46 +0000

    Apply fixes from Duster

--- src/Auth/MagentoCartTokenGuard.php
+++ src/Auth/MagentoCartTokenGuard.php
@@ -15,7 +15,7 @@ class MagentoCartTokenGuard extends MagentoCustomerTokenGuard implements Guard
             $middleware->disableFor('mask');
         });
 
-        $authGuardCreator = fn($config) => new self(auth()->createUserProvider($config['provider']), request(), 'mask', 'mask');
+        $authGuardCreator = fn ($config) => new self(auth()->createUserProvider($config['provider']), request(), 'mask', 'mask');
         auth()->extend('magento-cart', function (Application $app, string $name, array $config) use ($authGuardCreator) {
             return $authGuardCreator($config);
         });
