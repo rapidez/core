d18498ce32469c0400455accdbf4e8a84490ac75
indy koning <indy@justbetter.nl>
2026-05-04 16:50:28 +0200

    Fixed getting csrftoken

--- resources/js/fetch.js
+++ resources/js/fetch.js
@@ -1,5 +1,6 @@
 import { addFetch } from './stores/useFetches.js'
 import { token } from './stores/useUser'
+import { get } from '@vueuse/core'
 
 export class FetchError extends Error {
     constructor(message, response) {
@@ -42,7 +43,7 @@ export const rapidezAPI = (window.rapidezAPI = async (method, endpoint, data = {
                 Store: window.config.store_code,
                 Authorization: token.value ? `Bearer ${token.value}` : null,
                 'Content-Type': 'application/json',
-                'X-CSRF-Token': window.app.config.globalProperties.csrfToken,
+                'X-CSRF-Token': get(window.app.config.globalProperties.csrfToken),
             },
             options?.headers || {},
         ),
