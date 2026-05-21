d445dee85dacc65ea362c5cb85b1bc6846357ce7
Jade-GG <Jade-GG@users.noreply.github.com>
2026-05-21 13:14:34 +0000

    Apply fixes from Prettier

--- resources/js/components/Search/Autocomplete.vue
+++ resources/js/components/Search/Autocomplete.vue
@@ -135,7 +135,7 @@ export default {
                     label: 'Default',
                     order: 'desc',
                     value: window.config.index.product,
-                }
+                },
             }
 
             return config
