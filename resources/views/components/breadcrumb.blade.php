53d4ed019d71f6bedb0b260cff50dbdcda1f8340
jimmy <jimmy@justbetter.nl>
2026-06-02 11:07:32 +0200

    fix product structured data on pdp

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -2,10 +2,11 @@
 
 <li class="flex items-center shrink-0 whitespace-nowrap" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
     @if (!$active)
-        <a {{ $attributes->merge(['class' => 'text-sm hover:underline', 'href' => url($url), 'itemprop' => 'item']) }}>
+        <a {{ $attributes->merge(['class' => 'text-sm hover:underline', 'href' => url($url)]) }}>
             <span itemprop="name">{{ $slot }}</span>
-            <meta itemprop="position" content="{{ $position }}" />
         </a>
+        <meta itemprop="item" content="{{ url($url) }}" />
+        <meta itemprop="position" content="{{ $position }}" />
         <x-heroicon-o-chevron-right class="size-3 text-muted translate-y-px mx-1" stroke-width="2" />
     @else
         <span class="text-sm" itemprop="name">{{ $slot }}</span>
--- resources/views/product/partials/microdata.blade.php
+++ resources/views/product/partials/microdata.blade.php
@@ -1,5 +1,15 @@
+@php
+    $description = $product->description
+        ? str(html_entity_decode(strip_tags($product->description), ENT_QUOTES | ENT_HTML5))->squish()
+        : null;
+@endphp
+
+<meta itemprop="name" content="{{ $product->name }}" />
 <meta itemprop="mpn" content="{{ $product->entity_id }}" />
 <meta itemprop="sku" content="{{ $product->sku }}" />
+@if ($description)
+    <meta itemprop="description" content="{{ $description }}" />
+@endif
 
 @foreach ($product->images as $image)
     <meta itemprop="image" content="{{ config('rapidez.media_url').'/catalog/product'.$image }}" />
@@ -7,10 +17,11 @@
 
 <div itemprop="offers" itemtype="https://schema.org/Offer" itemscope>
     <meta itemprop="availability" content="https://schema.org/{{ $product->stock->is_in_stock ? 'InStock' : 'OutOfStock' }}" />
+    <meta itemprop="itemCondition" content="https://schema.org/NewCondition" />
     <meta itemprop="priceCurrency" content="@config('currency/options/default')" />
     <meta itemprop="price" content="{{ round($product->special_price ?: $product->price, 2) }}" />
     <meta itemprop="url" content="{{ url($product->url) }}" />
     @if ($product->special_to_date && $product->special_to_date > now()->toDateTimeString())
-        <meta itemprop="priceValidUntil" content="{{ $product->special_to_date }}" />
+        <meta itemprop="priceValidUntil" content="{{ \Illuminate\Support\Carbon::parse($product->special_to_date)->toDateString() }}" />
     @endif
 </div>
