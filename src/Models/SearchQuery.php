f7d276110584d7fe1fa10a91472979d6f171b189
Jade <jade@justbetter.nl>
2026-06-18 10:39:28 +0200

    Limit search query count when there are a lot of them

--- src/Models/SearchQuery.php
+++ src/Models/SearchQuery.php
@@ -3,6 +3,8 @@
 namespace Rapidez\Core\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Rapidez\Core\Models\Scopes\IsActiveScope;
 use Rapidez\Core\Models\Traits\Searchable;
 
@@ -24,9 +26,22 @@ class SearchQuery extends Model
 
     protected function makeAllSearchableUsing(Builder $query)
     {
+        // Decide on a minimum popularity to make sure we don't index too much.
+        // Simply using an orderBy with a limit on the query doesn't work as the limit gets stripped off.
+        $minPopularity = Cache::driver('array')->rememberForever(
+            'minPopularity',
+            fn() => DB::table('search_query')
+                ->orderByDesc('popularity')
+                ->limit(1)
+                ->offset(10000)
+                ->first()
+                ->popularity ?? 0
+        );
+
         return $query
-            ->where(fn ($subQuery) => $subQuery
-                ->where('num_results', '>', 0)
+            ->where(
+                fn($subQuery) => $subQuery
+                ->where(fn($subSubQuery) => $subSubQuery->where('num_results', '>', 0)->where('popularity', '>', $minPopularity))
                 ->orWhereNotNull('redirect')
             );
     }
