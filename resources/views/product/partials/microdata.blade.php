5ce4bd8e19f9190e309a0b91183c9d3f040b2126
Roene <roene@justbetter.nl>
2026-07-22 11:55:25 +0200

    fix date format | double check microdata

--- resources/views/product/partials/microdata.blade.php
+++ resources/views/product/partials/microdata.blade.php
@@ -11,7 +11,8 @@
     <meta itemprop="priceCurrency" content="@config('currency/options/default')" />
     <meta itemprop="price" content="{{ round($product->special_price ?: $product->price, 2) }}" />
     <meta itemprop="url" content="{{ url($product->url) }}" />
-    @if ($product->special_to_date && $product->special_to_date > now()->toDateTimeString())
-        <meta itemprop="priceValidUntil" content="{{ str($product->special_to_date)->before(' ')->before('T') }}" />
+    @php $specialToDate = $product->raw('special_to_date'); @endphp
+    @if ($specialToDate?->isFuture())
+        <meta itemprop="priceValidUntil" content="{{ $specialToDate->format('Y-m-d') }}" />
     @endif
 </div>
