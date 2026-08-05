e2c655b0c552873de95a46fef8eaf82b1e751936
Jade Geels <jade@justbetter.nl>
2026-08-05 09:09:56 +0000

    Check for searchquery being undefined (#1344)

--- resources/views/layouts/partials/header/autocomplete/history.blade.php
+++ resources/views/layouts/partials/header/autocomplete/history.blade.php
@@ -1,6 +1,6 @@
 <ais-state-results v-slot="{ state: { query: searchQuery } }">
     <div>
-        <div v-if="autocompleteSlotProps.searchHistory && autocompleteSlotProps.searchHistory?.filter(([query, metadata]) => query.includes(searchQuery.toLowerCase())).length" class="border-b py-2">
+        <div v-if="autocompleteSlotProps.searchHistory && searchQuery !== undefined && autocompleteSlotProps.searchHistory?.filter(([query, metadata]) => query.includes(searchQuery.toLowerCase())).length" class="border-b py-2">
             <x-rapidez::autocomplete.title>
                 @lang('Previous Searches')
             </x-rapidez::autocomplete.title>
