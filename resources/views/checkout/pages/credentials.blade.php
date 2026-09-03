082572a368583c792e9f29abe4e345b7628f5777
indy koning <indy@justbetter.nl>
2026-09-03 09:22:41 +0200

    Fixed double click submit before next

--- resources/views/checkout/pages/credentials.blade.php
+++ resources/views/checkout/pages/credentials.blade.php
@@ -33,7 +33,7 @@
                             @include('rapidez::checkout.steps.shipping-method')
                         </template>
 
-                        <x-rapidez::button.conversion type="submit" data-testid="continue" class="self-start" loader>
+                        <x-rapidez::button.conversion type="submit" data-testid="continue" class="self-start" loader v-on:mousedown.prevent="{{-- Do not remove, this prevents requiring double click for the submit action --}}">
                             @lang('Next')
                         </x-rapidez::button.conversion>
                     </form>
--- resources/views/checkout/steps/place-order.blade.php
+++ resources/views/checkout/steps/place-order.blade.php
@@ -8,7 +8,7 @@
     v-slot="{ mutate, variables }"
 >
     <fieldset>
-        <x-rapidez::button.conversion type="submit" data-testid="continue" class="mt-3" loader>
+        <x-rapidez::button.conversion type="submit" data-testid="continue" class="mt-3" loader v-on:mousedown.prevent="{{-- Do not remove, this prevents requiring double click for the submit action --}}">
             @lang('Place order')
         </x-rapidez::button.conversion>
     </fieldset>
